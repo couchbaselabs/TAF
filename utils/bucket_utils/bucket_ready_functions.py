472da153ce146f34e4bd83711c889bf332fed8ed
Bharath G P <gpbharath@yahoo.in>
2018-12-12 17:42:52 +0530

    Fixed getting the failover logs with right logic.

--- utils/bucket_utils/bucket_ready_functions.py
+++ utils/bucket_utils/bucket_ready_functions.py
@@ -1043,42 +1043,46 @@ class bucket_utils():
                                                        meta_data_store[bucket.name])
             task.result()
 
-    def compare_per_node_for_failovers_consistency(self, map1):
+    def compare_per_node_for_failovers_consistency(self, map1, vbucketMap):
         """
             Method to check uuid is consistent on active and replica new_vbucket_stats
         """
         bucketMap = {}
         logic = True
-        output = ""
         for bucket in map1.keys():
             map = {}
-            nodeMap = {}
-            logic = True
+            tempMap = {}
             output = ""
             for node in map1[bucket].keys():
                 for vbucket in map1[bucket][node].keys():
                     id = map1[bucket][node][vbucket]['id']
                     seq = map1[bucket][node][vbucket]['seq']
                     num_entries = map1[bucket][node][vbucket]['num_entries']
+                    state = vbucketMap[bucket][node][vbucket]['state']
                     if vbucket in map.keys():
                         if map[vbucket]['id'] != id:
                             logic = False
-                            output += "\n bucket {0}, vbucket {1} :: Original node {2}. UUID {3}, Change node {4}. UUID {5}".format(
-                                bucket, vbucket, nodeMap[vbucket], map[vbucket]['id'], node, id)
-                        if int(map[vbucket]['seq']) == int(seq):
+                            output += "\n bucket {0}, vbucket {1} :: Original node {2} {3} :: UUID {4}, Change node {5} {6} UUID {7}".format(
+                                bucket, vbucket, tempMap[vbucket]['node'], tempMap[vbucket]['state'],
+                                map[vbucket]['id'], node, state, id)
+                        if int(map[vbucket]['seq']) != int(seq):
                             logic = False
-                            output += "\n bucket {0}, vbucket {1} :: Original node {2}. seq {3}, Change node {4}. seq {5}".format(
-                                bucket, vbucket, nodeMap[vbucket], map[vbucket]['seq'], node, seq)
-                        if int(map[vbucket]['num_entries']) == int(num_entries):
+                            output += "\n bucket {0}, vbucket {1} :: Original node {2} {3} :: seq {4}, Change node {5} {6}  :: seq {7}".format(
+                                bucket, vbucket, tempMap[vbucket]['node'], tempMap[vbucket]['state'],
+                                map[vbucket]['seq'], node, state, seq)
+                        if int(map[vbucket]['num_entries']) != int(num_entries):
                             logic = False
-                            output += "\n bucket {0}, vbucket {1} :: Original node {2}. num_entries {3}, Change node {4}. num_entries {5}".format(
-                                bucket, vbucket, nodeMap[vbucket], map[vbucket]['num_entries'], node, num_entries)
+                            output += "\n bucket {0}, vbucket {1} :: Original node {2} {3} :: num_entries {4}, Change node {5} {6} :: num_entries {7}".format(
+                                bucket, vbucket, tempMap[vbucket]['node'], tempMap[vbucket]['state'],
+                                map[vbucket]['num_entries'], node, state, num_entries)
                     else:
                         map[vbucket] = {}
+                        tempMap[vbucket] = {}
                         map[vbucket]['id'] = id
                         map[vbucket]['seq'] = seq
                         map[vbucket]['num_entries'] = num_entries
-                        nodeMap[vbucket] = node
+                        tempMap[vbucket]['node'] = node
+                        tempMap[vbucket]['state'] = state
             bucketMap[bucket] = map
         if not logic:
             raise Exception(output)
@@ -1088,9 +1092,11 @@ class bucket_utils():
         """
             Method to get failovers logs from a cluster using cbstats
         """
-        servers = self.cluster_util.get_kv_nodes(servers)
+        vbucketMap = self.data_collector.collect_vbucket_stats(buckets, servers, collect_vbucket=True,
+                                                               collect_vbucket_seqno=False,
+                                                               collect_vbucket_details=False, perNode=True)
         new_failovers_stats = self.data_collector.collect_failovers_stats(buckets, servers, perNode=True)
-        new_failovers_stats = self.compare_per_node_for_failovers_consistency(new_failovers_stats)
+        new_failovers_stats = self.compare_per_node_for_failovers_consistency(new_failovers_stats, vbucketMap)
         return new_failovers_stats
 
     def compare_failovers_logs(self, prev_failovers_stats, servers, buckets, perNode=False, comp_map=None):
