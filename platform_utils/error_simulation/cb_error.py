03d7e09197e8d21b52a4e6392fd20f9d37f594d9
Ashwin G <ashwintrojan+github@gmail.com>
2019-04-08 15:42:56 +0530

    Creating constants within Cb_error
    Change-Id: I09e323225aa31572e11a67f3dff3976eaee334f6
    Reviewed-on: http://review.couchbase.org/107444
    Tested-by: Ashwin <ashwintrojan+github@gmail.com>
    Reviewed-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>

--- platform_utils/error_simulation/cb_error.py
+++ platform_utils/error_simulation/cb_error.py
@@ -5,6 +5,15 @@ log = logger.Logger.get_logger()
 
 
 class CouchbaseError(object):
+    # Constants used within this class
+    STOP_BEAMSMP = "stop_beam.smp"
+    STOP_MEMCACHED = "stop_memcached"
+    STOP_PERSISTENCE = "start_persistence"
+    STOP_SERVER = "stop_server"
+
+    KILL_MEMCACHED = "kill_memcached"
+    KILL_BEAMSMP = "kill_beam.smp"
+
     def __init__(self, shell_conn):
         self.shell_conn = shell_conn
 
@@ -24,36 +33,37 @@ class CouchbaseError(object):
             return self.kill_process(process_name, process_name, signum=9)
 
     def create(self, action=None, bucket_name="default"):
-        if action == "stop_memcached":
+        if action == CouchbaseError.STOP_MEMCACHED:
             _, error = self.__interrupt_process("memcached", "stop")
             self.__handle_shell_error(error)
-        elif action == "kill_memcached":
+        elif action == CouchbaseError.KILL_MEMCACHED:
             _, error = self.__interrupt_process("memcached", "kill")
             self.__handle_shell_error(error)
-        elif action == "stop_beam.smp":
+        elif action == CouchbaseError.STOP_BEAMSMP:
             _, error = self.__interrupt_process("beam.smp", "stop")
             self.__handle_shell_error(error)
-        elif action == "kill_beam.smp":
+        elif action == CouchbaseError.KILL_BEAMSMP:
             _, error = self.__interrupt_process("beam.smp", "kill")
             self.__handle_shell_error(error)
-        elif action == "stop_server":
+        elif action == CouchbaseError.STOP_SERVER:
             self.shell_conn.stop_server()
-        elif action == "stop_persistence":
+        elif action == CouchbaseError.STOP_PERSISTENCE:
             cbepctl_obj = Cbepctl(self.shell_conn)
             cbepctl_obj.persistence(bucket_name, "stop")
         else:
             log.error("Unsupported action: '{0}'".format(action))
 
     def revert(self, action=None, bucket_name="default"):
-        if action == "stop_memcached":
+        if action == CouchbaseError.STOP_MEMCACHED:
             _, error = self.__interrupt_process("memcached", "resume")
             self.__handle_shell_error(error)
-        elif action == "stop_beam.smp":
+        elif action == CouchbaseError.STOP_BEAMSMP:
             _, error = self.__interrupt_process("beam.smp", "resume")
             self.__handle_shell_error(error)
-        elif action in ["kill_beam.smp", "stop_server"]:
+        elif action == CouchbaseError.KILL_BEAMSMP \
+                or action == CouchbaseError.STOP_SERVER:
             self.shell_conn.start_server()
-        elif action == "start_persistence":
+        elif action == CouchbaseError.STOP_PERSISTENCE:
             cbepctl_obj = Cbepctl(self.shell_conn)
             cbepctl_obj.persistence(bucket_name, "start")
         else:
