955b305aa63477eca82d49780495e09d3e34c759
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2019-05-20 22:08:33 +0530

    Updating the bucket class and its usage
    Change-Id: I2826e52021692c11ad864b1086867fbfe3e839a4
    Pending: Need to delete the bucket class and file from the utils.
    Reviewed-on: http://review.couchbase.org/109375
    Reviewed-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>
    Tested-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>

--- lib/BucketLib/bucket.py
+++ lib/BucketLib/bucket.py
@@ -1,22 +1,19 @@
-'''
-Created on Sep 25, 2017
-
-@author: riteshagarwal
-'''
 from memcached.helper.kvstore import KVStore
 
 
 class Bucket(object):
     name = "name"
     replicas = "replicas"
-    size = "size"
-    type = "type"
-    replicaIndex = "replicaIndex"
-    eviction_policy = "eviction_policy"
+    ramQuotaMB = "ramQuotaMB"
+    bucketType = "bucketType"
+    replicaNumber = "replicaNumber"
+    evictionPolicy = "evictionPolicy"
     priority = "priority"
-    flush_enabled = "flush_enabled"
+    flushEnabled = "flushEnabled"
     lww = "lww"
     maxTTL = "maxTTL"
+    replicaIndex = "replicaIndex"
+    threadsNumber = "threadsNumber"
     compressionMode = "compressionMode"
     uuid = "uuid"
 
@@ -29,6 +26,7 @@ class Bucket(object):
         VALUE_ONLY = "valueOnly"
         FULL_EVICTION = "fullEviction"
         NO_EVICTION = "noEviction"
+        NRU_EVICTION = "nruEviction"
 
     class bucket_compression_mode:
         ACTIVE = "active"
@@ -51,20 +49,31 @@ class Bucket(object):
 
     def __init__(self, new_params={}):
         self.name = new_params.get(Bucket.name, "default")
-        self.type = new_params.get(Bucket.type, Bucket.bucket_type.MEMBASE)
-        self.replicas = new_params.get(Bucket.replicas, 0)
-        self.size = new_params.get(Bucket.size, 100)
+        self.bucketType = new_params.get(Bucket.bucketType,
+                                         Bucket.bucket_type.MEMBASE)
+        self.replicaNumber = new_params.get(Bucket.replicaNumber, 0)
+        self.ramQuotaMB = new_params.get(Bucket.ramQuotaMB, 100)
         self.kvs = {1: KVStore()}
-        self.eviction_policy = new_params.get(Bucket.eviction_policy,
-                                              Bucket.bucket_eviction_policy.VALUE_ONLY)
+
+        if self.bucketType == Bucket.bucket_type.EPHEMERAL:
+            self.evictionPolicy = new_params.get(
+                Bucket.evictionPolicy,
+                Bucket.bucket_eviction_policy.NO_EVICTION)
+        else:
+            self.evictionPolicy = new_params.get(
+                Bucket.evictionPolicy,
+                Bucket.bucket_eviction_policy.VALUE_ONLY)
+
         self.replicaIndex = new_params.get(Bucket.replicaIndex, 0)
         self.priority = new_params.get(Bucket.priority, None)
+        self.threadsNumber = new_params.get(Bucket.threadsNumber, 3)
         self.uuid = None
         self.lww = new_params.get(Bucket.lww, False)
         self.maxTTL = new_params.get(Bucket.maxTTL, None)
-        self.flush_enabled = new_params.get(Bucket.flush_enabled, 1)
-        self.compressionMode = new_params.get(Bucket.compressionMode,
-                                              Bucket.bucket_compression_mode.PASSIVE)
+        self.flushEnabled = new_params.get(Bucket.flushEnabled, 1)
+        self.compressionMode = new_params.get(
+            Bucket.compressionMode,
+            Bucket.bucket_compression_mode.PASSIVE)
         self.nodes = None
         self.stats = None
         self.servers = []
@@ -72,20 +81,4 @@ class Bucket(object):
         self.forward_map = []
 
     def __str__(self):
-        return self.params['name']
-
-
-class vBucket():
-    def __init__(self):
-        self.master = ''
-        self.replica = []
-        self.id = -1
-
-
-class BucketStats():
-    def __init__(self):
-        self.opsPerSec = 0
-        self.itemCount = 0
-        self.diskUsed = 0
-        self.memUsed = 0
-        self.ram = 0
+        return self.name
