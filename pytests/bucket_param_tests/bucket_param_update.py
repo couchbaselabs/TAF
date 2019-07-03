a1c9b3353c7d4db60dda1c4e22a6bbfc68d5e824
Ashwin G <ashwintrojan+github@gmail.com>
2019-07-03 17:02:34 +0530

    CBQE-4985: Fixing replica_update tests
    Change-Id: I7818a088d73508918e6949929a6bc38f90bb9147
    Reviewed-on: http://review.couchbase.org/111545
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Ashwin <ashwintrojan+github@gmail.com>
    Reviewed-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>

--- pytests/bucket_param_tests/bucket_param_update.py
+++ pytests/bucket_param_tests/bucket_param_update.py
@@ -3,9 +3,9 @@ from couchbase_helper.documentgenerator import doc_generator
 from BucketLib.BucketOperations import BucketHelper
 
 
-class Bucket_param_test(BaseTestCase):
+class BucketParamTest(BaseTestCase):
     def setUp(self):
-        super(Bucket_param_test, self).setUp()
+        super(BucketParamTest, self).setUp()
         self.key = 'test_docs'.rjust(self.key_size, '0')
         nodes_init = self.cluster.servers[1:self.nodes_init] \
             if self.nodes_init != 1 else []
@@ -39,48 +39,40 @@ class Bucket_param_test(BaseTestCase):
         self.log.info("==========Finished Bucket_param_test setup========")
 
     def tearDown(self):
-        super(Bucket_param_test, self).tearDown()
+        super(BucketParamTest, self).tearDown()
 
-    def generic_replica_update(self, doc_ops, bucket_helper_obj,
+    def generic_replica_update(self, doc_count, doc_ops, bucket_helper_obj,
                                replicas_to_update, start_doc_for_insert):
-        update_replicateTo_persistTo = self.input.param(
-            "update_replicateTo_persistTo", False)
         def_bucket = self.bucket_util.get_all_buckets()[0]
-        start_doc_for_delete = start_doc_for_insert
-        incr_val_for_delete = int(self.num_items/3)
 
         for replica_num in replicas_to_update:
             tasks = list()
             # Creating doc creator to be used by test cases
             doc_create = doc_generator(self.key, start_doc_for_insert,
-                                       self.num_items * 2,
-                                       doc_size=self.doc_size,
-                                       doc_type=self.doc_type,
-                                       vbuckets=self.vbuckets)
-
-            # Creating doc updater to be used by test cases
-            doc_update = doc_generator(self.key,
-                                       int(start_doc_for_insert/2),
                                        start_doc_for_insert + self.num_items,
                                        doc_size=self.doc_size,
                                        doc_type=self.doc_type,
                                        vbuckets=self.vbuckets)
 
             # Creating doc updater to be used by test cases
+            doc_update = doc_generator(
+                self.key,
+                start_doc_for_insert - (self.num_items/2),
+                start_doc_for_insert,
+                doc_size=self.doc_size,
+                doc_type=self.doc_type,
+                vbuckets=self.vbuckets)
+
+            # Creating doc updater to be used by test cases
             doc_delete = doc_generator(
-                self.key, start_doc_for_delete,
-                start_doc_for_delete + incr_val_for_delete,
+                self.key,
+                start_doc_for_insert - self.num_items,
+                start_doc_for_insert - (self.num_items/2),
                 doc_size=self.doc_size, doc_type=self.doc_type,
                 vbuckets=self.vbuckets)
 
             self.log.info("Updating replica count of bucket to {0}"
                           .format(replica_num))
-
-            # Used for testing old durability feature
-            if update_replicateTo_persistTo:
-                if self.self.durability_level is None:
-                    self.replicate_to = replica_num
-                    self.persist_to = replica_num + 1
 
             bucket_helper_obj.change_bucket_props(
                 def_bucket.name, replicaNumber=replica_num)
@@ -94,8 +86,8 @@ class Bucket_param_test(BaseTestCase):
                     durability=self.durability_level,
                     timeout_secs=self.sdk_timeout,
                     batch_size=10, process_concurrency=8))
-                self.num_items += (doc_create.end - doc_create.start)
-                start_doc_for_insert = self.num_items
+                doc_count += (doc_create.end - doc_create.start)
+                start_doc_for_insert += self.num_items
             if "update" in doc_ops:
                 # Start doc update task in parallel with replica_update
                 tasks.append(self.task.async_load_gen_docs(
@@ -112,8 +104,7 @@ class Bucket_param_test(BaseTestCase):
                     durability=self.durability_level,
                     timeout_secs=self.sdk_timeout,
                     batch_size=10, process_concurrency=8))
-                self.num_items -= (incr_val_for_delete - start_doc_for_delete)
-                start_doc_for_delete += incr_val_for_delete
+                doc_count -= (doc_delete.end - doc_delete.start)
 
             # Start rebalance task with doc_ops in parallel
             rebalance = self.task.async_rebalance(self.cluster.servers, [], [])
@@ -142,7 +133,7 @@ class Bucket_param_test(BaseTestCase):
 
             # Verify doc load count after each mutation cycle
             self.bucket_util._wait_for_stats_all_buckets()
-            self.bucket_util.verify_stats_all_buckets(self.num_items)
+            self.bucket_util.verify_stats_all_buckets(doc_count)
         return start_doc_for_insert
 
     def test_replica_update(self):
@@ -150,23 +141,22 @@ class Bucket_param_test(BaseTestCase):
             self.log.error("Test not supported for < 2 node cluster")
             return
 
-        doc_ops = self.input.param("doc_ops", None)
-        if doc_ops is None:
-            doc_ops = list()
-        else:
-            doc_ops = doc_ops.split(":")
-
+        doc_ops = self.input.param("doc_ops", "")
         bucket_helper = BucketHelper(self.cluster.master)
 
-        start_doc_for_insert = 0
+        doc_count = self.num_items
+        start_doc_for_insert = self.num_items
         # Replica increment tests
-        start_doc_for_insert = self.generic_replica_update(
+        doc_count, start_doc_for_insert = self.generic_replica_update(
+            doc_count,
             doc_ops,
             bucket_helper,
             range(1, min(4, self.nodes_init)),
             start_doc_for_insert)
+
         # Replica decrement tests
-        start_doc_for_insert = self.generic_replica_update(
+        _, _ = self.generic_replica_update(
+            doc_count,
             doc_ops,
             bucket_helper,
             range(min(4, self.nodes_init)-2, -1, -1),
