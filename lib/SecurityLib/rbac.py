92e5675bdf118c251da245df1399df45e8c2eee5
Ashwin <ashwintrojan+github@gmail.com>
2019-08-16 14:31:40 +0530

    Fixing PEP8 warnings in rbac.py
    Change-Id: Id0e22441c0f484de5a4a8ff074f0130326da9bce
    Reviewed-on: http://review.couchbase.org/113412
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ashwin <ashwintrojan+github@gmail.com>

--- lib/SecurityLib/rbac.py
+++ lib/SecurityLib/rbac.py
@@ -3,44 +3,44 @@ from internal_user import InternalUser
 import urllib
 import json
 
+
 class RbacUtil:
-    def __init__(self,
-                 host=None,
-                 source=None):
+    def __init__(self, host=None, source=None):
         self.host = host
         self.source = source
 
-    '''
+    """
     Enable LDAP on node
-    '''
-    def enable_ldap(self,rest):
+    """
+    def enable_ldap(self, rest):
         content = rest.ldapRestOperationGetResponse()
         if not content['enabled']:
             api = rest.baseUrl + 'settings/saslauthdAuth'
-            params = urllib.urlencode({"enabled": 'true', "admins": [], "roAdmins": []})
+            params = urllib.urlencode({"enabled": 'true',
+                                       "admins": [],
+                                       "roAdmins": []})
             status, content, header = rest._http_request(api, 'POST', params)
             return json.loads(content)
 
-    '''
+    """
          user_list = [{'id':ritam,'password':'password','name':'newname'}]
-    '''
-    def create_user_source(self,user_list=None,source=None,host=None):
+    """
+    def create_user_source(self, user_list=None, source=None, host=None):
         for user in user_list:
             userid = user['id']
             password = user['password']
             user_name = user['name']
             if source == 'ldap':
-                LdapUser(userid,password,host).user_setup()
+                LdapUser(userid, password, host).user_setup()
             if source == 'builtin':
                 payload = "name=" + user_name + "&roles=&password=" + password
-                InternalUser(userid,payload,host).user_setup()
+                InternalUser(userid, payload, host).user_setup()
 
-
-    '''
+    """
     user_role_list = list of user information and role assignment
                      [{'id':ritam,'name'=ritamsharma,'roles'='cluster_admin:admin'}]
-    '''
-    def add_user_role(self,user_role_list,rest,source=None):
+    """
+    def add_user_role(self, user_role_list, rest, source=None):
         if source:
             self.source = source
         response_return = []
@@ -54,26 +54,19 @@ class RbacUtil:
             else:
                 for role in user_role_param:
                     final_roles = role + "," + final_roles
-            payload="name="+username+"&roles="+final_roles
+            payload = "name=" + username + "&roles=" + final_roles
             if self.source == "ldap":
-                response = rest.set_user_roles(userid,payload)
+                response = rest.set_user_roles(userid, payload)
             elif self.source == 'builtin':
-                # cluster_compatibility = rest.check_cluster_compatibility("5.0")
-                # if cluster_compatibility is None:
-                #     pre_spock = True
-                # else:
-                #     pre_spock = not cluster_compatibility
-                # if pre_spock:
-                #     return None
-                response = rest.add_set_builtin_user(userid,payload)
-            response_return.append({'id':userid,'reponse':response})
+                response = rest.add_set_builtin_user(userid, payload)
+            response_return.append({'id': userid, 'reponse': response})
         return response_return
 
-    '''
+    """
     user_id_list - list of user that needs to be deleted
                     [ritam,arun]
-    '''
-    def remove_user_role(self,user_id_list,rest,source=None):
+    """
+    def remove_user_role(self, user_id_list, rest, source=None):
         response_return = []
         for user in user_id_list:
             if self.source == 'ldap':
@@ -87,10 +80,10 @@ class RbacUtil:
                 if pre_spock:
                     return None
                 response = rest.delete_builtin_user(user)
-            response_return.append({'id':user,'response':response})
+            response_return.append({'id': user, 'response': response})
         return response_return
 
-    def check_user_permission(self,user,password,user_per_list,rest):
-        response = rest.check_user_permission(user,password,user_per_list)
+    def check_user_permission(self, user, password, user_per_list, rest):
+        response = rest.check_user_permission(user, password, user_per_list)
         print response
         return response
