d0f2feca1160fb79bb76a3dc0d85ce5e8a66f34a
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2019-09-11 19:49:18 +0530

    Fix for exception check in result error
    Change-Id: Iacad6536a87ad0e3843cc8c063617ecb0cf0e531
    Reviewed-on: http://review.couchbase.org/114614
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ashwin <ashwintrojan+github@gmail.com>
    Tested-by: Ashwin <ashwintrojan+github@gmail.com>

--- lib/Jython_tasks/task.py
+++ lib/Jython_tasks/task.py
@@ -1905,7 +1905,7 @@ class ValidateDocumentsTask(GenericLoadingTask):
                                          % self.op_type))
         result_map, self.failed_reads = self.batch_read(key_value.keys())
         for key, value in self.failed_reads.items():
-            if self.failed_reads[key]["error"] != DurableExceptions.KeyNotFoundException:
+            if DurableExceptions.KeyNotFoundException not in self.failed_reads[key]["error"]:
                 self.failed_item_table.add_row([key, value['error']])
         missing_keys, wrong_values = self.validate_key_val(result_map,
                                                            key_value)
@@ -1946,7 +1946,7 @@ class ValidateDocumentsTask(GenericLoadingTask):
                     wrong_value = "Key: {} Expected: {} Actual: {}" \
                         .format(key, expected_val, actual_val)
                     wrong_values.append(wrong_value)
-            elif self.failed_reads[key]["error"] is DurableExceptions.KeyNotFoundException:
+            elif DurableExceptions.KeyNotFoundException in self.failed_reads[key]["error"]:
                 missing_keys.append(key)
         return missing_keys, wrong_values
 
