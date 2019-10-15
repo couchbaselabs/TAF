a97e0af01784aeab71805207af293926857be444
Anitha Kuberan <anitha.kuberan@couchbase.com>
2019-10-15 11:50:47 +0530

    upgrade changes for transaction
    Change-Id: Ied4b952a68929636feb519b7291b562bafa12e6d
    Reviewed-on: http://review.couchbase.org/116416
    Reviewed-by: Ashwin <ashwintrojan+github@gmail.com>
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Ashwin <ashwintrojan+github@gmail.com>

--- pytests/upgrade/durability_upgrade.py
+++ pytests/upgrade/durability_upgrade.py
@@ -38,27 +38,47 @@ class UpgradeTests(UpgradeBase):
                 self.cluster.update_master(
                     self.cluster.servers[self.nodes_init-1])
             # Validate sync_write results after upgrade
-            sync_write_task = self.task.async_load_gen_docs(
-                self.cluster, self.bucket, create_gen, "create",
-                durability=self.durability_level,
-                timeout_secs=self.sdk_timeout,
-                process_concurrency=4,
-                skip_read_on_error=True,
-                suppress_error_table=True)
+            if self.atomicity:
+                create_batch_size = 10
+                create_gen = doc_generator(self.key, self.num_items,
+                                   self.num_items+create_batch_size)
+                sync_write_task = self.task.async_load_gen_docs_atomicity(
+                    self.cluster, self.bucket_util.buckets, create_gen, "create",
+                    process_concurrency=1,
+                    transaction_timeout=self.transaction_timeout,
+                    record_fail=True)
+            else:
+                sync_write_task = self.task.async_load_gen_docs(
+                    self.cluster, self.bucket, create_gen, "create",
+                    durability=self.durability_level,
+                    timeout_secs=self.sdk_timeout,
+                    process_concurrency=4,
+                    skip_read_on_error=True,
+                    suppress_error_table=True)
             self.task_manager.get_task_result(sync_write_task)
 
             node_to_upgrade = self.fetch_node_to_upgrade()
-            if node_to_upgrade is None:
-                if sync_write_task.fail.keys():
-                    self.log_failure("Failures after cluster upgrade")
-            elif len(sync_write_task.fail.keys()) != create_batch_size:
-                self.log_failure("SyncWrite succeeded with mixed mode cluster")
+            if self.atomicity:
+                self.sleep(10)
+                current_items = self.bucket_util.get_bucket_current_item_count(
+                    self.cluster, self.bucket)
+                if node_to_upgrade is None:
+                    if current_items < self.num_items+create_batch_size:
+                        self.log_failure("Failures after cluster upgrade {} {}".format(current_items, self.num_items+create_batch_size))
+                elif current_items > self.num_items :
+                    self.log_failure("SyncWrite succeeded with mixed mode cluster")
             else:
-                for doc_id, doc_result in sync_write_task.fail.items():
-                    if ClientException.FeatureNotAvailableException \
-                            not in str(doc_result["error"]):
-                        self.log_failure("Invalid exception for %s: %s"
-                                         % (doc_id, doc_result))
+                if node_to_upgrade is None:
+                    if sync_write_task.fail.keys():
+                        self.log_failure("Failures after cluster upgrade")
+                elif len(sync_write_task.fail.keys()) != create_batch_size:
+                    self.log_failure("SyncWrite succeeded with mixed mode cluster")
+                else:
+                    for doc_id, doc_result in sync_write_task.fail.items():
+                        if ClientException.FeatureNotAvailableException \
+                                not in str(doc_result["error"]):
+                            self.log_failure("Invalid exception for %s: %s"
+                                             % (doc_id, doc_result))
 
             self.cluster_util.print_cluster_stats()
 
