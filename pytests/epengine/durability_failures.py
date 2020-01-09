86536867200ae5a9f1036cf5cd5b8133b71d8a24
Ashwin <ashwintrojan+github@gmail.com>
2020-01-09 19:43:00 +0530

    Passing 'mutate' field for doc_gen in durability_failures tests
    Change-Id: I9572865bfa6e05e838350ed74c72a3d9cbeae5a8
    Reviewed-on: http://review.couchbase.org/120227
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/epengine/durability_failures.py
+++ pytests/epengine/durability_failures.py
@@ -208,7 +208,7 @@ class DurabilityFailureTests(DurabilityTestsBase):
             vbuckets=self.vbuckets, target_vbucket=target_vbuckets)
         gen_update = doc_generator(
             self.key, 0, self.crud_batch_size, vbuckets=self.vbuckets,
-            target_vbucket=target_vbuckets)
+            target_vbucket=target_vbuckets, mutate=1)
         gen_delete = doc_generator(
             self.key, 0, self.crud_batch_size, vbuckets=self.vbuckets,
             target_vbucket=target_vbuckets)
@@ -657,7 +657,7 @@ class DurabilityFailureTests(DurabilityTestsBase):
             vbuckets=self.vbuckets, target_vbucket=target_vbuckets)
         gen_update = doc_generator(
             self.key, 0, self.crud_batch_size, vbuckets=self.vbuckets,
-            target_vbucket=target_vbuckets)
+            target_vbucket=target_vbuckets, mutate=1)
         gen_delete = doc_generator(
             self.key, 0, self.crud_batch_size, vbuckets=self.vbuckets,
             target_vbucket=target_vbuckets)
