d25154679cdf007e094114893f9c7356eda54903
Anitha Kuberan <anitha.kuberan@couchbase.com>
2020-01-16 21:30:02 +0530

    Fix txn hooks failures
    Change-Id: Iec9b99e30a484bae56a84df2bb5d6319a65246d2
    Reviewed-on: http://review.couchbase.org/120653
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ritam Sharma <ritam@couchbase.com>

--- pytests/Atomicity/mock_test.py
+++ pytests/Atomicity/mock_test.py
@@ -1,26 +1,13 @@
 import time
 from basetestcase import BaseTestCase
-from couchbase_helper.tuq_generators import JsonGenerator
-from remote.remote_util import RemoteMachineShellConnection
-from couchbase_helper.documentgenerator import DocumentGenerator
-from epengine.durability_base import DurabilityTestsBase
-from __builtin__ import True
 from BucketLib.bucket import Bucket
-import com.couchbase.test.transactions.SimpleTransaction as Transaction
 import logging
-from membase.api.rest_client import RestConnection
-from sdk_client3 import SDKClient as VBucketAwareMemcached
 import threading
-from random import randint
-from error_simulation.cb_error import CouchbaseError
 from membase.api.rest_client import RestConnection
 from sdk_client3 import SDKClient as VBucketAwareMemcached
-import random
-from couchbase_helper.documentgenerator import doc_generator
 from reactor.util.function import Tuples
 import com.couchbase.test.transactions.SimpleTransaction as Transaction
 import com.couchbase.client.java.json.JsonObject as JsonObject
-import json as pyJson
 
 """
 Basic test cases with commit,rollback scenarios
@@ -30,6 +17,7 @@ class basic_ops(BaseTestCase):
     def setUp(self):
         super(basic_ops, self).setUp()
         self.test_log = logging.getLogger("test")
+        self.fail = self.input.param("fail", False)
         nodes_init = self.cluster.servers[1:self.nodes_init] if self.nodes_init != 1 else []
         self.task.rebalance([self.cluster.master], nodes_init, [])
         self.cluster.nodes_in_cluster.extend([self.cluster.master] + nodes_init)
@@ -77,35 +65,36 @@ class basic_ops(BaseTestCase):
             self.durability = 0
 
     def set_exception(self, exception):
-        self.exception = exception
-        self.fail("Got an exception {}".format(self.exception))
+        raise BaseException(exception)
 
     def test_txnwithhooks(self):
-
+        self.verify = self.input.param("verify", True)
         # transaction load
         if "Atr" in self.operation:
             exception = Transaction().MockRunTransaction(self.client.cluster, self.transaction_config, 
-                                self.client.collection, self.docs, self.transaction_commit, self.operation)
+                                self.client.collection, self.docs, self.transaction_commit, self.operation, self.fail)
 
         else:
             if "Replace" in self.operation:
                 exception = Transaction().MockRunTransaction(self.client.cluster, self.transaction_config, 
-                                self.client.collection, self.docs, self.keys, [], self.transaction_commit, self.operation, self.keys[-1]) 
+                                self.client.collection, self.docs, self.keys, [], self.transaction_commit, self.operation, self.keys[-1], self.fail) 
                 self.value = {'mutated':1, 'value':'value1'}
                 self.content = self.client.translate_to_json_object(self.value)
             else:
                 exception = Transaction().MockRunTransaction(self.client.cluster, self.transaction_config, 
-                                self.client.collection, self.docs, [], [], self.transaction_commit, self.operation, self.keys[-1]) 
+                                self.client.collection, self.docs, [], [], self.transaction_commit, self.operation, self.keys[-1], self.fail) 
 
             if "Remove" in self.operation:
                 exception = Transaction().MockRunTransaction(self.client.cluster, self.transaction_config,
-                                 self.client.collection, [], [], self.keys, self.transaction_commit, self.operation, self.keys[-1])
+                                 self.client.collection, [], [], self.keys, self.transaction_commit, self.operation, self.keys[-1], self.fail)
 
         # verify the values
         for key in self.keys:
             result = self.client.read(key)
-            if "Remove" in self.operation or self.transaction_commit == False :
+            if "Remove" in self.operation or self.transaction_commit == False or self.verify == False :
                 if result['status']:
+                    actual_val = self.client.translate_to_json_object(result['value'])
+                    self.test_log.info("actual value for key {} is {}".format(key,actual_val))
                     msg = "Key should be deleted but present in the cluster {}".format(key)
                     self.set_exception(msg)
             else:
@@ -114,8 +103,9 @@ class basic_ops(BaseTestCase):
                     self.test_log.info("actual value for key {} is {}".format(key,actual_val))
                     self.test_log.info("expected value for key {} is {}".format(key,self.content))
                     self.set_exception("actual and expected value does not match")
+        
 
-        if exception:
+        if exception and self.fail != True:
             self.set_exception(exception)
 
 
