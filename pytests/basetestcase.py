0ac55707b42d34d004bf249dbb4813f78f21d70d
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2020-02-21 15:17:03 +0530

    Adding default key name in basetestcase
    Change-Id: I2fe3350a6c4aad09177254f8f8375fe51df4cd80
    Reviewed-on: http://review.couchbase.org/122641
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <ankush.sharma@couchbase.com>

--- pytests/basetestcase.py
+++ pytests/basetestcase.py
@@ -73,6 +73,7 @@ class BaseTestCase(unittest.TestCase):
         # End of bucket parameters
 
         # Doc specific params
+        self.key = self.input.param("key", "test_docs")
         self.key_size = self.input.param("key_size", 8)
         self.doc_size = self.input.param("doc_size", 10)
         self.sub_doc_size = self.input.param("sub_doc_size", 10)
