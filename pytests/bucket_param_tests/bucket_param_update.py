f83540021d1eaf0266aac95a9e752aaf887fd5c6
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2020-04-09 14:07:59 +0530

    Pass more pramas in bucket creation for replica update tests.
    Change-Id: I48fdab044c62401f1eabae3d06d116e2309b1a42
    Reviewed-on: http://review.couchbase.org/c/TAF/+/125322
    Reviewed-by: <ankush.sharma@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- pytests/bucket_param_tests/bucket_param_update.py
+++ pytests/bucket_param_tests/bucket_param_update.py
@@ -15,8 +15,11 @@ class BucketParamTest(BaseTestCase):
         self.cluster.nodes_in_cluster.extend(
             [self.cluster.master] + nodes_init)
         self.bucket_util.create_default_bucket(
-            replica=self.num_replicas, compression_mode=self.compression_mode,
-            storage=self.bucket_storage)
+            compression_mode=self.compression_mode,
+            bucket_type=self.bucket_type,
+            replica=self.num_replicas,
+            storage=self.bucket_storage,
+            eviction_policy=self.bucket_eviction_policy)
         self.bucket_util.add_rbac_user()
         self.src_bucket = self.bucket_util.get_all_buckets()
 
