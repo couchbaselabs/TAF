3a1ca706daa5bc3c4468468fbfd4e529813e907d
sumedhpb <sumedhpb8@gmail.com>
2020-04-29 19:30:00 +0530

    Minor fixes in rebalance and failover test for collections
    -> create new bucket spec for durability level = MAJORITY_AND_PERSIST_TO_ACTIVE,
    and PERSIST_TO_MAJORITY to keep only membase buckets
    
    Change-Id: Iec1df4d9a58459ae720df8c900a077ae32038934
    Reviewed-on: http://review.couchbase.org/c/TAF/+/126927
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

---
+++ pytests/bucket_collections/bucket_templates/multi_bucket/buckets_all_membase_for_rebalance_tests.py
@@ -0,0 +1,150 @@
+from BucketLib.bucket import Bucket
+from collections_helper.collections_spec_constants import MetaConstants
+
+spec = {
+    MetaConstants.NUM_BUCKETS: 3,
+    MetaConstants.NUM_SCOPES_PER_BUCKET: 3,
+    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 2,
+    MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+    MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
+
+    Bucket.bucketType: Bucket.Type.MEMBASE,
+    Bucket.replicaNumber: Bucket.ReplicaNum.THREE,
+    Bucket.ramQuotaMB: 100,
+    Bucket.replicaIndex: 1,
+    Bucket.flushEnabled: Bucket.FlushBucket.DISABLED,
+    Bucket.priority: Bucket.Priority.LOW,
+    Bucket.conflictResolutionType: Bucket.ConflictResolution.SEQ_NO,
+    Bucket.maxTTL: 0,
+    Bucket.storageBackend: Bucket.StorageBackend.couchstore,
+    Bucket.evictionPolicy: Bucket.EvictionPolicy.VALUE_ONLY,
+    Bucket.compressionMode: Bucket.CompressionMode.PASSIVE,
+
+    "buckets": {
+        "default": {
+            MetaConstants.NUM_SCOPES_PER_BUCKET: 10,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 10,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+            Bucket.ramQuotaMB: 1500,
+            Bucket.bucketType: Bucket.Type.MEMBASE,
+            "privileges": [
+                "Perm1"
+            ],
+            "scopes": {
+                "scope1": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection_1": {
+                            "rbac": "rbac1",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        },
+                        "collections_2": {
+                            "rbac": "rbac2",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                },
+                "scope2": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                }
+            }
+        },
+        "bucket1": {
+            Bucket.bucketType: Bucket.Type.MEMBASE,
+            "privileges": [
+                "Perm2"
+            ],
+            "scopes": {
+                "scope1": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                },
+                "scope2": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                }
+            }
+        },
+        "bucket2": {
+            Bucket.bucketType: Bucket.Type.MEMBASE,
+            "privileges": [
+                "Perm3"
+            ],
+            "scopes": {
+                "scope1": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                },
+                "scope2": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            MetaConstants.NUM_ITEMS_PER_COLLECTION: 10000,
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
