c874ed93aa8454fec76fc736d1e8784c9a275714
ankushsharma29 <ankush.sharma@couchbase.com>
2020-05-11 20:49:58 +0530

    Adding more cases for drop collections Description: Deletes op with drop collections
    Change-Id: I4471d7b3f7499b056cf132742796425968c800a2
    Reviewed-on: http://review.couchbase.org/c/TAF/+/127798
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/magma/magma_basic_crud.py
+++ pytests/magma/magma_basic_crud.py
@@ -1481,3 +1481,148 @@ class BasicCrudTests(MagmaBaseTest):
         # # # # Space Amplification check ends # # # #
 
         self.log.info("====test_drop_collections_after_upserts====")
+
+    def test_drop_collections_after_deletes(self):
+        """
+        Test will check space
+        Amplification after collection drop
+        !) Create multiple collections
+        2) Load docs in all collections
+        3) Delete 3/4th of docs in one collection
+        4) Since default frag is 50, auto compaction shouldn't trigger
+        5) Drop a collection
+        6)Auto compaction shoudl trigger now, Verify space amplification
+        """
+        scope_name = CbServer.default_scope
+        collection_prefix = "FunctionCollection"
+
+        # # # # Non Default Scope creation # # # #
+        if self.num_scopes > 1:
+            scope_name = "FunctionScope"
+            self.bucket_util.create_scope(self.cluster.master,
+                                          self.buckets[0],
+                                          {"name": scope_name})
+
+        # # # # Collections Creation # # # #
+        for i in range(self.num_collections):
+            collection_name = collection_prefix + str(i)
+            self.log.info("Creating scope::collection {} {}\
+            ".format(scope_name, collection_name))
+            self.bucket_util.create_collection(
+                self.cluster.master, self.buckets[0],
+                scope_name, {"name": collection_name})
+            self.sleep(2)
+        collections = self.buckets[0].scopes[scope_name].collections.keys()
+        if self.num_collections > 1:
+            collections.remove(CbServer.default_collection)
+        self.log.info("List of collections {}".format(collections))
+
+        # # # # DOC LOADING # # # #
+        end = 0
+        init_items = self.num_items
+        tasks_info = dict()
+        self.doc_ops = "create"
+        for collection in collections:
+            start = end
+            end += init_items
+            self.gen_create = doc_generator(
+                self.key, start, end,
+                doc_size=self.doc_size,
+                doc_type=self.doc_type,
+                target_vbucket=self.target_vbucket,
+                vbuckets=self.cluster_util.vbuckets,
+                key_size=self.key_size,
+                randomize_doc_size=self.randomize_doc_size,
+                randomize_value=self.randomize_value,
+                mix_key_size=self.mix_key_size,
+                deep_copy=self.deep_copy)
+            tem_tasks_info = self.loadgen_docs(
+                self.retry_exceptions,
+                self.ignore_exceptions,
+                scope=scope_name,
+                collection=collection,
+                _sync=False)
+            tasks_info.update(tem_tasks_info.items())
+        self.num_items -= init_items
+        for task in tasks_info:
+            self.task_manager.get_task_result(task)
+        self.bucket_util.verify_doc_op_task_exceptions(
+            tasks_info, self.cluster)
+        self.bucket_util.log_doc_ops_task_failures(tasks_info)
+        self.log.info("Verifying num_items counts after doc_ops")
+        self.bucket_util._wait_for_stats_all_buckets()
+        self.bucket_util.verify_stats_all_buckets(self.num_items)
+
+        # # # # Initial Disk Usage # # # #
+        disk_usage = self.get_disk_usage(
+            self.buckets[0], self.servers)
+        self.disk_usage[self.buckets[0].name] = disk_usage[0]
+        self.log.info(
+            "For bucket {} disk usage after initial '\n' \
+            creation is {}MB".format(
+                self.buckets[0].name,
+                self.disk_usage[self.buckets[0].name]))
+
+        # Space amplification check before deletes
+        # This check is to make sure, compaction doesn't get triggerd
+        _result = self.check_fragmentation_using_magma_stats(
+            self.buckets[0], self.servers)
+        self.assertIs(_result, True,
+                      "Fragmentation value exceeds from '\n' \
+                      the configured fragementaion value")
+
+        _r = self.check_fragmentation_using_bucket_stats(
+            self.buckets[0], self.servers)
+        self.assertIs(_r, True,
+                      "Fragmentation value exceeds from '\n' \
+                      the configured fragementaion value")
+
+        # # # # Delete  3/4th docs in a single collection # # # #
+        self.doc_ops = "delete"
+        self.log.info("For deletion collection picked is {} \
+        ".format(collections[-1]))
+        self.gen_delete = doc_generator(
+            self.key, start, start + int(0.75 * (end-start)),
+            doc_size=self.doc_size,
+            doc_type=self.doc_type,
+            target_vbucket=self.target_vbucket,
+            vbuckets=self.cluster_util.vbuckets,
+            key_size=self.key_size,
+            randomize_doc_size=self.randomize_doc_size,
+            randomize_value=self.randomize_value,
+            mix_key_size=self.mix_key_size,
+            deep_copy=self.deep_copy)
+        _ = self.loadgen_docs(self.retry_exceptions,
+                              self.ignore_exceptions,
+                              scope=scope_name,
+                              collection=collections[-1],
+                              _sync=True)
+        self.log.info("Waiting for ep-queues to get drained")
+        self.bucket_util._wait_for_stats_all_buckets()
+
+        # # # # Drop a collection # # # #
+        self.log.info("Collection to be dropped {}\
+        ".format(collections[0]))
+        self.bucket_util.drop_collection(
+            self.cluster.master, self.buckets[0],
+            scope_name=scope_name,
+            collection_name=collections[0])
+        self.buckets[0].scopes[scope_name].collections.pop(collections[0])
+        collections.remove(collections[0])
+
+        # # # # Space Amplification check # # # #
+        _result = self.check_fragmentation_using_magma_stats(
+            self.buckets[0], self.servers)
+        self.assertIs(_result, True,
+                      "Fragmentation value exceeds from '\n' \
+                      the configured fragementaion value")
+
+        _r = self.check_fragmentation_using_bucket_stats(
+             self.buckets[0], self.servers)
+        self.assertIs(_r, True,
+                      "Fragmentation value exceeds from '\n' \
+                      the configured fragementaion value")
+
+        # # # # Space Amplification check ends # # # #
+
+        self.log.info("====test_drop_collections_after_deletes====")
