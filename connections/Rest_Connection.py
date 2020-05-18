a4c4bc5c0591159ba89a28a0e9d3a470ae1f4d50
umang-cb <63640118+umang-cb@users.noreply.github.com>
2020-05-18 20:47:11 +0530

    Update _create_headers method to accept username and password

--- connections/Rest_Connection.py
+++ connections/Rest_Connection.py
@@ -208,7 +208,11 @@ class RestConnection(object):
                     raise ServerUnavailableException(ip=self.ip)
             sleep(3, log_type="infra")
 
-    def _create_headers(self):
+    def _create_headers(self, username=None, password=None):
+        if username is None:
+            username = self.username
+        if password is None:
+            password = self.password
         authorization = base64.encodestring('%s:%s'
                                             % (self.username, self.password))
         return {'Content-Type': 'application/x-www-form-urlencoded',
