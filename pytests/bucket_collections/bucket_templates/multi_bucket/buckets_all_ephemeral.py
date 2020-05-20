4787887d25209ea850fb69545cc3dfee2f91712d
sumedhpb <sumedhpb8@gmail.com>
2020-05-20 14:41:10 +0530

    Fix in buckets_all_ephemeral to reduce buckets
    Change-Id: Iba3044b42ef0b9739242e272e9f9d10f82cf1ebf
    Reviewed-on: http://review.couchbase.org/c/TAF/+/128594
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/bucket_collections/bucket_templates/multi_bucket/buckets_all_ephemeral.py
+++ pytests/bucket_collections/bucket_templates/multi_bucket/buckets_all_ephemeral.py
@@ -2,7 +2,7 @@ from BucketLib.bucket import Bucket
 from collections_helper.collections_spec_constants import MetaConstants
 
 spec = {
-    MetaConstants.NUM_BUCKETS: 5,
+    MetaConstants.NUM_BUCKETS: 3,
     MetaConstants.NUM_SCOPES_PER_BUCKET: 3,
     MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 2,
     MetaConstants.NUM_ITEMS_PER_COLLECTION: 50,
