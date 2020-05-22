507a7fc6d1e225c888e5d52263108cb0f99268a1
Balakumaran G <balakumaran.gopal@couchbase.com>
2020-05-22 16:07:26 +0530

    Increase the bucket replicas in these templates as some of them are used in DiskAutoFailover tests
    Change-Id: Iaefd056402707980265a4ab9aec212e13081b062
    Reviewed-on: http://review.couchbase.org/c/TAF/+/128849
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Sumedh Basarkod <sumedhpb8@gmail.com>

--- pytests/bucket_collections/bucket_templates/single_bucket/def_scope_two_hundred_collections.py
+++ pytests/bucket_collections/bucket_templates/single_bucket/def_scope_two_hundred_collections.py
@@ -1,4 +1,6 @@
 from collections_helper.collections_spec_constants import MetaConstants
+from BucketLib.bucket import Bucket
+
 
 spec = {
     MetaConstants.NUM_BUCKETS: 1,
@@ -6,4 +8,7 @@ spec = {
     MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 200,
     MetaConstants.NUM_ITEMS_PER_COLLECTION: 10,
     MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
+
+    Bucket.replicaNumber: Bucket.ReplicaNum.TWO,
+
 }
