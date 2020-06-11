d057edec4e8f8e50815a40e600a3e00af6bb0c08
sumedhpb <sumedhpb8@gmail.com>
2020-06-11 13:57:33 +0530

    Add tests with all durability levels for rebalance/failover + collection crud
    Change-Id: I9724d5def25249bc0009977536de449c9ed3aa23
    Reviewed-on: http://review.couchbase.org/c/TAF/+/130296
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

---
+++ pytests/bucket_collections/bucket_templates/multi_bucket/buckets_all_membase_for_rebalance_tests_more_collections.py
@@ -0,0 +1,139 @@
+from BucketLib.bucket import Bucket
+from collections_helper.collections_spec_constants import MetaConstants
+
+spec = {
+    MetaConstants.NUM_BUCKETS: 3,
+    MetaConstants.NUM_SCOPES_PER_BUCKET: 5,
+    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 40,
+    MetaConstants.NUM_ITEMS_PER_COLLECTION: 5000,
+    MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
+
+    Bucket.bucketType: Bucket.Type.MEMBASE,
+    Bucket.replicaNumber: Bucket.ReplicaNum.THREE,
+    Bucket.ramQuotaMB: 200,
+    Bucket.replicaIndex: 1,
+    Bucket.flushEnabled: Bucket.FlushBucket.DISABLED,
+    Bucket.priority: Bucket.Priority.LOW,
+    Bucket.conflictResolutionType: Bucket.ConflictResolution.SEQ_NO,
+    Bucket.maxTTL: 0,
+    Bucket.storageBackend: Bucket.StorageBackend.couchstore,
+    Bucket.evictionPolicy: Bucket.EvictionPolicy.VALUE_ONLY,
+    Bucket.compressionMode: Bucket.CompressionMode.PASSIVE,
+
+    "buckets": {
+        "default": {
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 2500,
+            Bucket.ramQuotaMB: 2000,
+            Bucket.bucketType: Bucket.Type.MEMBASE,
+            "privileges": [
+                "Perm1"
+            ],
+            "scopes": {
+                "scope1": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection_1": {
+                            "rbac": "rbac1",
+                            Bucket.maxTTL: 0
+                        },
+                        "collections_2": {
+                            "rbac": "rbac2",
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                },
+                "scope2": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                }
+            }
+        },
+        "bucket1": {
+            Bucket.bucketType: Bucket.Type.MEMBASE,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 15,
+            "privileges": [
+                "Perm2"
+            ],
+            "scopes": {
+                "scope1": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                },
+                "scope2": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                }
+            }
+        },
+        "bucket2": {
+            Bucket.bucketType: Bucket.Type.MEMBASE,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 15,
+            "privileges": [
+                "Perm3"
+            ],
+            "scopes": {
+                "scope1": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                },
+                "scope2": {
+                    "privileges": [
+                        "Perm1"
+                    ],
+                    "collections": {
+                        "collection1": {
+                            "rbac": "rbac1",
+                            Bucket.maxTTL: 0
+                        },
+                        "collection2": {
+                            "rbac": "rbac2",
+                            Bucket.maxTTL: 0
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
