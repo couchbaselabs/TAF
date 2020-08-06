bef51b36ee48609ac90eeaa7486a94e7063868ce
Ashwin <ashwintrojan+github@gmail.com>
2020-08-06 15:02:59 +0530

    Aligning AutoFailoverBase with CollectionBase for over_ride_spec params
    Follow up of commit:
     f948bf59be6ef0d71de9801e009e521350128a7b
    
    Change-Id: I4b6b9ee4039635745e251e469d4a46698cc0b8d4
    Reviewed-on: http://review.couchbase.org/c/TAF/+/133884
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/failover/AutoFailoverBaseTest.py
+++ pytests/failover/AutoFailoverBaseTest.py
@@ -129,8 +129,8 @@ class AutoFailoverBaseTest(BaseTestCase):
             self.bucket_util.get_crud_template_from_package("initial_load")
 
         # Process params to over_ride values if required
-        self.over_ride_template_params(buckets_spec)
-        self.over_ride_template_params(doc_loading_spec)
+        self.over_ride_bucket_template_params(buckets_spec)
+        self.over_ride_doc_loading_template_params(doc_loading_spec)
 
         # MB-38438, adding CollectionNotFoundException in retry exception
         doc_loading_spec[MetaCrudParams.RETRY_EXCEPTIONS].append(
@@ -175,17 +175,25 @@ class AutoFailoverBaseTest(BaseTestCase):
         self.bucket_util.print_bucket_stats()
         self.bucket_helper_obj = BucketHelper(self.cluster.master)
 
-    def over_ride_template_params(self, target_spec):
+    def over_ride_bucket_template_params(self, bucket_spec):
         for over_ride_param in self.over_ride_spec_params:
-            if over_ride_param == "num_items":
-                target_spec[MetaConstants.NUM_ITEMS_PER_COLLECTION] = \
+            if over_ride_param == "replicas":
+                bucket_spec[Bucket.replicaNumber] = self.num_replicas
+            elif over_ride_param == "bucket_size":
+                bucket_spec[Bucket.ramQuotaMB] = self.bucket_size
+            elif over_ride_param == "num_items":
+                bucket_spec[MetaConstants.NUM_ITEMS_PER_COLLECTION] = \
                     self.num_items
-            elif over_ride_param == "durability":
+
+    def over_ride_doc_loading_template_params(self, target_spec):
+        for over_ride_param in self.over_ride_spec_params:
+            if over_ride_param == "durability":
                 target_spec[MetaCrudParams.DURABILITY_LEVEL] = \
                     self.durability_level
-            elif over_ride_param == "replicas":
-                target_spec[Bucket.replicaNumber] = \
-                    self.num_replicas
+            elif over_ride_param == "sdk_timeout":
+                target_spec[MetaCrudParams.SDK_TIMEOUT] = self.sdk_timeout
+            elif over_ride_param == "doc_size":
+                target_spec[MetaCrudParams.DocCrud.DOC_SIZE] = self.doc_size
 
     def bareSetUp(self):
         super(AutoFailoverBaseTest, self).setUp()
