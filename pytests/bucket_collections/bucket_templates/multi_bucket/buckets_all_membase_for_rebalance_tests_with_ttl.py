0d6de3e499e38ab8445e549f596de5b10c2e87a7
sumedhpb <sumedhpb8@gmail.com>
2020-08-19 14:21:03 +0530

    Modify TTL buckets for rebalance and failover
    Change-Id: I4730822d994f317af744a498562ca3f6da23f525
    Reviewed-on: http://review.couchbase.org/c/TAF/+/134613
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/bucket_collections/bucket_templates/multi_bucket/buckets_all_membase_for_rebalance_tests_with_ttl.py
+++ pytests/bucket_collections/bucket_templates/multi_bucket/buckets_all_membase_for_rebalance_tests_with_ttl.py
@@ -1,7 +1,6 @@
 from BucketLib.bucket import Bucket
 from collections_helper.collections_spec_constants import MetaConstants
 
-
 spec = {
     MetaConstants.NUM_BUCKETS: 3,
     MetaConstants.NUM_SCOPES_PER_BUCKET: 2,
@@ -11,20 +10,20 @@ spec = {
 
     Bucket.bucketType: Bucket.Type.MEMBASE,
     Bucket.replicaNumber: Bucket.ReplicaNum.TWO,
-    Bucket.ramQuotaMB: 100,
+    Bucket.ramQuotaMB: 300,
     Bucket.replicaIndex: 1,
     Bucket.flushEnabled: Bucket.FlushBucket.DISABLED,
     Bucket.priority: Bucket.Priority.LOW,
     Bucket.conflictResolutionType: Bucket.ConflictResolution.SEQ_NO,
-    Bucket.maxTTL: 100,
+    Bucket.maxTTL: 350,
     Bucket.storageBackend: Bucket.StorageBackend.couchstore,
-    Bucket.evictionPolicy: Bucket.EvictionPolicy.VALUE_ONLY,
-    Bucket.compressionMode: Bucket.CompressionMode.PASSIVE,
+    Bucket.evictionPolicy: Bucket.EvictionPolicy.FULL_EVICTION,
+    Bucket.compressionMode: Bucket.CompressionMode.ACTIVE,
 
     "buckets": {
         "default": {
-            MetaConstants.NUM_SCOPES_PER_BUCKET: 2,
-            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 2,
+            MetaConstants.NUM_SCOPES_PER_BUCKET: 5,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 10,
             MetaConstants.NUM_ITEMS_PER_COLLECTION: 0,
             Bucket.ramQuotaMB: 1500,
             Bucket.bucketType: Bucket.Type.MEMBASE,
@@ -40,12 +39,12 @@ spec = {
                         "collection_1": {
                             "rbac": "rbac1",
                             MetaConstants.NUM_ITEMS_PER_COLLECTION: 50000,
-                            Bucket.maxTTL: 50
+                            Bucket.maxTTL: 300
                         },
                         "collections_2": {
                             "rbac": "rbac2",
                             MetaConstants.NUM_ITEMS_PER_COLLECTION: 50000,
-                            Bucket.maxTTL: 50
+                            Bucket.maxTTL: 300
                         }
                     }
                 },
--- pytests/bucket_collections/bucket_templates/multi_bucket/buckets_for_rebalance_tests_with_ttl.py
+++ pytests/bucket_collections/bucket_templates/multi_bucket/buckets_for_rebalance_tests_with_ttl.py
@@ -9,21 +9,21 @@ spec = {
     MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
 
     Bucket.bucketType: Bucket.Type.MEMBASE,
-    Bucket.replicaNumber: Bucket.ReplicaNum.THREE,
-    Bucket.ramQuotaMB: 100,
+    Bucket.replicaNumber: Bucket.ReplicaNum.TWO,
+    Bucket.ramQuotaMB: 300,
     Bucket.replicaIndex: 1,
     Bucket.flushEnabled: Bucket.FlushBucket.DISABLED,
     Bucket.priority: Bucket.Priority.LOW,
     Bucket.conflictResolutionType: Bucket.ConflictResolution.SEQ_NO,
-    Bucket.maxTTL: 100,
+    Bucket.maxTTL: 350,
     Bucket.storageBackend: Bucket.StorageBackend.couchstore,
-    Bucket.evictionPolicy: Bucket.EvictionPolicy.VALUE_ONLY,
-    Bucket.compressionMode: Bucket.CompressionMode.PASSIVE,
+    Bucket.evictionPolicy: Bucket.EvictionPolicy.FULL_EVICTION,
+    Bucket.compressionMode: Bucket.CompressionMode.ACTIVE,
 
     "buckets": {
         "default": {
-            MetaConstants.NUM_SCOPES_PER_BUCKET: 2,
-            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 2,
+            MetaConstants.NUM_SCOPES_PER_BUCKET: 5,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 10,
             MetaConstants.NUM_ITEMS_PER_COLLECTION: 0,
             Bucket.ramQuotaMB: 1500,
             Bucket.bucketType: Bucket.Type.MEMBASE,
@@ -39,12 +39,12 @@ spec = {
                         "collection_1": {
                             "rbac": "rbac1",
                             MetaConstants.NUM_ITEMS_PER_COLLECTION: 50000,
-                            Bucket.maxTTL: 50
+                            Bucket.maxTTL: 300
                         },
                         "collections_2": {
                             "rbac": "rbac2",
                             MetaConstants.NUM_ITEMS_PER_COLLECTION: 50000,
-                            Bucket.maxTTL: 50
+                            Bucket.maxTTL: 300
                         }
                     }
                 },
