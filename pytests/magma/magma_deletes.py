c3865ed38b979427cc68fb48bfb785fec7637611
ankushsharma29 <ankush.sharma@couchbase.com>
2020-09-23 09:11:29 +0530

    Fixing parallel creates and delete test
    Change-Id: Ic0b68c1cbef76e8ecdae9b25b833287ec0936ba9
    Reviewed-on: http://review.couchbase.org/c/TAF/+/136754
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>

--- pytests/magma/magma_deletes.py
+++ pytests/magma/magma_deletes.py
@@ -89,53 +89,53 @@ class BasicDeleteTests(BasicCrudTests):
             ######################################################################
         self.log.info("====test_basic_create_delete ends====")
 
+
     def test_parallel_creates_deletes(self):
         """
-        Test Focus: Primary focus for this is to check space
-                    Amplification.
-                    Create new docs and deletes already created docs
-                    Check disk_usage after each Iteration
+        STEPS:
+          -- Create new items and deletes already
+             existing items
+          -- Check disk_usage after each Iteration
+
         """
-        self.log.info("====test_parallel_creates_deletes starts====")
-        init_items = self.num_items
-
-        self.create_end = self.num_items
-        self.delete_end = self.num_items
-
-        self.doc_ops = "create:delete"
-
+        self.log.info("test_parallel_create_delete starts")
         count = 0
+        init_items = copy.deepcopy(self.num_items)
+        self.doc_ops = "create:delete"
+        self.delete_start = 0
+        self.delete_end = self.num_items
+        if self.rev_del:
+            self.delete_start = -int(self.num_items -1)
+            self.delete_end = 1
+
         while count < self.test_itr:
             self.log.info("Iteration {}".format(count+1))
+            self.create_start = (count+1) * init_items
+            self.create_end = self.create_start + init_items
+
             if self.rev_write:
-                self.create_start = -int(self.create_end + init_items - 1)
-                self.create_end = -int(self.create_end - 1)
-            else:
-                self.create_start = self.create_end
-                self.create_end += init_items
+                self.create_end =  -int(((count+1) * init_items) - 1)
+                self.create_start = -int(-(self.create_end) + init_items)
+            self.log.info("Iteration={}, del_s={}, del_e={},create_s={},create_e={}".
+                          format(count+1, self.delete_start, self.delete_end,
+                                 self.create_start,self.create_end))
 
-            if self.rev_del:
-                self.delete_start = -int(self.delete_end + init_items - 1)
-                self.delete_end = -int(self.delete_end - 1)
-            else:
-                self.delete_start = self.delete_end
-                self.delete_end += init_items
-
-            self.generate_docs(doc_ops="create")
-
+            self.generate_docs()
             _ = self.loadgen_docs(self.retry_exceptions,
                                   self.ignore_exceptions,
                                   _sync=True)
-
             self.bucket_util._wait_for_stats_all_buckets()
             self.bucket_util.verify_stats_all_buckets(self.num_items)
 
-            self.generate_docs(doc_ops="delete")
-
-            if self.rev_write:
-                self.create_end = -(self.create_start-1)
+            self.delete_start = (count+1) * init_items
+            self.delete_end = self.delete_start + init_items
             if self.rev_del:
-                self.delete_end = -(self.delete_start-1)
+                self.delete_end = -int(((count+1) * init_items) - 1)
+                self.delete_start = -int(-(self.delete_end) + init_items)
+
+            disk_usage = self.get_disk_usage(
+                self.buckets[0],
+                self.cluster.nodes_in_cluster)
 
             # Space Amplification Check
             msg_stats = "{} stats fragmentation exceeds configured value"
@@ -147,6 +147,22 @@ class BasicDeleteTests(BasicCrudTests):
                 self.buckets[0], self.cluster.nodes_in_cluster)
             self.assertIs(_r, True, msg_stats.format("KV"))
 
+            time_end = time.time() + 60 * 2
+            while time.time() < time_end:
+                disk_usage = self.get_disk_usage(self.buckets[0],
+                                                 self.cluster.nodes_in_cluster)
+                _res = disk_usage[0]
+                self.log.info("Iteration-{}, Disk Usage at time {} is {}MB \
+                ".format(count+1, time_end - time.time(), _res))
+                if _res < 1.7 * self.disk_usage[self.disk_usage.keys()[0]]:
+                    break
+
+            msg = "Disk Usage={}MB > {} * init_Usage={}MB"
+            self.assertIs(_res > 1.7 * self.disk_usage[
+                self.disk_usage.keys()[0]], False,
+                msg.format(disk_usage[0], 0.5,
+                           self.disk_usage[self.disk_usage.keys()[0]]))
+
             disk_usage = self.get_disk_usage(
                 self.buckets[0],
                 self.cluster.nodes_in_cluster)
@@ -157,38 +173,11 @@ class BasicDeleteTests(BasicCrudTests):
                     msg.format(count+1, disk_usage[3], disk_usage[2]))
             else:
                 msg = "Iteration={}, Disk Usage={}MB > {} * init_Usage={}MB"
-                self.assertIs(disk_usage[0] > 2.5 * self.disk_usage[
+                self.assertIs(disk_usage[0] > 1.7 * self.disk_usage[
                     self.disk_usage.keys()[0]],
-                    False, msg.format(count+1, disk_usage[0], 2.5,
+                    False, msg.format(count+1, disk_usage[0], 1.7,
                                       self.disk_usage[self.disk_usage.keys()[0]]))
             #Space Amplifacation check Ends
             count += 1
-
-        self.doc_ops = "delete"
-        self.gen_delete = copy.deepcopy(self.gen_create)
-        _ = self.loadgen_docs(self.retry_exceptions,
-                              self.ignore_exceptions,
-                              _sync=True)
-
-        #Space Amplifaction check after all deletes
-        _result = self.check_fragmentation_using_magma_stats(
-            self.buckets[0], self.cluster.nodes_in_cluster)
-        self.assertIs(_result, True, msg_stats.format("magma"))
-
-        _r = self.check_fragmentation_using_bucket_stats(
-            self.buckets[0], self.cluster.nodes_in_cluster)
-        self.assertIs(_r, True, msg_stats.format("KV"))
-
-        disk_usage = self.get_disk_usage(
-                self.buckets[0],
-                self.cluster.nodes_in_cluster)
-        msg = "Disk Usage={}MB > {} * init_Usage={}MB"
-        self.assertIs(disk_usage[0] > 0.4 * self.disk_usage[
-            self.disk_usage.keys()[0]], False,
-            msg.format(disk_usage[0], 0.4,
-                       self.disk_usage[self.disk_usage.keys()[0]]))
-        # Space Amplifiaction check ends
-
-        self.change_swap_space(self.cluster.nodes_in_cluster,
-                               disable=False)
-        self.log.info("====test_parallel_create_delete ends====")
+        self.change_swap_space(self.cluster.nodes_in_cluster, disable=False)
+        self.log.info("====test_parallel_create_deletes ends====")
