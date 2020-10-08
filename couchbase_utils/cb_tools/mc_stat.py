ebd7b4a1810db85cb80143df3a2d18e8de77f8fb
Ashwin <ashwintrojan+github@gmail.com>
2020-10-08 14:27:24 +0530

    Adding test case for MB-41510
    Change-Id: I77495ea085324c16fa49d27904bd33f28e5f5304
    Reviewed-on: http://review.couchbase.org/c/TAF/+/137634
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

---
+++ couchbase_utils/cb_tools/mc_stat.py
@@ -0,0 +1,21 @@
+from cb_tools.cb_tools_base import CbCmdBase
+
+
+class McStat(CbCmdBase):
+    def __init__(self, shell_conn, username="Administrator",
+                 password="password"):
+        CbCmdBase.__init__(self, shell_conn, "mcstat",
+                           username=username, password=password)
+
+    def reset(self, bucket_name):
+        """
+        Resets mcstat for the specified bucket_name
+        :param bucket_name: Bucket name to reset stat
+        """
+        cmd = "%s -u %s -P %s -b %s reset" % (self.cbstatCmd,
+                                              self.username,
+                                              self.password,
+                                              bucket_name)
+        _, error = self._execute_cmd(cmd)
+        if error:
+            raise Exception("".join(error))
