5b4c137e0c9a68485b9b81fe8019e66a6d699053
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2020-10-13 13:05:27 +0530

    Adding support for firewall, swapiness, thp.
    Change-Id: I4a6b036ce5ec758bbff931e9353f949c0dcb29b9
    Reviewed-on: http://review.couchbase.org/c/TAF/+/138031
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- scripts/ServerPoolMachinesHealth.py
+++ scripts/ServerPoolMachinesHealth.py
@@ -1,123 +1,16 @@
 from com.jcraft.jsch import JSchException
 from com.jcraft.jsch import JSch
 from org.python.core.util import FileUtil
-
-servers = [
-    "172.23.105.191",
-    "172.23.105.192",
-    "172.23.105.224",
-    "172.23.105.231",
-    "172.23.106.120",
-    "172.23.106.122",
-    "172.23.106.125",
-    "172.23.106.155",
-    "172.23.106.156",
-    "172.23.106.159",
-    "172.23.106.163",
-    "172.23.106.164",
-    "172.23.106.165",
-    "172.23.106.168",
-    "172.23.106.177",
-    "172.23.106.196",
-    "172.23.106.198",
-    "172.23.106.199",
-    "172.23.106.204",
-    "172.23.106.211",
-    "172.23.106.214",
-    "172.23.106.215",
-    "172.23.106.216",
-    "172.23.106.217",
-    "172.23.106.218",
-    "172.23.106.219",
-    "172.23.106.220",
-    "172.23.106.222",
-    "172.23.106.53",
-    "172.23.106.56",
-    "172.23.106.60",
-    "172.23.106.62",
-    "172.23.106.63",
-    "172.23.106.69",
-    "172.23.106.71",
-    "172.23.106.87",
-    "172.23.106.89",
-    "172.23.106.92",
-    "172.23.106.95",
-    "172.23.106.99",
-    "172.23.120.119",
-    "172.23.120.120",
-    "172.23.120.122",
-    "172.23.120.123",
-    "172.23.120.124",
-    "172.23.120.125",
-    "172.23.120.126",
-    "172.23.120.127",
-    "172.23.120.128",
-    "172.23.120.129",
-    "172.23.120.197",
-    "172.23.120.201",
-    "172.23.120.215",
-    "172.23.120.222",
-    "172.23.120.229",
-    "172.23.120.230",
-    "172.23.120.233",
-    "172.23.120.237",
-    "172.23.120.241",
-    "172.23.120.249",
-    "172.23.121.76",
-    "172.23.121.79",
-    "172.23.122.208",
-    "172.23.122.221",
-    "172.23.122.223",
-    "172.23.122.224",
-    "172.23.122.225",
-    "172.23.122.226",
-    "172.23.122.227",
-    "172.23.122.228",
-    "172.23.122.229",
-    "172.23.122.231",
-    "172.23.122.240",
-    "172.23.122.241",
-    "172.23.122.242",
-    "172.23.122.243",
-    "172.23.122.245",
-    "172.23.122.246",
-    "172.23.122.247",
-    "172.23.122.248",
-    "172.23.122.249",
-    "172.23.123.10",
-    "172.23.123.11",
-    "172.23.123.12",
-    "172.23.123.4",
-    "172.23.123.5",
-    "172.23.123.6",
-    "172.23.123.7",
-    "172.23.123.8",
-    "172.23.123.9"
-    ]
-servers += [
-    "172.23.120.61",
-    "172.23.120.62",
-    "172.23.120.63",
-    "172.23.120.64",
-    "172.23.120.65",
-    "172.23.120.66",
-    "172.23.120.67",
-    "172.23.120.68",
-    "172.23.120.69",
-    "172.23.120.70"
-    ]
-
-servers += [
-    "172.23.105.220",
-    "172.23.105.221",
-    "172.23.105.223",
-    "172.23.105.225",
-    "172.23.105.226",
-    "172.23.105.227",
-    "172.23.105.208"]
-
+from java.time import Duration
+from com.couchbase.client.java import Cluster, ClusterOptions
+from com.couchbase.client.java.env import ClusterEnvironment
+from com.couchbase.client.core.env import TimeoutConfig, IoConfig
 failed = []
 
+
+swapiness_cmd = "echo 0 > /proc/sys/vm/swappiness;echo \"net.ipv4.conf.all.arp_notify = 1\" > /etc/sysctl.conf;echo \"#Set swappiness to 0 to avoid swapping\" >> /etc/sysctl.conf;echo vm.swappiness = 0 >> /etc/sysctl.conf"
+thp_cmd = "echo never >  /sys/kernel/mm/transparent_hugepage/enabled"
+disable_firewall = "systemctl stop firewalld; systemctl disable firewalld; ls -l /data"
 
 def run(command, server):
     output = []
@@ -155,34 +48,33 @@ def run(command, server):
     return output, error
 
 
-def stop_firewall(nodes=None):
-    global servers
-    print "#######################"
-    if nodes:
-        servers = nodes
-    for server in servers:
-        print server
-        output, error = run('systemctl stop firewalld; systemctl disable firewalld; ls -l /data', server)
+def execute(cmd="free -m"):
+    cluster_env = ClusterEnvironment.builder().ioConfig(IoConfig.numKvConnections(25)).timeoutConfig(TimeoutConfig.builder().connectTimeout(Duration.ofSeconds(20)).kvTimeout(Duration.ofSeconds(10)))
+    cluster_options = ClusterOptions.clusterOptions("Administrator", "esabhcuoc").environment(cluster_env.build())
+    cluster = Cluster.connect("172.23.104.162", cluster_options)
+    STATEMENT = "select meta().id from `QE-server-pool` where os='centos' and '12hrreg' in poolId or 'regression' in poolId or 'magmareg' in poolId;"
+    result = cluster.query(STATEMENT);
+
+    count = 1
+    for server in result.rowsAsObject():
+        server = server.get("id")
+        print "--+--+--+--+-- {}. SERVER: {} --+--+--+--+--".format(count, server)
+        count += 1
+        output, error = run(cmd, server)
         if output:
             print output
         if error:
             print error
-
-def iptables(nodes=None):
-    global servers
-    print "#######################"
-    if nodes:
-        servers = nodes
-    for server in servers:
-        print server
-        output, error = run("iptables -F", server)
-        if output:
-            print output
-        if error:
-            print error
-
-
-stop_firewall()
+try:
+    param = sys.argv[1]
+    if "thp" in param.lower():
+        execute(thp_cmd)
+    if "firewall" in param.lower():
+        execute(thp_cmd)
+    if "swapiness" in param.lower():
+        execute(thp_cmd)
+except:
+    execute()
 
 if failed:
     for server in failed:
