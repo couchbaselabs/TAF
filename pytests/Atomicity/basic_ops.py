429df611c08fd55d3f8d00c016de173c6e908d62
Ashwin <ashwintrojan+github@gmail.com>
2020-10-20 18:40:10 +0530

    Adding functional test for MB-41944
    Change-Id: I38a9be2d454cab1557bf85cbebeb57759855f372
    Reviewed-on: http://review.couchbase.org/c/TAF/+/138529
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Ashwin <ashwintrojan+github@gmail.com>
    Reviewed-by: Paolo Cocchi <paolo.cocchi@couchbase.com>
    Reviewed-by: <anitha.kuberan@couchbase.com>

--- pytests/Atomicity/basic_ops.py
+++ pytests/Atomicity/basic_ops.py
@@ -1,7 +1,10 @@
+from Cb_constants import DocLoading
 from basetestcase import BaseTestCase
+from couchbase_helper.documentgenerator import DocumentGenerator, doc_generator
 from couchbase_helper.tuq_generators import JsonGenerator
 from remote.remote_util import RemoteMachineShellConnection
-from couchbase_helper.documentgenerator import DocumentGenerator
+from sdk_client3 import SDKClient
+
 from com.couchbase.client.java.json import JsonObject
 
 """
@@ -12,9 +15,17 @@ Basic test cases with commit,rollback scenarios
 class basic_ops(BaseTestCase):
     def setUp(self):
         super(basic_ops, self).setUp()
+
+        services = None
+        if self.services_init:
+            services = list()
+            for service in self.services_init.split("-"):
+                services.append(service.replace(":", ","))
+
         nodes_init = self.cluster.servers[1:self.nodes_init] \
             if self.nodes_init != 1 else []
-        self.task.rebalance([self.cluster.master], nodes_init, [])
+        self.task.rebalance([self.cluster.master], nodes_init, [],
+                            services=services)
         self.cluster.nodes_in_cluster.extend([self.cluster.master]+nodes_init)
         self.bucket_util.add_rbac_user()
 
@@ -146,3 +157,72 @@ class basic_ops(BaseTestCase):
 
         self.log.info("going to execute the task")
         self.task.jython_task_manager.get_task_result(task)
+
+    def test_MB_41944(self):
+        num_index = self.input.param("num_index", 1)
+        # Create doc_gen for loading
+        doc_gen = doc_generator(self.key, 0, self.num_items)
+
+        # Get key for delete op and reset the gen
+        key, v = doc_gen.next()
+        doc_gen.reset()
+
+        # Open SDK client connection
+        client = SDKClient([self.cluster.master], self.bucket_util.buckets[0])
+
+        query = list()
+        query.append("CREATE PRIMARY INDEX index_0 on %s USING GSI"
+                     % self.bucket_util.buckets[0].name)
+        if num_index == 2:
+            query.append("CREATE INDEX index_1 on %s(name,age) "
+                         "WHERE mutated=0 USING GSI"
+                         % self.bucket_util.buckets[0].name)
+        # Create primary index on the bucket
+        for q in query:
+            client.cluster.query(q)
+        # Wait for index to become online`
+        for index, _ in enumerate(query):
+            query = "SELECT state FROM system:indexes WHERE name='index_%s'" \
+                    % index
+            index = 0
+            state = None
+            while index < 30:
+                state = client.cluster.query(query) \
+                    .rowsAsObject()[0].get("state")
+                if state == "online":
+                    break
+                self.sleep(1)
+
+            if state != "online":
+                self.log_failure("Index 'index_%s' not yet online" % index)
+
+        # Start transaction to create the doc
+        trans_task = self.task.async_load_gen_docs_atomicity(
+            self.cluster, self.bucket_util.buckets,
+            doc_gen, DocLoading.Bucket.DocOps.CREATE)
+        self.task_manager.get_task_result(trans_task)
+
+        # Perform sub_doc operation on same key
+        _, fail = client.crud(DocLoading.Bucket.SubDocOps.INSERT,
+                              key=key, value=["_sysxattr", "sysxattr-payload"],
+                              xattr=True)
+        if fail:
+            self.log_failure("Subdoc insert failed: %s" % fail)
+        else:
+            self.log.info("Subdoc insert success")
+
+        # Delete the created doc
+        result = client.crud(DocLoading.Bucket.DocOps.DELETE, key)
+        if result["status"] is False:
+            self.log_failure("Doc delete failed: %s" % result["error"])
+        else:
+            self.log.info("Document deleted")
+            # Re-insert same doc through transaction
+            trans_task = self.task.async_load_gen_docs_atomicity(
+                self.cluster, self.bucket_util.buckets,
+                doc_gen, DocLoading.Bucket.DocOps.CREATE)
+            self.task_manager.get_task_result(trans_task)
+
+        # Close SDK Client connection
+        client.close()
+        self.validate_test_failure()
