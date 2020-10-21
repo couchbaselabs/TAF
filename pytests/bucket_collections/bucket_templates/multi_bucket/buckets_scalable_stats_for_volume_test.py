98c6712ef27aa4177e788e6846e3413e88cdf44d
sumedhpb <sumedhpb8@gmail.com>
2020-10-21 14:12:50 +0530

    CBQE-6165: 1/n Add indexing + n1ql support to volume test
    This adds support to create user defined number of secondary indexes on
    collections after inital setup in the volume test. It creates equal
    number of queries that are run during the entire test (during rebalance
    /failover ops).
    
    Also creating a new bucket spec for scalable stats. The goal for
    scalable stats volume test is to generate high number of stats with the
    help of large number of collections/scopes and indexes with less docs,
    as number of docs don't significantly affect the number of stats
    entities. So this bucket spec will have lesser docs but 1000
    collections and 1000's of indexes.
    
    Change-Id: I49b77db9cb0823ad3f5f6d3c462e39505dc12ced
    Reviewed-on: http://review.couchbase.org/c/TAF/+/138626
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ashwin <ashwintrojan+github@gmail.com>

---
+++ pytests/bucket_collections/bucket_templates/multi_bucket/buckets_scalable_stats_for_volume_test.py
@@ -0,0 +1,23 @@
+from BucketLib.bucket import Bucket
+from collections_helper.collections_spec_constants import MetaConstants
+
+spec = {
+    MetaConstants.NUM_BUCKETS: 2,
+    MetaConstants.NUM_SCOPES_PER_BUCKET: 500,
+    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 1,
+    MetaConstants.NUM_ITEMS_PER_COLLECTION: 1,
+    MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
+    MetaConstants.CREATE_COLLECTIONS_USING_MANIFEST_IMPORT: True,
+
+    Bucket.bucketType: Bucket.Type.MEMBASE,
+    Bucket.replicaNumber: Bucket.ReplicaNum.TWO,
+    Bucket.ramQuotaMB: 100,
+    Bucket.replicaIndex: 1,
+    Bucket.flushEnabled: Bucket.FlushBucket.DISABLED,
+    Bucket.priority: Bucket.Priority.LOW,
+    Bucket.conflictResolutionType: Bucket.ConflictResolution.SEQ_NO,
+    Bucket.maxTTL: 0,
+    Bucket.storageBackend: Bucket.StorageBackend.couchstore,
+    Bucket.evictionPolicy: Bucket.EvictionPolicy.FULL_EVICTION,
+    Bucket.compressionMode: Bucket.CompressionMode.ACTIVE,
+}
--- pytests/volumetests/Collections.py
+++ pytests/volumetests/Collections.py
@@ -1,3 +1,4 @@
+import threading
 import time
 import urllib
 
@@ -25,19 +26,46 @@ class volume(CollectionBase):
         self.rest = RestConnection(self.servers[0])
         self.available_servers = list()
         self.available_servers = self.cluster.servers[self.nodes_init:]
+        self.exclude_nodes = [self.cluster.master]
         self.iterations = self.input.param("iterations", 2)
         self.vbucket_check = self.input.param("vbucket_check", True)
         self.data_load_spec = self.input.param("data_load_spec", "volume_test_load_for_volume_test")
         self.contains_ephemeral = self.input.param("contains_ephemeral", True)
+
         # the stage at which CRUD for collection level/ document level take place.
         # "before" - start and finish before rebalance/failover starts at each step
         # "during" - during rebalance/failover at each step
         self.data_load_stage = self.input.param("data_load_stage", "during")
+
         self.doc_and_collection_ttl = self.input.param("doc_and_collection_ttl", False)  # For using doc_ttl + coll_ttl
         self.skip_validations = self.input.param("skip_validations", True)
 
+        # Services to be added on rebalance-in nodes during the volume test
+        self.services_for_rebalance_in = self.input.param("services_for_rebalance_in", None)
+
+        # Initialize parameters for index querying
+        self.n1ql_node = None
+        self.number_of_indexes = self.input.param("number_of_indexes", 0)
+        if self.number_of_indexes > 0:
+            self.set_memory_quota_kv_index()
+            self.n1ql_node = self.cluster_util.get_nodes_from_services_map(service_type="n1ql",
+                                                                           get_all_nodes=False,
+                                                                           servers=self.cluster.servers[
+                                                                                   :self.nodes_init],
+                                                                           master=self.cluster.master)
+            self.n1ql_rest = RestConnection(self.n1ql_node)
+            self.exclude_nodes.append(self.n1ql_node)
+            indexes_to_build = self.create_indexes_and_initialize_queries()
+            self.build_deferred_indexes(indexes_to_build)
+        self.query_thread_flag = False
+        self.query_thread = None
+
     def tearDown(self):
         # Do not call the base class's teardown, as we want to keep the cluster intact after the volume run
+        if self.query_thread:
+            self.query_thread_flag = False
+            self.query_thread.join()
+            self.query_thread = None
         self.log.info("Printing bucket stats before teardown")
         self.bucket_util.print_bucket_stats()
         if self.collect_pcaps:
@@ -48,6 +76,108 @@ class volume(CollectionBase):
             self.assertFalse(result, msg=core_msg + stream_msg + asan_msg)
         if self.crash_warning and result:
             self.log.warn(core_msg + stream_msg + asan_msg)
+
+    def set_memory_quota_kv_index(self):
+        """
+        To set memory quota of Kv and index services before starting step 5 of volume test
+        """
+        if self.number_of_indexes == 0:
+            return
+        else:
+            self.rest.set_service_memoryQuota(service="memoryQuota", memoryQuota=18000)
+            self.rest.set_service_memoryQuota(service="indexMemoryQuota", memoryQuota=3000)
+
+    def run_cbq_query(self, query):
+        result = self.n1ql_rest.query_tool(query, timeout=1300)
+        return result
+
+    def wait_for_indexes_to_go_online(self, gsi_index_names, timeout=300):
+        """
+        Wait for indexes to go online after building the deferred indexes
+        """
+        self.log.info("Waiting for indexes to go online")
+        start_time = time.time()
+        stop_time = start_time + timeout
+        for gsi_index_name in gsi_index_names:
+            while True:
+                check_state_query = "SELECT state FROM system:indexes WHERE name='%s'" % gsi_index_name
+                result = self.run_cbq_query(check_state_query)
+                if result['results'][0]['state'] == "online":
+                    break
+                if time.time() > stop_time:
+                    self.fail("Index availability timeout of index: {0}".format(gsi_index_name))
+
+    def build_deferred_indexes(self, indexes_to_build):
+        """
+        Build secondary indexes that were deferred
+        """
+        self.log.info("Building indexes")
+        for bucket, bucket_data in indexes_to_build.items():
+            for scope, collection_data in bucket_data.items():
+                for collection, gsi_index_names in collection_data.items():
+                    build_query = "BUILD INDEX on `%s`.`%s`.`%s`(`%s`) " \
+                                  "USING GSI" \
+                                  % (bucket, scope, collection, ",".join(gsi_index_names))
+                    result = self.run_cbq_query(build_query)
+                    self.assertTrue(result['status'] == "success", "Build query %s failed." % build_query)
+                    self.wait_for_indexes_to_go_online(gsi_index_names)
+
+        query = "select state from system:indexes where state='deferred'"
+        result = self.run_cbq_query(query)
+        self.log.info("deferred indexes remaining: {0}".format(len(result['results'])))
+        query = "select state from system:indexes where state='online'"
+        result = self.run_cbq_query(query)
+        self.log.info("online indexes count: {0}".format(len(result['results'])))
+
+    def create_indexes_and_initialize_queries(self):
+        """
+        Create gsi indexes on collections - according to number_of_indexes, and
+        Initialize select queries on collections that will be run later
+        """
+        self.log.info("Creating indexes with defer build")
+        self.select_queries = list()
+        indexes_to_build = dict()
+        count = 0
+        # ToDO create indexes on ephemeral buckets too using MOI storage
+        couchbase_buckets = [bucket for bucket in self.bucket_util.buckets if bucket.bucketType == "couchbase"]
+        for bucket in couchbase_buckets:
+            indexes_to_build[bucket.name] = dict()
+            for _, scope in bucket.scopes.items():
+                indexes_to_build[bucket.name][scope.name] = dict()
+                for _, collection in scope.collections.items():
+                    gsi_index_name = "gsi-" + str(count)
+                    create_index_query = "CREATE INDEX `%s` " \
+                                         "ON `%s`.`%s`.`%s`(`age`)" \
+                                         "WITH { 'defer_build': true, 'num_replica': 2 }" \
+                                         % (gsi_index_name, bucket.name, scope.name, collection.name)
+                    result = self.run_cbq_query(create_index_query)
+                    # self.assertTrue(result['status'] == "success", "Defer build Query %s failed." % create_index_query)
+
+                    if collection.name not in indexes_to_build[bucket.name][scope.name]:
+                        indexes_to_build[bucket.name][scope.name][collection.name] = list()
+                    indexes_to_build[bucket.name][scope.name][collection.name].append(gsi_index_name)
+
+                    select_query = "select age from `%s`.`%s`.`%s` where age > 1 limit 1" % (
+                        bucket.name, scope.name, collection.name)
+                    self.select_queries.append(select_query)
+
+                    count = count + 1
+                    if count >= self.number_of_indexes:
+                        return indexes_to_build
+        return indexes_to_build
+
+    def run_select_query(self):
+        """
+        Runs select queries in a loop in a separate thread until the thread is asked for to join
+        """
+        self.log.info("Starting select queries: {0}".format(self.query_thread_flag))
+        while self.query_thread_flag:
+            for select_query in self.select_queries:
+                result = self.run_cbq_query(select_query)
+                if result['status'] != "success":
+                    self.log.warn("Query failed: {0}".format(select_query))
+                time.sleep(2)
+        self.log.info("Stopping select queries")
 
     # Stopping and restarting the memcached process
     def stop_process(self):
@@ -66,15 +196,23 @@ class volume(CollectionBase):
         servs_in = random.sample(self.available_servers, nodes_in)
 
         self.nodes_cluster = self.cluster.nodes_in_cluster[:]
-        self.nodes_cluster.remove(self.cluster.master)
+        for node in self.exclude_nodes:
+            self.nodes_cluster.remove(node)
         servs_out = random.sample(self.nodes_cluster, nodes_out)
 
         if nodes_in == nodes_out:
             self.vbucket_check = False
 
+        services = None
+        if self.services_for_rebalance_in and nodes_in > 0:
+            services = list()
+            services.append(self.services_for_rebalance_in.replace(":", ","))
+            services = services * nodes_in
+            services = services * nodes_in
+
         rebalance_task = self.task.async_rebalance(
             self.cluster.servers[:self.nodes_init], servs_in, servs_out, check_vbucket_shuffling=self.vbucket_check,
-            retry_get_process_num=100)
+            retry_get_process_num=200, services=services)
 
         self.available_servers = [servs for servs in self.available_servers if servs not in servs_in]
         self.available_servers += servs_out
@@ -167,6 +305,10 @@ class volume(CollectionBase):
     def test_volume_taf(self):
         self.loop = 0
         # self.cluster_utils.set_metadata_purge_interval()
+        if self.number_of_indexes > 0:
+            self.query_thread = threading.Thread(target=self.run_select_query)
+            self.query_thread_flag = True
+            self.query_thread.start()
         while self.loop < self.iterations:
             self.log.info("Finished steps 1-4 successfully in setup")
             self.log.info("Step 5: Rebalance in with Loading of docs")
@@ -252,7 +394,7 @@ class volume(CollectionBase):
                     task = self.data_load_collection(async_load=False)
                     if task.result is False:
                         self.fail("Doc loading failed")
-                rebalance_task = self.task.async_rebalance(self.cluster.servers, [], [], retry_get_process_num=100)
+                rebalance_task = self.task.async_rebalance(self.cluster.servers, [], [], retry_get_process_num=200)
                 if self.data_load_stage == "during":
                     task = self.data_load_collection()
                 self.wait_for_rebalance_to_complete(rebalance_task)
@@ -278,18 +420,21 @@ class volume(CollectionBase):
                     self.std_vbucket_dist = self.input.param("std_vbucket_dist", None)
                     std = self.std_vbucket_dist or 1.0
 
-                    prev_failover_stats = self.bucket_util.get_failovers_logs(self.cluster.nodes_in_cluster,
+                    kv_nodes = self.cluster_util.get_kv_nodes()
+                    self.log.info("Collecting pre_failover_stats. KV nodes are {0}".format(kv_nodes))
+                    prev_failover_stats = self.bucket_util.get_failovers_logs(kv_nodes,
                                                                               self.bucket_util.buckets)
-                    prev_vbucket_stats = self.bucket_util.get_vbucket_seqnos(self.cluster.nodes_in_cluster,
+                    prev_vbucket_stats = self.bucket_util.get_vbucket_seqnos(kv_nodes,
                                                                              self.bucket_util.buckets)
                     self.sleep(10)
 
                     disk_replica_dataset, disk_active_dataset = self.bucket_util.get_and_compare_active_replica_data_set_all(
-                        self.cluster.nodes_in_cluster, self.bucket_util.buckets, path=None)
+                        kv_nodes, self.bucket_util.buckets, path=None)
 
                     self.rest = RestConnection(self.cluster.master)
                     self.nodes = self.cluster_util.get_nodes(self.cluster.master)
-                    self.chosen = self.cluster_util.pick_nodes(self.cluster.master, howmany=1)
+                    self.chosen = self.cluster_util.pick_nodes(self.cluster.master, howmany=1,
+                                                               exclude_nodes=self.exclude_nodes)
 
                     if self.data_load_stage == "during":
                         task = self.data_load_collection()
@@ -321,7 +466,7 @@ class volume(CollectionBase):
                                 self.rest.set_recovery_type(otpNode=self.chosen[0].id, recoveryType="delta")
 
                         rebalance_task = self.task.async_rebalance(
-                            self.cluster.servers[:self.nodes_init], [], [], retry_get_process_num=100)
+                            self.cluster.servers[:self.nodes_init], [], [], retry_get_process_num=200)
                         self.wait_for_rebalance_to_complete(rebalance_task)
                         self.sleep(10)
 
@@ -329,17 +474,18 @@ class volume(CollectionBase):
                         self.wait_for_async_data_load_to_complete(task)
                     self.data_validation_collection()
 
-                    self.bucket_util.compare_failovers_logs(prev_failover_stats, self.cluster.nodes_in_cluster,
+                    kv_nodes = self.cluster_util.get_kv_nodes()
+                    self.log.info("Collecting post_failover_stats. KV nodes are {0}".format(kv_nodes))
+                    self.bucket_util.compare_failovers_logs(prev_failover_stats, kv_nodes,
                                                             self.bucket_util.buckets)
                     self.sleep(10)
 
                     self.bucket_util.data_analysis_active_replica_all(
                         disk_active_dataset, disk_replica_dataset,
-                        self.cluster.servers[:self.nodes_in + self.nodes_init],
+                        kv_nodes,
                         self.bucket_util.buckets, path=None)
-                    nodes = self.cluster_util.get_nodes_in_cluster(self.cluster.master)
                     self.bucket_util.vb_distribution_analysis(
-                        servers=nodes, buckets=self.bucket_util.buckets,
+                        servers=kv_nodes, buckets=self.bucket_util.buckets,
                         num_replicas=2,
                         std=std, total_vbuckets=self.cluster_util.vbuckets)
                     self.sleep(10)
@@ -361,7 +507,7 @@ class volume(CollectionBase):
             for i in range(len(self.bucket_util.buckets)):
                 bucket_helper.change_bucket_props(
                     self.bucket_util.buckets[i], replicaNumber=1)
-            rebalance_task = self.task.async_rebalance(self.cluster.servers, [], [], retry_get_process_num=100)
+            rebalance_task = self.task.async_rebalance(self.cluster.servers, [], [], retry_get_process_num=200)
             if self.data_load_stage == "during":
                 task = self.data_load_collection()
             self.wait_for_rebalance_to_complete(rebalance_task)
@@ -383,10 +529,14 @@ class volume(CollectionBase):
                     servs_out = random.sample(self.nodes_cluster,
                                               int(len(self.cluster.nodes_in_cluster) - self.nodes_init))
                     rebalance_task = self.task.async_rebalance(
-                        self.cluster.servers[:self.nodes_init], [], servs_out, retry_get_process_num=100)
+                        self.cluster.servers[:self.nodes_init], [], servs_out, retry_get_process_num=200)
                     self.wait_for_rebalance_to_complete(rebalance_task)
                     self.available_servers += servs_out
                     self.cluster.nodes_in_cluster = list(set(self.cluster.nodes_in_cluster) - set(servs_out))
             else:
+                if self.number_of_indexes > 0:
+                    self.query_thread_flag = False
+                    self.query_thread.join()
+                    self.query_thread = None
                 self.log.info("Volume Test Run Complete")
         ############################################################################################################################
