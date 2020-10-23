c7560f3d125598df83acb5c9acc592447424fe44
Ashwin <ashwintrojan+github@gmail.com>
2020-10-23 14:31:37 +0530

    Fixing doc_key for collection-auto_compaction tests
    Change-Id: I76899eb22c46a64472846270496235924e627d73
    Reviewed-on: http://review.couchbase.org/c/TAF/+/138808
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Sumedh Basarkod <sumedhpb8@gmail.com>
    Reviewed-by: Sumedh Basarkod <sumedhpb8@gmail.com>

--- pytests/bucket_param_tests/autocompaction.py
+++ pytests/bucket_param_tests/autocompaction.py
@@ -18,7 +18,6 @@ from cb_tools.cbstats import Cbstats
 class AutoCompactionTests(CollectionBase):
     def setUp(self):
         super(AutoCompactionTests, self).setUp()
-        self.key = self.input.param("key", "compact")
         self.is_crashed = Event()
         self.autocompaction_value = self.input.param("autocompaction_value", 0)
         self.during_ops = self.input.param("during_ops", None)
