262996058c34fd814bc37e65861a765e89e1b708
Balakumaran G <balakumaran.gopal@couchbase.com>
2021-01-25 15:58:46 +0530

    Change replica to 2 for dgm tests
    Change-Id: Ic5d6da99e281ab26fcc333efc5f4b394ae328c24
    Reviewed-on: http://review.couchbase.org/c/TAF/+/144257
    Tested-by: Sumedh Basarkod <sumedhpb8@gmail.com>
    Reviewed-by: Sumedh Basarkod <sumedhpb8@gmail.com>

--- pytests/bucket_collections/bucket_templates/dgm/buckets_for_rebalance_tests_more_collections.py
+++ pytests/bucket_collections/bucket_templates/dgm/buckets_for_rebalance_tests_more_collections.py
@@ -9,7 +9,7 @@ spec = {
     MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
 
     Bucket.bucketType: Bucket.Type.MEMBASE,
-    Bucket.replicaNumber: Bucket.ReplicaNum.ONE,
+    Bucket.replicaNumber: Bucket.ReplicaNum.TWO,
     Bucket.ramQuotaMB: 800,
     Bucket.replicaIndex: 1,
     Bucket.flushEnabled: Bucket.FlushBucket.DISABLED,
