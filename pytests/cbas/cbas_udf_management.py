793b155871ae6668e4db25a09f158d2f4f693885
umang <umang.agrawal@couchbase.com>
2021-04-23 11:24:06 +0530

    Fix for CBQE-6779
    Change-Id: Ic2f011881dae4a2cf7fc65145213bb304f2c9b7b
    Reviewed-on: http://review.couchbase.org/c/TAF/+/151941
    Reviewed-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- pytests/cbas/cbas_udf_management.py
+++ pytests/cbas/cbas_udf_management.py
@@ -191,7 +191,8 @@ class CBASUDF(CBASBaseTest):
                 query_context=self.input.param('query_context', False),
                 use_statement=self.input.param('use_statement', False),
                 validate_error_msg=self.input.param('validate_error', False),
-                expected_error=self.input.param('expected_error', None),
+                expected_error=self.input.param('expected_error', None).format(
+                    udf_obj.dataverse_name),
                 timeout=300, analytics_timeout=300):
             self.fail("Error while creating Analytics UDF")
         if not self.input.param('validate_error', False):
@@ -292,7 +293,7 @@ class CBASUDF(CBASBaseTest):
                 validate_error_msg=self.input.param('validate_error', False),
                 expected_error=self.input.param('expected_error', "").format(
                     CBASHelper.unformat_name(
-                        CBASHelper.metadata_format(test_udf_obj.dataverse_name), 
+                        "\/".join(CBASHelper.metadata_format(test_udf_obj.dataverse_name).split("/")), 
                         test_udf_obj.name)),
                 timeout=300, analytics_timeout=300):
             self.fail("Error while creating Analytics UDF")
