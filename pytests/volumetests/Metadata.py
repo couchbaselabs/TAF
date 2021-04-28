9ca1369f725cb2ba8d1f3fae0592c36aaeb5cea3
sumedhpb <sumedhpb8@gmail.com>
2021-04-28 12:33:01 +0530

    Create indexes in incremental fashion in volume metadata test
    Change-Id: Ie0030034c9de519d3a78143c4f2cf6ddb41dc776
    Reviewed-on: http://review.couchbase.org/c/TAF/+/152303
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/volumetests/Metadata.py
+++ pytests/volumetests/Metadata.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from com.couchbase.client.java import *
@@ -25,13 +26,15 @@ class volume(CollectionBase):
         self.iterations = self.input.param("iterations", 1)
         self.retry_get_process_num = self.input.param("retry_get_process_num", 400)
         self.kv_mem_quota = self.input.param("kv_mem_quota", 2000)
-        self.index_mem_quota = self.input.param("index_mem_quota", 15000)
+        self.index_mem_quota = self.input.param("index_mem_quota", 22700)
+        self.skip_index_creation_in_setup = self.input.param("skip_index_creation_in_setup", False)
         self.index_setup()
 
     def index_setup(self):
         # Initialize parameters for index querying
         self.n1ql_nodes = None
-        self.number_of_indexes_per_coll = self.input.param("number_of_indexes_per_coll", 5) # with 1 replicas = total 10 indexes per coll
+        self.number_of_indexes_per_coll = self.input.param("number_of_indexes_per_coll",
+                                                           5)  # with 1 replicas = total 10 indexes per coll
         # Below is the total num of indexes to drop after each rebalance (and recreate them)
         self.num_indexes_to_drop = self.input.param("num_indexes_to_drop", 500)
         self.bucket_util.flush_all_buckets(self.cluster.master, skip_resetting_num_items=True)
@@ -46,8 +49,9 @@ class volume(CollectionBase):
             self.n1ql_rest_connections.append(RestConnection(n1ql_node))
             self.exclude_nodes.append(n1ql_node)
         self.n1ql_turn_counter = 0  # To distribute the turn of using n1ql nodes for query. Start with first node
-        self.indexes_to_build = self.create_indexes()
-        self.build_deferred_indexes(self.indexes_to_build)
+        if not self.skip_index_creation_in_setup:
+            self.indexes_to_build = self.create_indexes()
+            self.build_deferred_indexes(self.indexes_to_build)
 
     def tearDown(self):
         self.log.info("Printing bucket stats before teardown")
@@ -61,7 +65,7 @@ class volume(CollectionBase):
         self.log.info("Checking logs on {0}".format(self.servers))
         result = self.check_coredump_exist(self.servers, force_collect=True)
         if not self.crash_warning:
-            self.skip_check_logs = True # Setting this, as we don't have to check logs again in tearDown
+            self.skip_check_logs = True  # Setting this, as we don't have to check logs again in tearDown
             self.assertFalse(result, msg="Cb_log file validation failed")
         if self.crash_warning and result:
             self.log.warn("CRASH | CRITICAL | WARN messages found in cb_logs")
@@ -122,7 +126,7 @@ class volume(CollectionBase):
         self.log.info("online indexes count: {0}".format(len(result['results'])))
         self.sleep(60, "Wait after building indexes")
 
-    def create_indexes(self):
+    def create_indexes(self, gsi_base_name="gsi"):
         """
         Create gsi indexes on collections - according to number_of_indexes_per_coll
         """
@@ -136,10 +140,10 @@ class volume(CollectionBase):
                 indexes_to_build[bucket.name][scope.name] = dict()
                 for _, collection in scope.collections.items():
                     for _ in range(self.number_of_indexes_per_coll):
-                        gsi_index_name = "gsi-" + str(count)
+                        gsi_index_name = gsi_base_name + str(count)
                         create_index_query = "CREATE INDEX `%s` " \
-                                             "ON `%s`.`%s`.`%s`(`age`)" \
-                                             "WITH { 'defer_build': true, 'num_replica': 1 }" \
+                                             "ON `%s`.`%s`.`%s`(`age`) " \
+                                             "WITH { 'defer_build': true, 'num_replica': 0 }" \
                                              % (gsi_index_name, bucket.name, scope.name, collection.name)
                         result = self.run_cbq_query(create_index_query)
                         # self.assertTrue(result['status'] == "success", "Defer build Query %s failed." % create_index_query)
@@ -161,7 +165,7 @@ class volume(CollectionBase):
                     for gsi_index_name in gsi_index_names:
                         create_index_query = "CREATE INDEX `%s` " \
                                              "ON `%s`.`%s`.`%s`(`age`)" \
-                                             "WITH { 'defer_build': true, 'num_replica': 1 }" \
+                                             "WITH { 'defer_build': true, 'num_replica': 0 }" \
                                              % (gsi_index_name, bucket, scope, collection)
                         result = self.run_cbq_query(create_index_query)
         self.build_deferred_indexes(indexes_dropped)
@@ -181,9 +185,9 @@ class volume(CollectionBase):
                 for collection, gsi_index_names in collection_data.items():
                     for gsi_index_name in gsi_index_names:
                         drop_index_query = "DROP INDEX `%s` ON " \
-                                             "`%s`.`%s`.`%s`" \
-                                             "USING GSI" \
-                                             % (gsi_index_name, bucket, scope, collection)
+                                           "`%s`.`%s`.`%s`" \
+                                           "USING GSI" \
+                                           % (gsi_index_name, bucket, scope, collection)
                         result = self.run_cbq_query(drop_index_query)
                         if collection not in indexes_dropped[bucket][scope]:
                             indexes_dropped[bucket][scope][collection] = list()
@@ -235,11 +239,11 @@ class volume(CollectionBase):
         self.log.info("Finished steps 1-4 successfully in setup")
         self.reload_data_into_buckets()
         step_count = 1
-        while len(self.available_servers)!=0:
+        while len(self.available_servers) != 0:
             add_node = self.available_servers[0]
             self.log.info("Step: {0} Adding node {1}".format(step_count, add_node.ip))
             operation = self.task.async_rebalance(self.nodes_in_cluster, [add_node], [],
-                                                  services = ["index"],
+                                                  services=["index"],
                                                   retry_get_process_num=self.retry_get_process_num)
             self.wait_for_rebalance_to_complete(operation)
             self.available_servers.remove(add_node)
@@ -294,3 +298,48 @@ class volume(CollectionBase):
             self.nodes_in_cluster.remove(remove_node)
 
             cycles = cycles + 1
+
+    def test_volume_incremental_index_creation(self):
+        """
+        Perform following steps in a loop until 10000 indexes are created.
+
+        Step 1. Add all the remaining indexer nodes in the cluster.
+        Step 2. Remove some indexer nodes from the cluster - by keeping some indexer nodes.
+        No of nodes remaining are proportional to number of indexes.
+        Step 3. Create 2000 indexes
+        Step 4. Build all newly crated 2000 indexes, 2 indexes at a time.
+        Step 5. Wait for indexes to become active synchronously.
+        """
+        self.reload_data_into_buckets()
+        self.available_index_nodes = self.available_servers  # dynamic variable to keep track of free index nodes
+        self.total_available_index_nodes = self.available_servers  # nodes reserved for index nodes. Remains constant
+        self.number_of_nodes_to_rebalance_out = [14, 10, 6, 2]
+
+        for increment_count in range(5):
+            # Rebalance-in all available free nodes
+            services = ["index"]
+            services = services * (len(self.available_index_nodes))
+            operation = self.task.async_rebalance(self.nodes_in_cluster, self.available_index_nodes, [],
+                                                  services=services,
+                                                  retry_get_process_num=self.retry_get_process_num)
+            self.wait_for_rebalance_to_complete(operation)
+            for node in self.available_index_nodes:
+                self.nodes_in_cluster.append(node)
+
+            # Rebalance-out a few index nodes as appropriate. Not needed for last step
+            # as in the last step we need all nodes to be there in cluster
+            if increment_count != 4:
+                remove_nodes = random.sample(self.total_available_index_nodes,
+                                             self.number_of_nodes_to_rebalance_out[increment_count])
+                operation = self.task.async_rebalance(self.nodes_in_cluster, [], remove_nodes,
+                                                      retry_get_process_num=self.retry_get_process_num)
+                self.wait_for_rebalance_to_complete(operation)
+                self.available_index_nodes = remove_nodes
+                for node in remove_nodes:
+                    self.nodes_in_cluster.remove(node)
+
+            self.number_of_indexes_per_coll = 2
+            self.n1ql_turn_counter = 0  # To distribute the turn of using n1ql nodes for query. Start with first node
+            gsi_base_name = "gsi-set-" + str(increment_count) + "-"
+            self.indexes_to_build = self.create_indexes(gsi_base_name=gsi_base_name)
+            self.build_deferred_indexes(self.indexes_to_build)
