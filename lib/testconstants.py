9424e70bacce854b49603c4d9866bd1b7f0c8508
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2021-06-11 13:45:59 +0530

    Update testconstants to incorporate 7.1
    Change-Id: Ic1e541c1d3dd387b424be605b203140969bc1ad7
    Reviewed-on: http://review.couchbase.org/c/TAF/+/155593
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ritam Sharma <ritam@couchbase.com>
    Reviewed-by: Ashwin <ashwintrojan+github@gmail.com>

--- lib/testconstants.py
+++ lib/testconstants.py
@@ -5,8 +5,6 @@ MEMBASE_DATA_PATH = "/opt/membase/var/lib/membase/data/"
 MEMBASE_VERSIONS = ["1.5.4", "1.6.5.4-win64", "1.7.0", "1.7.1", "1.7.1.1", "1.7.2"]
 COUCHBASE_DATA_PATH = "/opt/couchbase/var/lib/couchbase/data/"
 # remember update WIN_REGISTER_ID also when update COUCHBASE_VERSION
-
-temp_DP_magma = "1006.5.1"
 COUCHBASE_VERSIONS = ["0.0.0",
                       "1.8.0r", "1.8.0", "1.8.1", "2.0.0", "2.0.1", "2.0.2", "2.1.0",
                       "2.1.1", "2.2.0", "2.2.1", "2.5.0", "2.5.1", "2.5.2", "3.0.0",
@@ -16,8 +14,8 @@ COUCHBASE_VERSIONS = ["0.0.0",
                       "4.6.3", "4.6.4", "4.6.5", "4.7.0", "5.0.0", "5.0.1", "5.0.2",
                       "5.1.0", "5.1.1", "5.1.2", "5.1.3", "5.5.0", "5.5.1", "5.5.2",
                       "5.5.3", "5.5.4", "5.5.5", "5.5.6", "6.0.0", "6.0.1", "6.0.2",
-                      "6.0.3", "6.0.4", "6.5.0", "6.5.1", "6.6.0", "6.6.2", "7.0.0",
-                      temp_DP_magma]
+                      "6.0.3", "6.0.4", "6.0.5", "6.5.0", "6.5.1", "6.5.2", "6.6.0", 
+                      "6.6.1", "6.6.2", "6.6.3", "7.0.0", "7.0.1", "7.1.0"]
 CB_RELEASE_BUILDS = {"0.0.0":"0000",
                      "2.1.1":"764", "2.2.0":"821", "2.5.0":"1059", "2.5.1":"1083",
                      "2.5.2":"1154", "3.0.3":"1716", "3.1.5":"1859", "3.1.6":"1904",
@@ -28,9 +26,10 @@ CB_RELEASE_BUILDS = {"0.0.0":"0000",
                      "5.1.0":"5552", "5.1.1":"5723", "5.1.2":"6030", "5.1.3":"6212",
                      "5.5.0":"2958", "5.5.1":"3511", "5.5.2":"3733", "5.5.3":"4041",
                      "5.5.4":"4338", "5.5.5":"4523", "5.5.6":"0000", "6.0.0":"1693",
-                     "6.0.1":"2037", "6.0.2":"2601", "6.0.3":"2895", "6.0.4":"0000",
-                     "6.5.0":"4967", "6.5.1":"0000", "6.6.0":"0000", "6.6.2":"0000",
-                     "7.0.0":"0000", temp_DP_magma:"0000"}
+                     "6.0.1":"2037", "6.0.2":"2601", "6.0.3":"2895", "6.0.4":"3090",
+                     "6.0.5":"3340", "6.5.0":"4967", "6.5.1":"6296", "6.5.2":"0000",
+                     "6.6.0":"7909", "6.6.1":"9213", "6.6.2":"9588", "6.6.3":"0000",
+                     "7.0.0":"0000", "7.0.1":"0000", "7.1.0":"0000"}
 COUCHBASE_FROM_VERSION_3 = ["3.0.0", "3.0.1", "3.0.2", "3.0.3", "3.1.0", "3.1.1",
                             "3.1.2", "3.1.3", "3.1.4", "3.1.5", "3.1.6", "3.5.0",
                             "4.0.0", "4.0.1", "4.1.0", "4.1.1", "4.1.2", "4.5.0",
@@ -38,8 +37,9 @@ COUCHBASE_FROM_VERSION_3 = ["3.0.0", "3.0.1", "3.0.2", "3.0.3", "3.1.0", "3.1.1"
                             "4.6.5", "4.7.0", "5.0.0", "5.0.1", "5.0.2", "5.1.0",
                             "5.1.1", "5.1.2", "5.1.3", "5.5.0", "5.5.1", "5.5.2",
                             "5.5.3", "5.5.4", "5.5.5", "5.5.6", "6.0.0", "6.0.1",
-                            "6.0.2", "6.0.3", "6.0.4", "6.5.0", "6.5.1", "6.6.0",
-                            "6.6.2", "7.0.0", temp_DP_magma]
+                            "6.0.2", "6.0.3", "6.0.4", "6.0.5", "6.5.0", "6.5.1",
+                            "6.5.2", "6.6.0", "6.6.1", "6.6.2", "6.6.3", "7.0.0",
+                            "7.0.1", "7.1.0"]
 COUCHBASE_RELEASE_FROM_VERSION_3 = ["3.0.0", "3.0.1", "3.0.2", "3.0.3", "3.1.0",
                                     "3.1.1", "3.1.2", "3.1.3", "3.1.5", "4.0.0",
                                     "4.1.0", "4.1.1", "4.1.2", "4.5.0", "4.5.1",
@@ -47,37 +47,42 @@ COUCHBASE_RELEASE_FROM_VERSION_3 = ["3.0.0", "3.0.1", "3.0.2", "3.0.3", "3.1.0",
                                     "4.6.5", "5.0.0", "5.0.1", "5.1.0", "5.1.1",
                                     "5.1.2", "5.1.3", "5.5.0", "5.5.1", "5.5.2",
                                     "5.5.3", "5.5.4", "5.5.5", "5.5.6", "6.0.0",
-                                    "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.5.0",
-                                    "6.6.0"]
+                                    "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.0.5", 
+                                    "6.5.0", "6.5.1", "6.6.0", "6.6.1", "6.6.2"]
 COUCHBASE_RELEASE_FROM_SPOCK = ["5.0.0", "5.0.1", "5.1.0", "5.1.1", "5.1.2", "5.1.3", "5.5.0",
                                 "5.5.1", "5.5.2", "5.5.3", "5.5.4", "5.5.5", "5.5.6", "6.0.0",
-                                "6.0.1", "6.0.2", "6.0.3", "6.0.4"]
+                                "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.0.5", "6.5.0", "6.5.1", 
+                                "6.6.0", "6.6.1", "6.6.2"]
 COUCHBASE_FROM_VERSION_4 = ["0.0.0",
                             "4.0.0", "4.0.1", "4.1.0", "4.1.1", "4.1.2", "4.5.0",
                             "4.5.1", "4.6.0", "4.6.1", "4.6.2", "4.6.3", "4.6.4",
                             "4.6.5", "4.7.0", "5.0.0", "5.0.1", "5.0.2", "5.1.0",
                             "5.1.1", "5.1.2", "5.1.3", "5.5.0", "5.5.1", "5.5.2",
                             "5.5.3", "5.5.4", "5.5.5", "5.5.6", "6.0.0", "6.0.1",
-                            "6.0.2", "6.0.3", "6.0.4", "6.5.0", "6.5.1", "6.6.0",
-                            "6.6.2", "7.0.0", temp_DP_magma]
+                            "6.0.2", "6.0.3", "6.0.4", "6.0.5", "6.5.0", "6.5.1",
+                            "6.5.2", "6.6.0", "6.6.1", "6.6.2", "6.6.3", "7.0.0",
+                            "7.0.1", "7.1.0"]
 COUCHBASE_FROM_SHERLOCK = ["4.0.0", "4.0.1", "4.1.0", "4.1.1", "4.1.2", "4.5.0",
                            "4.5.1", "4.6.0", "4.6.1", "4.6.2", "4.6.3", "4.6.4",
                            "4.6.5", "4.7.0", "5.0.0", "5.0.1", "5.0.2", "5.1.0",
                            "5.1.1", "5.1.2", "5.1.3", "5.5.0", "5.5.1", "5.5.2",
                            "5.5.3", "5.5.4", "5.5.5", "5.5.6", "6.0.0", "6.0.1",
-                           "6.0.2", "6.0.3", "6.0.4", "6.5.0", "6.5.1", "6.6.0",
-                           "6.6.2", "7.0.0", temp_DP_magma]
+                           "6.0.2", "6.0.3", "6.0.4", "6.0.5", "6.5.0", "6.5.1",
+                           "6.5.2", "6.6.0", "6.6.1", "6.6.2", "6.6.3", "7.0.0",
+                           "7.0.1", "7.1.0"]
 COUCHBASE_FROM_4DOT6 = ["4.6.0", "4.6.1", "4.6.2", "4.6.3", "4.6.4", "4.6.5", "4.7.0",
                         "5.0.0", "5.0.1", "5.0.2", "5.1.0", "5.1.1", "5.1.2", "5.1.3",
                         "5.5.0", "5.5.1", "5.5.2", "5.5.3", "5.5.4", "5.5.5", "5.5.6",
-                        "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.5.0", "6.5.1",
-                        "6.6.0", "6.6.2", "7.0.0", temp_DP_magma]
+                        "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.0.5", "6.5.0",
+                        "6.5.1", "6.5.2", "6.6.0", "6.6.1", "6.6.2", "6.6.3", "7.0.0",
+                        "7.0.1", "7.1.0"]
 COUCHBASE_FROM_WATSON = ["0.0.0",
                          "4.5.0", "4.5.1", "4.6.0", "4.6.1", "4.6.2", "4.6.3", "4.6.4",
                          "4.6.5", "4.7.0", "5.0.0", "5.0.1", "5.0.2", "5.1.0", "5.1.1",
                          "5.1.2", "5.1.3", "5.5.0", "5.5.1", "5.5.2", "5.5.3", "5.5.4",
                          "5.5.5", "5.5.6", "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4",
-                         "6.5.0", "6.5.1", "6.6.0", "6.6.2", "7.0.0", temp_DP_magma]
+                         "6.0.5", "6.5.0", "6.5.1", "6.5.2", "6.6.0", "6.6.1", "6.6.2", 
+                         "6.6.3", "7.0.0", "7.0.1", "7.1.0"]
 """
     If new version is greater than 5.0.0, we need to add new version to constant
     COUCHBASE_FROM_SPOCK below so that windows will get correct build (msi) to un/install
@@ -85,17 +90,21 @@ COUCHBASE_FROM_WATSON = ["0.0.0",
 COUCHBASE_FROM_SPOCK = ["0.0.0", "4.7.0", "5.0.0", "5.0.1", "5.0.2", "5.1.0", "5.1.1",
                         "5.1.2", "5.1.3", "5.5.0", "5.5.1", "5.5.2", "5.5.3", "5.5.4",
                         "5.5.5", "5.5.6", "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4",
-                        "6.5.0", "6.5.1", "6.6.0", "6.6.2", "7.0.0", temp_DP_magma]
+                        "6.0.5", "6.5.0", "6.5.1", "6.5.2", "6.6.0", "6.6.1", "6.6.2",
+                        "6.6.3", "7.0.0", "7.0.1", "7.1.0"]
 
 COUCHBASE_FROM_VULCAN = ["0.0.0", "5.5.0", "5.5.1", "5.5.2", "5.5.3", "5.5.4", "5.5.5",
-                         "5.5.6", "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.5.0",
-                        "6.5.1", "6.6.0", "6.6.2", "7.0.0", temp_DP_magma]
-COUCHBASE_FROM_ALICE = ["0.0.0", "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.5.0",
-                        "6.6.0", "6.6.2", "7.0.0", temp_DP_magma]
-COUCHBASE_FROM_601 = ["0.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.5.0", "6.5.1",
-                      "6.6.0", "6.6.2", "7.0.0", temp_DP_magma]
-COUCHBASE_FROM_MAD_HATTER = ["0.0.0", "6.5.0", "6.6.0", "6.6.2", "7.0.0", temp_DP_magma]
-COUCHBASE_FROM_CHESHIRE_CAT = ["0.0.0", "6.6.2", "7.0.0", temp_DP_magma]
+                         "5.5.6", "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.0.5",
+                         "6.5.0", "6.5.1", "6.5.2", "6.6.0", "6.6.1", "6.6.2", "6.6.3",
+                         "7.0.0", "7.0.1", "7.1.0"]
+COUCHBASE_FROM_ALICE = ["0.0.0", "6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.0.5", "6.5.0", "6.5.1",
+                        "6.5.2", "6.6.0", "6.6.1", "6.6.2", "6.6.3", "7.0.0", "7.0.1", "7.1.0"]
+COUCHBASE_FROM_601 = ["0.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4", "6.0.5", "6.5.0", "6.5.1", "6.5.2", 
+                      "6.6.0", "6.6.1", "6.6.2", "6.6.3", "7.0.0", "7.0.1", "7.1.0"]
+COUCHBASE_FROM_MAD_HATTER = ["0.0.0", "6.5.0", "6.5.1", "6.5.2", "6.6.0", "6.6.1", "6.6.2", "6.6.3", 
+                             "7.0.0", "7.0.1", "7.1.0"]
+COUCHBASE_FROM_CHESHIRE_CAT = ["0.0.0", "7.0.0", "7.0.1", "7.1.0"]
+COUCHBASE_FROM_NEO = ["0.0.0", "7.1.0"]
 COUCHBASE_RELEASE_VERSIONS_3 = ["3.0.1", "3.0.1-1444", "3.0.2", "3.0.2-1603", "3.0.3",
                                 "3.0.3-1716", "3.1.0", "3.1.0-1797", "3.1.1", "3.1.1-1807",
                                 "3.1.2", "3.1.2-1815", "3.1.3", "3.1.3-1823", "3.1.5"]
@@ -119,18 +128,20 @@ CB_VERSION_NAME = {"0.0":"master", "4.0":"sherlock", "4.1":"sherlock",
                    "6.0":"alice",
                    "6.5":"mad-hatter",
                    "6.6":"mad-hatter",
-                   "7.0":"cheshire-cat"}
+                   "7.0":"cheshire-cat",
+                   "7.1":"neo"}
 
 MACOS_NAME = {"10.10":"Yosemite", "10.11":"El Capitan", "10.12":"Sierra", "10.13":"High Sierra",
               "10.14":"Mojave", "10.15":"Catalina"}
 
 SYSTEMD_SERVER = ["centos 8", "centos 7", "suse 12", "suse 15", "ubuntu 16.04", "ubuntu 18.04",
-                  "ubuntu 20.04", "debian 8", "debian 9", "debian 10", "debian gnu/linux 8", "debian gnu/linux 9",
-                  "rhel 8", "oel 7", "oel 8"]
+                  "ubuntu 20.04", "debian 8", "debian 9", "debian 10", "rhel 8", "oel 7", "oel 8"]
+WIN_NUM_ERLANG_PROCESS = 4
 WIN_MEMBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Membase/Server/var/lib/membase/data/'
 WIN_COUCHBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Couchbase/Server/var/lib/couchbase/data/'
 WIN_COUCHBASE_DATA_PATH_RAW = 'c:/Program\ Files/Couchbase/Server/var/lib/couchbase/data/'
-WIN_CB_PATH = "/cygdrive/c/Program Files/Couchbase/Server/"
+WIN_CB_PATH = "/cygdrive/c/Program\ Files/Couchbase/Server/"
+WIN_CB_PATH_PARA = "/cygdrive/c/Program Files/Couchbase/Server/"
 WIN_MB_PATH = "/cygdrive/c/Program Files/Membase/Server/"
 WIN_PROCESSES_KILLED = ["msiexec32.exe", "msiexec.exe", "setup.exe", "ISBEW64.*",
                         "iexplore.*", "WerFault.*", "Firefox.*", "bash.exe",
@@ -199,7 +210,7 @@ MAX_TIME_HOUR = 23
 NUM_ERLANG_THREADS = 16
 MIN_KV_QUOTA = 256
 INDEX_QUOTA = 256
-FTS_QUOTA = 256
+FTS_QUOTA = 512
 EVENTING_QUOTA = 512
 CBAS_QUOTA = 1024
 """ when we run with small server, it needs to increase cluster quota so that small
@@ -266,16 +277,16 @@ LOG_FILE_NAMES = ['cbcollect_info.log', 'couchbase.log', 'couchstore_local.log',
                   'ns_server.info.log', 'ns_server.json_rpc.log', 'ns_server.mapreduce_errors.log', 'ns_server.metakv.log',
                   'ns_server.ns_couchdb.log', 'ns_server.projector.log', 'ns_server.query.log', 'ns_server.reports.log',
                   'ns_server.stats.log', 'ns_server.views.log', 'ns_server.xdcr_target.log', 'projector_pprof.log',
-                  'stats.log', 'stats_archives.json', 'syslog.tar.gz', 'systemd_journal.gz', 'users.dets']
+                  'stats.log', 'syslog.tar.gz', 'systemd_journal.gz', 'users.dets']
 # Allow for easy switch to a local mirror of the download stuff
 # (for people outside the mountain view office it's nice to be able to
 # be running this locally without being on VPN (which my test machines isn't)
 CB_DOWNLOAD_SERVER = "172.23.126.166"
-CB_DOWNLOAD_SERVER_FQDN = "nas.build.couchbase.com"
+CB_DOWNLOAD_SERVER_FQDN = "latestbuilds.service.couchbase.com"
 #CB_DOWNLOAD_SERVER = "10.0.0.117:8080"
 
 # old url MV_LATESTBUILD_REPO = "http://builds.hq.northscale.net/latestbuilds/"
-MV_LATESTBUILD_REPO = "http://latestbuilds.hq.couchbase.com/"
+MV_LATESTBUILD_REPO = "http://latestbuilds.service.couchbase.com/"
 #SHERLOCK_BUILD_REPO = "http://latestbuilds.hq.couchbase.com/couchbase-server/sherlock/"
 SHERLOCK_BUILD_REPO = "http://{0}/builds/latestbuilds/couchbase-server/sherlock/".format(CB_DOWNLOAD_SERVER)
 #COUCHBASE_REPO = "http://latestbuilds.hq.couchbase.com/couchbase-server/"
@@ -288,19 +299,20 @@ CB_FQDN_REPO = "http://{0}/builds/latestbuilds/couchbase-server/".format(CB_DOWN
 CB_RELEASE_REPO = "http://{0}/builds/releases/".format(CB_DOWNLOAD_SERVER)
 #MV_RELEASE_REPO = "http://latestbuilds.hq.couchbase.com/release"
 MC_BIN_CLIENT = "mc_bin_client"
-TESTRUNNER_CLIENT = "testrunner_client"
 PYTHON_SDK = "python_sdk"
-CB_RELEASE_APT_GET_REPO = "http://latestbuilds.hq.couchbase.com/couchbase-release/10/couchbase-release-1.0-0.deb"
-CB_RELEASE_YUM_REPO = "http://latestbuilds.hq.couchbase.com/couchbase-release/10/couchbase-release-1.0-0.noarch.rpm"
+TESTRUNNER_CLIENT = "testrunner_client"
+CB_RELEASE_APT_GET_REPO = "http://latestbuilds.service.couchbase.com/couchbase-release/10/couchbase-release-1.0-0.deb"
+CB_RELEASE_YUM_REPO = "http://latestbuilds.service.couchbase.com/couchbase-release/10/couchbase-release-1.0-0.noarch.rpm"
 DEWIKI = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/dewiki.txt.gz"
 ENWIKI = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/enwiki.txt.gz"
 ESWIKI = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/eswiki.txt.gz"
 FRWIKI = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/frwiki.txt.gz"
+NAPADATASET = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/napa_dataset.txt.gz"
 QUERY_5K_FIELDS = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/query_50000_fields.txt"
 QUERY_5K_NUM_DATE = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/query_50000_functions_numeric_string_datetime.txt"
 QUERY_JOIN = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/queries_joins_50000.txt"
 QUERY_SUBQUERY = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/queries_subqueries_1000.txt"
-ENT_BKRS = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/entbackup.tgz"
+ENT_BKRS = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/entbackup-6.6.0.tar.gz"
 ENT_BKRS_FTS = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/entbackup-fts.tgz"
 WIN_UNZIP = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/win-cmd/unzip.exe"
 WIN_PSSUSPEND = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/win-cmd/pssuspend.exe"
@@ -309,3 +321,5 @@ THROUGHPUT_CONCURRENCY = 4
 # determine wether or not to use high throughput
 ALLOW_HTP = True
 IS_CONTAINER = False
+FUZZY_FTS_LARGE_DATASET = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/fuzzy_large_dataset.json"
+FUZZY_FTS_SMALL_DATASET = "https://s3-us-west-1.amazonaws.com/qebucket/testrunner/data/fuzzy_small_dataset.json"
