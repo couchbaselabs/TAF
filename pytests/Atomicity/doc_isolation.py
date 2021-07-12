0fc324740a237622de3742a614965cd9d676bd19
Ashwin <ashwintrojan+github@gmail.com>
2021-07-12 11:32:45 +0530

    Fixing mutate value for rollback transaction
    Change-Id: Iffe31ce9e9df65bcc640ea3e28943091fddae644
    Reviewed-on: http://review.couchbase.org/c/TAF/+/157294
    Reviewed-by: <ankush.sharma@couchbase.com>
    Tested-by: Ashwin <ashwintrojan+github@gmail.com>

--- pytests/Atomicity/doc_isolation.py
+++ pytests/Atomicity/doc_isolation.py
@@ -552,7 +552,7 @@ class IsolationDocTest(BaseTestCase):
             self.task_manager.get_task_result(trans_task_2)
             load_gen_1 = doc_generator(self.key, 0, self.num_items, mutate=1)
             load_gen_2 = doc_generator(self.key, self.num_items,
-                                       self.num_items*2, mutate=1)
+                                       self.num_items*2)
 
         # Start transaction tasks with success & rollback for shadow docs test
         # Successful transaction
