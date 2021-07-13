49fe5b1af5dcd8c3df2b2c4246dafba7a73ac048
Balakumaran G <balakumaran.gopal@couchbase.com>
2021-07-13 10:44:46 +0530

    Initial magma checkin for cluster operation tests
    Change-Id: I2c452d91a9a53d76eea4dbfef9022f65badf297a
    Reviewed-on: http://review.couchbase.org/c/TAF/+/157380
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Sumedh Basarkod <sumedhpb8@gmail.com>

---
+++ pytests/bucket_collections/bucket_templates/magma_dgm/20_percent_dgm/5_node_2_replica_magma_256.py
@@ -0,0 +1,45 @@
+from BucketLib.bucket import Bucket
+from collections_helper.collections_spec_constants import MetaConstants
+
+spec = {
+    MetaConstants.NUM_BUCKETS: 3,
+    MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
+    MetaConstants.CREATE_COLLECTIONS_USING_MANIFEST_IMPORT: True,
+    MetaConstants.NUM_SCOPES_PER_BUCKET: 1,
+    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 1,
+    MetaConstants.NUM_ITEMS_PER_COLLECTION: 1,
+
+    Bucket.bucketType: Bucket.Type.MEMBASE,
+    Bucket.replicaNumber: Bucket.ReplicaNum.TWO,
+    Bucket.ramQuotaMB: 256,
+    Bucket.replicaIndex: 1,
+    Bucket.flushEnabled: Bucket.FlushBucket.ENABLED,
+    Bucket.priority: Bucket.Priority.LOW,
+    Bucket.conflictResolutionType: Bucket.ConflictResolution.SEQ_NO,
+    Bucket.maxTTL: 0,
+    Bucket.storageBackend: Bucket.StorageBackend.couchstore,
+    Bucket.evictionPolicy: Bucket.EvictionPolicy.FULL_EVICTION,
+    Bucket.compressionMode: Bucket.CompressionMode.ACTIVE,
+    "buckets": {
+        "default": {
+            MetaConstants.NUM_SCOPES_PER_BUCKET: 10,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 50,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 16500,
+            Bucket.ramQuotaMB: 256,
+            Bucket.storageBackend: Bucket.StorageBackend.magma,
+        },
+        "bucket1": {
+            MetaConstants.NUM_SCOPES_PER_BUCKET: 10,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 20,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 2500,
+            Bucket.ramQuotaMB: 2000
+        },
+        "bucket2": {
+            MetaConstants.NUM_SCOPES_PER_BUCKET: 10,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 20,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 250,
+            Bucket.ramQuotaMB: 1000,
+            Bucket.storageBackend: Bucket.StorageBackend.magma
+        }
+    }
+}
---
+++ pytests/bucket_collections/collection_ops_specs/volume_test_load_with_CRUD_on_collections_magma.py
@@ -0,0 +1,62 @@
+from collections_helper.collections_spec_constants import MetaCrudParams
+
+spec = {
+    # Scope/Collection ops params
+    MetaCrudParams.COLLECTIONS_TO_FLUSH: 0,
+    MetaCrudParams.COLLECTIONS_TO_DROP: 40,
+
+    MetaCrudParams.SCOPES_TO_DROP: 2,
+    MetaCrudParams.SCOPES_TO_ADD_PER_BUCKET: 2,
+    MetaCrudParams.COLLECTIONS_TO_ADD_FOR_NEW_SCOPES: 2,
+
+    MetaCrudParams.COLLECTIONS_TO_ADD_PER_BUCKET: 2,
+
+    MetaCrudParams.BUCKET_CONSIDERED_FOR_OPS: "all",
+    MetaCrudParams.SCOPES_CONSIDERED_FOR_OPS: "all",
+    MetaCrudParams.COLLECTIONS_CONSIDERED_FOR_OPS: "all",
+
+    # Doc loading params
+    "doc_crud": {
+        MetaCrudParams.DocCrud.RANDOMIZE_VALUE: True,
+        MetaCrudParams.DocCrud.NUM_ITEMS_FOR_NEW_COLLECTIONS: 500,
+
+        MetaCrudParams.DocCrud.COMMON_DOC_KEY: "test_collections",
+        MetaCrudParams.DocCrud.CREATE_PERCENTAGE_PER_COLLECTION: 20,
+        MetaCrudParams.DocCrud.READ_PERCENTAGE_PER_COLLECTION: 20,
+        MetaCrudParams.DocCrud.UPDATE_PERCENTAGE_PER_COLLECTION: 20,
+        MetaCrudParams.DocCrud.REPLACE_PERCENTAGE_PER_COLLECTION: 0,
+        MetaCrudParams.DocCrud.DELETE_PERCENTAGE_PER_COLLECTION: 20,
+    },
+
+    "subdoc_crud": {
+        MetaCrudParams.SubDocCrud.XATTR_TEST: False,
+
+        MetaCrudParams.SubDocCrud.INSERT_PER_COLLECTION: 0,
+        MetaCrudParams.SubDocCrud.UPSERT_PER_COLLECTION: 0,
+        MetaCrudParams.SubDocCrud.REMOVE_PER_COLLECTION: 0,
+        MetaCrudParams.SubDocCrud.LOOKUP_PER_COLLECTION: 0,
+    },
+
+    # Doc_loading task options
+    MetaCrudParams.DOC_TTL: 0,
+    MetaCrudParams.DURABILITY_LEVEL: "",
+    MetaCrudParams.SDK_TIMEOUT: 120,
+    MetaCrudParams.SDK_TIMEOUT_UNIT: "seconds",
+    MetaCrudParams.TARGET_VBUCKETS: "all",
+    MetaCrudParams.SKIP_READ_ON_ERROR: True,
+    MetaCrudParams.SUPPRESS_ERROR_TABLE: True,
+    # The below is to skip populating success dictionary for reads
+    MetaCrudParams.SKIP_READ_SUCCESS_RESULTS: True,
+
+    MetaCrudParams.RETRY_EXCEPTIONS: [],
+    MetaCrudParams.IGNORE_EXCEPTIONS: [],
+
+    # Applies only for DocCrud / SubDocCrud operation
+    MetaCrudParams.COLLECTIONS_CONSIDERED_FOR_CRUD: "all",
+    MetaCrudParams.SCOPES_CONSIDERED_FOR_CRUD: "all",
+    MetaCrudParams.BUCKETS_CONSIDERED_FOR_CRUD: "all",
+
+    # Number of threadpool executor workers for scope/collection drops/creates
+    # for making parallel rest calls during subsequent data load
+    MetaCrudParams.THREADPOOL_MAX_WORKERS: 10
+}
