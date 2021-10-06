375dc117ca26519b26824741929de05ceab80b26
Balakumaran G <balakumaran.gopal@couchbase.com>
2021-10-06 10:24:18 +0530

    CBQE-7196: Create backup conf files and add missing bucket specs
    Change-Id: I48b375a435938c4b50691dcbee0dcfacd9e03a62
    Reviewed-on: http://review.couchbase.org/c/TAF/+/163056
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Sumedh Basarkod <sumedhpb8@gmail.com>

---
+++ pytests/bucket_collections/bucket_templates/magma_dgm/20_percent_dgm/5_node_1_replica_magma_ttl_512_single_bucket.py
@@ -0,0 +1,33 @@
+from BucketLib.bucket import Bucket
+from collections_helper.collections_spec_constants import MetaConstants
+
+spec = {
+    MetaConstants.NUM_BUCKETS: 1,
+    MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
+    MetaConstants.CREATE_COLLECTIONS_USING_MANIFEST_IMPORT: True,
+    MetaConstants.NUM_SCOPES_PER_BUCKET: 1,
+    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 1,
+    MetaConstants.NUM_ITEMS_PER_COLLECTION: 1,
+
+    Bucket.bucketType: Bucket.Type.MEMBASE,
+    Bucket.replicaNumber: Bucket.ReplicaNum.TWO,
+    Bucket.ramQuotaMB: 256,
+    Bucket.replicaIndex: 1,
+    Bucket.flushEnabled: Bucket.FlushBucket.ENABLED,
+    Bucket.priority: Bucket.Priority.LOW,
+    Bucket.conflictResolutionType: Bucket.ConflictResolution.SEQ_NO,
+    Bucket.maxTTL: 1200,
+    Bucket.storageBackend: Bucket.StorageBackend.couchstore,
+    Bucket.evictionPolicy: Bucket.EvictionPolicy.FULL_EVICTION,
+    Bucket.compressionMode: Bucket.CompressionMode.ACTIVE,
+    "buckets": {
+        "default": {
+            MetaConstants.NUM_SCOPES_PER_BUCKET: 10,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 10,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 122100,
+            Bucket.ramQuotaMB: 512,
+            Bucket.storageBackend: Bucket.StorageBackend.magma,
+            Bucket.priority: Bucket.Priority.HIGH
+        }
+    }
+}
