1566942936823bb1adf1457bf3d6e3cab7c90074
Asad Zaidi <asad.zaidi@couchbase.com>
2021-12-01 12:45:52 +0000

    CBQE-7368: Factories for ns_server and kv limits
    Add factories which configure and produce throughput objects for all
    resource limits.
    
    Update LimitConfig to account for all ns_server and kv limits
    
    Change-Id: I8202283417b0cf55b03681e1f488bcc1a0168dd1
    Reviewed-on: https://review.couchbase.org/c/TAF/+/166767
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/limits/resource_producer.py
+++ pytests/limits/resource_producer.py
@@ -1,6 +1,13 @@
+from collections import defaultdict
 from limits.ns_server_num_concurrent_requests import NsServerNumConcurrentRequests
 from limits.ns_server_ingress import NsServerIngress
 from limits.ns_server_egress import NsServerEgress
+from limits.ns_server_num_collections import NsServerNumCollections
+from limits.kv_num_connections import KvNumConnections
+from limits.kv_egress import KvEgress
+from limits.kv_ingress import KvIngress
+from limits.kv_ops import KvOps
+from limits.kv_data_size import KvDataSize
 
 
 class UserResourceProducer(object):
@@ -20,6 +27,18 @@ class UserResourceProducer(object):
             return NsServerEgress(user, node)
         elif self.resource_name == "ns_server_ingress":
             return NsServerIngress(user, node)
+        elif self.resource_name == "ns_server_num_collections":
+            return NsServerNumCollections("default", user.scope, user, node)
+        elif self.resource_name == "kv_num_connections":
+            return KvNumConnections(user, node)
+        elif self.resource_name == "kv_egress":
+            return KvEgress(user, node)
+        elif self.resource_name == "kv_ingress":
+            return KvIngress(user, node)
+        elif self.resource_name == "kv_ops":
+            return KvOps(user, node)
+        elif self.resource_name == "kv_data_size":
+            return KvDataSize("default", user.scope, user, node)
         else:
             raise ValueError(
                 "unknown resource name '{}'".format(
@@ -28,3 +47,38 @@ class UserResourceProducer(object):
     def get_resource_stat_monitor(self, user, node):
         """ Returns an object that allows you monitor this statisic for the given user and node """
         pass
+
+
+class LimitConfig:
+    def __init__(self):
+        self.user_config = defaultdict(dict)
+        self.scope_config = defaultdict(dict)
+
+    def set_limit(self, resource_name, value):
+        if resource_name == "ns_server_num_concurrent_requests":
+            self.user_config["clusterManager"]["num_concurrent_requests"] = value
+        elif resource_name == "ns_server_ingress":
+            self.user_config["clusterManager"]["ingress_mib_per_min"] = value
+        elif resource_name == "ns_server_egress":
+            self.user_config["clusterManager"]["egress_mib_per_min"] = value
+        elif resource_name == "ns_server_num_collections":
+            self.scope_config["clusterManager"]["num_collections"] = value
+        elif resource_name == "kv_num_connections":
+            self.user_config["kv"]["num_connections"] = value
+        elif resource_name == "kv_egress":
+            self.user_config["kv"]["egress_mib_per_min"] = value
+        elif resource_name == "kv_ingress":
+            self.user_config["kv"]["ingress_mib_per_min"] = value
+        elif resource_name == "kv_ops":
+            self.user_config["kv"]["num_ops_per_min"] = value
+        elif resource_name == "kv_data_size":
+            self.scope_config["kv"]["data_size"] = value
+        else:
+            raise ValueError(
+                "unknown resource name '{}'".format(resource_name))
+
+    def get_user_config(self):
+        return self.user_config
+
+    def get_scope_config(self):
+        return self.scope_config
