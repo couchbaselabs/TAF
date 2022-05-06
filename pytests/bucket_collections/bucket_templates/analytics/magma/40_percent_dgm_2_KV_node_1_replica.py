4d760604568f92ec6bc2e30fd3e9e6eaa65a3ff8
umang <umang.agrawal@couchbase.com>
2022-05-06 21:23:52 +0530

    Modifying number of scopes and collections to be created for magma bucket for CBAS tests
    Change-Id: I1893b0670eef178d16179545e9f9f06393e991ab
    Reviewed-on: https://review.couchbase.org/c/TAF/+/174588
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ashwin <ashwin.trojan@gmail.com>
    Reviewed-on: https://review.couchbase.org/c/TAF/+/174930
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/bucket_collections/bucket_templates/analytics/magma/40_percent_dgm_2_KV_node_1_replica.py
+++ pytests/bucket_collections/bucket_templates/analytics/magma/40_percent_dgm_2_KV_node_1_replica.py
@@ -23,8 +23,8 @@ spec = {
     "buckets": {
         "default": {
             MetaConstants.NUM_SCOPES_PER_BUCKET: 10,
-            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 25,
-            MetaConstants.NUM_ITEMS_PER_COLLECTION: 5300,
+            MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 2,
+            MetaConstants.NUM_ITEMS_PER_COLLECTION: 65600,
             Bucket.ramQuotaMB: 512,
             Bucket.storageBackend: Bucket.StorageBackend.magma,
             Bucket.priority: Bucket.Priority.HIGH,
