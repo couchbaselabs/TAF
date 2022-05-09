dc359874884ff1001a7630870341807c680f672b
sumedhpb <sumedhpb8@gmail.com>
2022-05-09 10:46:22 +0530

    CBQE-7612: 2/n Support passing node and build number
    Build number is required to pass install a version of CB server
    during docker build command. Node IP is used to identify the VM on
    which the docker container will be run.
    
    Change-Id: Icd65f2bc49607d2480eb20b447e4a77a7d5c1ad9
    Reviewed-on: https://review.couchbase.org/c/TAF/+/174624
    Reviewed-by: Ashwin <ashwin.trojan@gmail.com>
    Tested-by: Sumedh Basarkod <sumedhpb8@gmail.com>

--- scripts/containers/couchbase/CentOS7/install_couchbase_container.py
+++ scripts/containers/couchbase/CentOS7/install_couchbase_container.py
@@ -1,52 +1,73 @@
 """
 Builds a 'couchbase-neo' docker image on a given VM from the corresponding Dockerfile
 """
-
-try:
-    import paramiko
-except Exception as e:
-    print("no paramiko library present")
+import paramiko
+import getopt
+import sys
 
 
-def build_image():
+def build_image(ssh, build):
     # ToDo instead of assuming the dockerfile to be present on the VM...,
     # TODo ...copy the dockerfile from slave to the VM
     print("Building an image with name couchbase-neo")
-    cmd = "cd /root/cb_container/CentOS7/; docker build -t couchbase-neo ."
+    cmd = "cd /root/cb_container/CentOS7/; docker build --build-arg BUILD_NO=" + build + \
+          " -t couchbase-neo ."
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
     print(ssh_stdout.read(), ssh_stderr.read())
 
 
-def remove_image():
+def remove_image(ssh):
     print("Removing any image with name couchbase-neo")
     cmd = "docker image rm couchbase-neo"
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
     print(ssh_stdout.read(), ssh_stderr.read())
 
 
-def stop_all_containers():
+def stop_all_containers(ssh):
     print("Stopping all container")
     cmd = "docker stop $(docker ps -a -q)"
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
     print(ssh_stdout.read(), ssh_stderr.read())
 
 
-def remove_all_containers():
+def remove_all_containers(ssh):
     print("Removing all containers")
     cmd = "docker rm $(docker ps -a -q)"
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
     print(ssh_stdout.read(), ssh_stderr.read())
 
 
-if __name__ == "__main__":
-    # TODO take this from ini file
-    serv = "172.23.122.100"
+def main(argv):
+    test_input = dict()
+    try:
+        opts, args = getopt.getopt(argv, "hn:b:")
+    except getopt.GetoptError:
+        print('python2 scripts/containers/couchbase/CentOS7/install_couchbase_container.py '
+              '-n node_ip -b neo_build_number')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('python2 scripts/containers/couchbase/CentOS7/install_couchbase_container.py '
+                  '-n node_ip -b neo_build_number')
+            sys.exit()
+        elif opt in "-n":
+            test_input["node"] = arg
+        elif opt in "-b":
+            test_input["build"] = arg
+
+    serv = test_input["node"]
+    build = test_input["build"]
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(serv, username="root", password="couchbase")
-    stop_all_containers()
-    remove_all_containers()
-    remove_image()
-    build_image()
+    stop_all_containers(ssh)
+    remove_all_containers(ssh)
+    remove_image(ssh)
+    build_image(ssh, build)
     ssh.close()
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1:]
+    main(argv=arg)
