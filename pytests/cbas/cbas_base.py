1925262c713a006bc9543d41d2117d21cb5beead
umang <umang.agrawal@couchbase.com>
2022-06-28 12:32:39 +0530

    Fix for bucket ram allocation
    Change-Id: I44fa872abc708355eb9e82731d5199e2a34d7e01
    Reviewed-on: https://review.couchbase.org/c/TAF/+/176785
    Tested-by: Ashwin <ashwin.trojan@gmail.com>
    Reviewed-by: Ashwin <ashwin.trojan@gmail.com>
    Reviewed-on: https://review.couchbase.org/c/TAF/+/182255
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- pytests/cbas/cbas_base.py
+++ pytests/cbas/cbas_base.py
@@ -626,13 +626,13 @@ class CBASBaseTest(BaseTestCase):
                 bucket_spec[Bucket.storageBackend] = self.bucket_storage
             elif key == "compression_mode":
                 bucket_spec[Bucket.compressionMode] = self.compression_mode
-            elif key == "bucket_size":
-                if self.bucket_size == "auto":
-                    cluster_info = cluster.rest.get_nodes_self()
-                    kv_quota = cluster_info.__getattribute__(CbServer.Settings.KV_MEM_QUOTA)
-                    self.bucket_size = (kv_quota // int(
-                        self.input.param("num_buckets", 1))) * 0.9
-                bucket_spec[Bucket.ramQuotaMB] = self.bucket_size
+        if "bucket_size" in self.input.test_params:
+            if self.bucket_size == "auto":
+                cluster_info = cluster.rest.get_nodes_self()
+                kv_quota = cluster_info.__getattribute__(CbServer.Settings.KV_MEM_QUOTA)
+                self.bucket_size = int((kv_quota // bucket_spec[
+                    MetaConstants.NUM_BUCKETS]) * 0.9)
+            bucket_spec[Bucket.ramQuotaMB] = self.bucket_size
 
     @staticmethod
     def create_or_delete_users(rbac_util, rbac_users_created, delete=False):
