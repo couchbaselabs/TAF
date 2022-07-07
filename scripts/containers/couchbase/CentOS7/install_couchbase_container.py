22a9ad40f891cf6b22e398882215bf8bb97d224d
sumedhpb <sumedhpb8@gmail.com>
2022-07-07 14:35:51 +0530

    CBQE-7612:7/n Add suport to build docker image of any branch & build
    Taking parameters of branch(eg:neo, master, elixir etc) using -f,
    and version(eg: 7.1.1, 7.1.2) using -v to locate the appro build
    
    Change-Id: I03c9d8b10d28d7b42268cb630f2430312445168b
    Reviewed-on: https://review.couchbase.org/c/TAF/+/177193
    Tested-by: Sumedh Basarkod <sumedhpb8@gmail.com>
    Reviewed-by: Sumedh Basarkod <sumedhpb8@gmail.com>
    Reviewed-by: Umang Agrawal <umang.agrawal@couchbase.com>

--- scripts/containers/couchbase/CentOS7/install_couchbase_container.py
+++ scripts/containers/couchbase/CentOS7/install_couchbase_container.py
@@ -6,11 +6,16 @@ import getopt
 import sys
 
 
-def build_image(ssh, build):
+def build_image(ssh, branch, version, build):
     # ToDo instead of assuming the dockerfile to be present on the VM...,
     # TODo ...copy the dockerfile from slave to the VM
+    # TODo..change the image name from 'couchbase-neo' to something more appropriate...
+    # TODo...as we may not necessarily be building a neo image here
     print("Building an image with name couchbase-neo")
-    cmd = "cd /root/cb_container/CentOS7/; docker build --build-arg BUILD_NO=" + build + \
+    cmd = "cd /root/cb_container/CentOS7/; docker build" \
+          " --build-arg FLAVOR=" + branch + \
+          " --build-arg VERSION=" + version + \
+          " --build-arg BUILD_NO=" + build + \
           " -t couchbase-neo ."
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
     print(ssh_stdout.read(), ssh_stderr.read())
@@ -40,22 +45,28 @@ def remove_all_containers(ssh):
 def main(argv):
     test_input = dict()
     try:
-        opts, args = getopt.getopt(argv, "hn:b:")
+        opts, args = getopt.getopt(argv, "hn:f:v:b:")
     except getopt.GetoptError:
         print('python2 scripts/containers/couchbase/CentOS7/install_couchbase_container.py '
-              '-n node_ip -b neo_build_number')
+              '-n node_ip -f branch -v version -b build_number')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print('python2 scripts/containers/couchbase/CentOS7/install_couchbase_container.py '
-                  '-n node_ip -b neo_build_number')
+                  '-n node_ip -f branch -v version -b build_number')
             sys.exit()
         elif opt in "-n":
             test_input["node"] = arg
+        elif opt in "-f":
+            test_input["branch"] = arg
+        elif opt in "-v":
+            test_input["version"] = arg
         elif opt in "-b":
             test_input["build"] = arg
 
     serv = test_input["node"]
+    branch = test_input["branch"]
+    version = test_input["version"]
     build = test_input["build"]
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
@@ -64,7 +75,7 @@ def main(argv):
     stop_all_containers(ssh)
     remove_all_containers(ssh)
     remove_image(ssh)
-    build_image(ssh, build)
+    build_image(ssh, branch, version, build)
     ssh.close()
 
 
