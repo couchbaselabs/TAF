500c240fc6f9e6dc3dbd0345f4f57ebfaaac5d5b
Ashwin G <ashwin.trojan@gmail.com>
2022-07-08 20:42:50 +0530

    Adding functional test for MB-52793
    Change-Id: I0b2c387a15250f39ccca02cf86cd23f11dd65af6
    Reviewed-on: https://review.couchbase.org/c/TAF/+/177290
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/epengine/basic_ops.py
+++ pytests/epengine/basic_ops.py
@@ -9,17 +9,20 @@ from basetestcase import ClusterSetup
 from cb_tools.cbepctl import Cbepctl
 from cb_tools.cbstats import Cbstats
 from cb_tools.mc_stat import McStat
+from cluster_utils.cluster_ready_functions import CBCluster
 from couchbase_helper.documentgenerator import doc_generator
 from couchbase_helper.durability_helper import DurabilityHelper
 from error_simulation.cb_error import CouchbaseError
 
 from mc_bin_client import MemcachedClient, MemcachedError
+from membase.api.rest_client import RestConnection
 from remote.remote_util import RemoteMachineShellConnection
 from sdk_client3 import SDKClient
 from sdk_exceptions import SDKException
 from table_view import TableView
 
 from java.lang import RuntimeException
+
 
 """
 Capture basic get, set operations, also the meta operations.
@@ -1537,6 +1540,130 @@ class basic_ops(ClusterSetup):
 
         shell.disconnect()
         self.validate_test_failure()
+
+    def test_store_value_del_updates_datatype(self):
+        """
+        1. Bucket with replica=1
+        2. Create document with system_xattr set
+        3. Delete the document on active (system_xattr is still accessible)
+        4. Load bucket into DGM to trigger the deleted doc eviction
+        5. Rebalance_in a new node such that the vb with the deleted doc
+           becomes as replica vb (Still holding the doc in the HT)
+        6. Remove the sys_xattr from the deleted doc and rebalance_out the
+           new node so the replica switches back to active vb
+        7. Now the dbdump the current replica node should display the
+           data_type=raw instead of 'xattr'
+        8. Create a new cluster (single node) for XDCR with default bucket
+        9. Start replication from the source to the xdcr bucket
+           and expect no crash till the xdcr replication is complete.
+
+        Note: Crash was seen in the ticket due to the fact that the source
+              bucket was trying to read the document in memory with type
+              xttr instead of raw during the xdcr replication process
+
+        Ref: MB-52793
+        """
+        key, val = "test_key", {"f": "value"}
+        sub_doc = ["_key", "value"]
+        key_vb = self.bucket_util.get_vbucket_num_for_key(key)
+        bucket = self.cluster.buckets[0]
+        in_node = self.cluster.servers[1]
+        num_items = 0
+        err = None
+
+        client = SDKClient([self.cluster.master], bucket)
+
+        self.log.info("Creating tombstone '%s' with sys-xattr" % key)
+        # Create a document
+        client.crud(DocLoading.Bucket.DocOps.CREATE, key, val)
+        # Load sys-xattr for the document
+        client.crud(DocLoading.Bucket.SubDocOps.INSERT,
+                    key, sub_doc, xattr=True)
+        # Wait for ep_queue_size to become Zero
+        self.bucket_util._wait_for_stats_all_buckets(self.cluster, [bucket])
+        # Delete the document
+        client.crud(DocLoading.Bucket.DocOps.DELETE, key)
+        # Wait for ep_queue_size to become Zero
+        self.bucket_util._wait_for_stats_all_buckets(self.cluster, [bucket])
+        client.crud(DocLoading.Bucket.SubDocOps.LOOKUP, key, sub_doc[0],
+                    xattr=True, access_deleted=True)
+
+        self.log.info("Loading docs to make the tombstone doc as non-resident")
+        is_resident = True
+        start_index = 0
+        batch_size = 1000
+        shell = RemoteMachineShellConnection(self.cluster.master)
+        cbstat = Cbstats(shell)
+
+        hash_dump_cmd = \
+            "%s -u %s -p %s localhost:%d raw \"_hash-dump %d\" | grep %s" \
+            % (cbstat.cbstatCmd, cbstat.username, cbstat.password,
+               self.cluster.master.memcached_port, key_vb, key)
+
+        # Loading docs until the target doc is evicted from memory
+        while is_resident:
+            doc_gen = doc_generator("docs", start_index, batch_size,
+                                    key_size=100, doc_size=10240,
+                                    target_vbucket=[key_vb])
+            while doc_gen.has_next():
+                d_key, val = doc_gen.next()
+                client.crud(DocLoading.Bucket.DocOps.CREATE, d_key, val)
+
+            output, _ = shell.execute_command(hash_dump_cmd)
+            if " X.. .D..Cm " in output[0]:
+                is_resident = False
+            start_index = doc_gen.key_counter
+            num_items += batch_size
+
+        result = self.task.rebalance([self.cluster.master], [in_node], [])
+        self.assertTrue(result, "Rebalance_in failed")
+
+        replica_vbs = cbstat.vbucket_list(bucket.name, Bucket.vBucket.REPLICA)
+        self.assertTrue(key_vb in replica_vbs, "vBucket is still active vb")
+
+        client.crud(DocLoading.Bucket.SubDocOps.REMOVE, key, sub_doc[0],
+                    xattr=True, access_deleted=True)
+        client.close()
+
+        db_dump = "/".join(cbstat.cbstatCmd.split("/")[:-1]) + "/couch_dbdump"
+        output, _ = shell.execute_command(
+            "%s --json "
+            "/opt/couchbase/var/lib/couchbase/data/default/%d.couch.* "
+            "| grep %s" % (db_dump, key_vb, key))
+        output = json.loads(output[0])
+        if output["datatype_as_text"] != ["raw"]:
+            err = "Datatype mismatch: %s" % output
+
+        # Rebalance out the new node
+        result = self.task.rebalance(self.cluster.servers[:2],
+                                     to_add=[], to_remove=[in_node])
+        self.assertTrue(result, "Rebalance_out failed")
+
+        self.log.info("Starting XDCR replication")
+        xdcr_cluster = CBCluster("C2", servers=[in_node])
+        RestConnection(in_node).init_node()
+        self.create_bucket(xdcr_cluster)
+        rest = RestConnection(self.cluster.master)
+        rest.add_remote_cluster(xdcr_cluster.master.ip,
+                                xdcr_cluster.master.port,
+                                xdcr_cluster.master.rest_username,
+                                xdcr_cluster.master.rest_password,
+                                xdcr_cluster.master.ip)
+        rest.start_replication("continuous",
+                               self.cluster.buckets[0].name,
+                               xdcr_cluster.master.ip,
+                               toBucket=xdcr_cluster.buckets[0].name)
+        try:
+            self.log.info("Waiting for all items to get replicated")
+            self.bucket_util.verify_stats_all_buckets(xdcr_cluster,
+                                                      num_items, timeout=180)
+        finally:
+            self.log.info("Removing xdcr bucket and remote references")
+            self.bucket_util.delete_all_buckets(xdcr_cluster)
+            rest.remove_all_replications()
+            rest.remove_all_remote_clusters()
+        if err:
+            self.fail(err)
 
     def verify_stat(self, items, value="active"):
         mc = MemcachedClient(self.cluster.master.ip,
