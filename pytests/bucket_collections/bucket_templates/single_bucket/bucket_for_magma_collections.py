3d982cf00a6db2b6686f8446eac6c7fcdbf1ad06
Balakumaran G <balakumaran.gopal@couchbase.com>
2022-07-19 12:21:50 +0530

    CBQE-7642: Ensure the default bucket spec file used adheres to serverless limits
    Change-Id: I45d0bf7cc3ce078b3883237972e067a1c8fc0fcb
    Reviewed-on: https://review.couchbase.org/c/TAF/+/177715
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Ashwin <ashwin.trojan@gmail.com>

--- pytests/bucket_collections/bucket_templates/single_bucket/bucket_for_magma_collections.py
+++ pytests/bucket_collections/bucket_templates/single_bucket/bucket_for_magma_collections.py
@@ -4,8 +4,8 @@ from collections_helper.collections_spec_constants import MetaConstants
 spec = {
     MetaConstants.NUM_BUCKETS: 1,
     MetaConstants.NUM_SCOPES_PER_BUCKET: 10,
-    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 20,
-    MetaConstants.NUM_ITEMS_PER_COLLECTION: 15000,
+    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 5,
+    MetaConstants.NUM_ITEMS_PER_COLLECTION: 60000,
     MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
 
     Bucket.bucketType: Bucket.Type.MEMBASE,
