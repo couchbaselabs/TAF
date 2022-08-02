28c6be2cfed52b2e8ce6ac6d870f3e811af84a96
pulkitM1 <pulkit.matta@couchbase.com>
2022-08-02 14:20:57 +0530

    Small Fix for rebalance fail during auto failover
    Change-Id: I35312c9c625d295ea7b0063fca24c124909055d1
    Reviewed-on: https://review.couchbase.org/c/TAF/+/178322
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- lib/Jython_tasks/task.py
+++ lib/Jython_tasks/task.py
@@ -5006,6 +5006,7 @@ class AutoFailoverNodesFailureTask(Task):
         return False, None
 
     def get_failover_count(self):
+        self.sleep(10,"Waiting for node status to be updated")
         rest = RestConnection(self.master)
         cluster_status = rest.cluster_status()
         failover_count = 0
