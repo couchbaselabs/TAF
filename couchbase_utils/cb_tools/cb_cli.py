couchbaselabs/TAF

commit e773478d6ef185eddaf6c32b1101231943f0c34b

Avoid setting use_https/n2n_encryption values within lib
Change-Id: I68399f3edd4b50a9cbcfe89c31ecdb7eb186eb98
Reviewed-on: https://review.couchbase.org/c/TAF/+/178392
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

couchbase_utils/cb_tools/cb_cli.py
@@ -103,8 +103,6 @@ class CbCli(CbCmdBase):
         output, error = self._execute_cmd(cmd)
         if len(error) != 0:
             raise Exception(str(error))
-        CbServer.n2n_encryption = True
-        CbServer.use_https = False
         return output
 
     def disable_n2n_encryption(self):
@@ -115,8 +113,6 @@ class CbCli(CbCmdBase):
         output, error = self._execute_cmd(cmd)
         if len(error) != 0:
             raise Exception(str(error))
-        CbServer.n2n_encryption = False
-        CbServer.use_https = False
         return output
 
     def set_n2n_encryption_level(self, level="all"):
@@ -127,11 +123,6 @@ class CbCli(CbCmdBase):
         output, error = self._execute_cmd(cmd)
         if len(error) != 0:
             raise Exception(str(error))
-        if level != "strict":
-            CbServer.use_https = False
-        else:
-            CbServer.use_https = True
-            trust_all_certs()
         return output
 
     def get_n2n_encryption_level(self):
@@ -148,4 +139,3 @@ class CbCli(CbCmdBase):
             return security_dict["clusterEncryptionLevel"]
         else:
             return None
-

pytests/onPrem_basetestcase.py
@@ -283,6 +283,7 @@ class OnPremBaseTest(CouchbaseBaseTest):
             # Enforce tls on nodes of all clusters
             if (self.use_https and self.enforce_tls) \
                     and not self.skip_cluster_reset:
+                CbServer.n2n_encryption = True
                 for _, cluster in self.cb_clusters.items():
                     tasks = [self.node_utils.async_enable_tls(node)
                              for node in cluster.servers]
@@ -414,6 +415,7 @@ class OnPremBaseTest(CouchbaseBaseTest):
 
         # Disable n2n encryption on nodes of all clusters
         if self.use_https and self.enforce_tls:
+            CbServer.n2n_encryption = False
             for _, cluster in self.cb_clusters.items():
                 tasks = [self.node_utils.async_disable_tls(node)
                          for node in cluster.servers]

commit 59286adcc914a2e8084c9fd0a00fb7455726a7f3

Fixing the flag issue introduced by e773478d6ef185eddaf6c32b1101231943f0c34b
Change-Id: I1cc772c685e2307ba9dfa9f898fb45cbd97a5bf6
Reviewed-on: https://review.couchbase.org/c/TAF/+/178631
Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

couchbase_utils/cb_tools/cb_cli.py
@@ -1,6 +1,5 @@
 import json
 
-from security_config import trust_all_certs
 from BucketLib.bucket import Bucket
 from cb_tools.cb_tools_base import CbCmdBase
 from Cb_constants import CbServer
@@ -16,6 +15,12 @@ class CbCli(CbCmdBase):
         self.cli_flags = ""
         if no_ssl_verify:
             self.cli_flags += " --no-ssl-verify"
+
+    def __get_http_port(self):
+        port = self.port
+        if self.port == str(CbServer.ssl_port):
+            port = str(CbServer.port)
+        return port
 
     def create_bucket(self, bucket_dict, wait=False):
         """
@@ -97,7 +102,7 @@ class CbCli(CbCmdBase):
 
     def enable_n2n_encryption(self):
         cmd = "%s node-to-node-encryption -c %s:%s -u %s -p %s --enable" \
-             % (self.cbstatCmd, "localhost", self.port,
+             % (self.cbstatCmd, "localhost", self.__get_http_port(),
                 self.username, self.password)
         cmd += self.cli_flags
         output, error = self._execute_cmd(cmd)
@@ -107,20 +112,18 @@ class CbCli(CbCmdBase):
 
     def disable_n2n_encryption(self):
         cmd = "%s node-to-node-encryption -c %s:%s -u %s -p %s --disable" \
-              % (self.cbstatCmd, "localhost", self.port,
+              % (self.cbstatCmd, "localhost", self.__get_http_port(),
                  self.username, self.password)
         cmd += self.cli_flags
+
         output, error = self._execute_cmd(cmd)
         if len(error) != 0:
             raise Exception(str(error))
         return output
 
     def set_n2n_encryption_level(self, level="all"):
-        port = self.port
-        if self.port == CbServer.ssl_memcached_port:
-            port = CbServer.memcached_port
         cmd = "%s setting-security -c %s:%s -u %s -p %s --set --cluster-encryption-level %s" \
-              % (self.cbstatCmd, "localhost", port,
+              % (self.cbstatCmd, "localhost", self.__get_http_port(),
                  self.username, self.password, level)
         cmd += self.cli_flags
         output, error = self._execute_cmd(cmd)
@@ -130,7 +133,7 @@ class CbCli(CbCmdBase):
 
     def get_n2n_encryption_level(self):
         cmd = "%s setting-security -c %s:%s -u %s -p %s --get" \
-              % (self.cbstatCmd, "localhost", self.port,
+              % (self.cbstatCmd, "localhost", self.__get_http_port(),
                  self.username, self.password)
         cmd += self.cli_flags
         output, error = self._execute_cmd(cmd)

pytests/onPrem_basetestcase.py
@@ -331,6 +331,7 @@ class OnPremBaseTest(CouchbaseBaseTest):
             if (self.use_https and self.enforce_tls) \
                     and not self.skip_cluster_reset:
                 CbServer.n2n_encryption = True
+                CbServer.use_https = True
                 for _, cluster in self.cb_clusters.items():
                     tasks = [self.node_utils.async_enable_tls(node)
                              for node in cluster.servers]
@@ -469,12 +470,24 @@ class OnPremBaseTest(CouchbaseBaseTest):
 
         # Disable n2n encryption on nodes of all clusters
         if self.use_https and self.enforce_tls:
-            CbServer.n2n_encryption = False
             for _, cluster in self.cb_clusters.items():
                 tasks = [self.node_utils.async_disable_tls(node)
                          for node in cluster.servers]
                 for task in tasks:
                     self.task_manager.get_task_result(task)
+            # Set CbServer.use_https to False when
+            # n2n encryption level is not strict
+            CbServer.use_https = False
+            CbServer.n2n_encryption = False
+            for server in self.input.servers:
+                server.port = str(CbServer.port)
+                server.memcached_port = str(CbServer.memcached_port)
+                server.fts_port = str(CbServer.fts_port)
+                server.index_port = str(CbServer.index_port)
+                server.n1ql_port = str(CbServer.n1ql_port)
+                server.cbas_port = str(CbServer.cbas_port)
+                server.eventing_port = str(CbServer.eventing_port)
+
         if self.multiple_ca:
             CbServer.use_https = False
             for _, cluster in self.cb_clusters.items():