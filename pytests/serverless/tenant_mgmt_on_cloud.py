62b7de24e672761911dc3d76062ea3e48d51126a
pulkitM1 <pulkit.matta@couchbase.com>
2022-09-08 21:27:30 +0530

    CBQE-7652: Adding on-cloud serverless_db scaling tests
    Change-Id: I54cf0d0c851bbfba2c89a0ff7ca919390617c1f5
    Reviewed-on: https://review.couchbase.org/c/TAF/+/179741
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- pytests/serverless/tenant_mgmt_on_cloud.py
+++ pytests/serverless/tenant_mgmt_on_cloud.py
@@ -5,6 +5,10 @@ from Cb_constants import CbServer
 from capella_utils.serverless import CapellaUtils as ServerlessUtils
 from cluster_utils.cluster_ready_functions import Nebula
 from serverlessbasetestcase import OnCloudBaseTest
+from Cb_constants import CbServer
+from capellaAPI.capella.serverless.CapellaAPI import CapellaAPI
+from TestInput import TestInputServer
+from org.xbill.DNS import Lookup, Type
 
 from com.couchbase.test.sdk import Server, SDKClient
 from com.couchbase.test.sdk import SDKClient as NewSDKClient
@@ -14,6 +18,7 @@ from com.couchbase.test.docgen import \
 from couchbase.test.docgen import DRConstants
 
 from java.util import HashMap
+from java.util.concurrent import ExecutionException
 
 
 class TenantMgmtOnCloud(OnCloudBaseTest):
@@ -23,6 +28,75 @@ class TenantMgmtOnCloud(OnCloudBaseTest):
 
     def tearDown(self):
         super(TenantMgmtOnCloud, self).tearDown()
+
+    def __get_width_scenarios(self, target_scenario):
+        self.log.debug("Fetching spec for scenario: %s" % target_scenario)
+        scenario = dict()
+        scenario["single_bucket_incr"] = {
+            "spec": {
+                "bucket-1": {
+                    "width": 1,
+                    "update_spec": {
+                        "overRide": {
+                            "width": 2,
+                        }
+                    }
+                }
+            },
+        }
+        scenario["single_bucket_decr"] = {
+            "spec": {
+                "bucket-1": {
+                    "width": 2,
+                    "update_spec": {
+                        "overRide": {
+                            "width": 1,
+                        }
+                    }
+                }
+            },
+        }
+        scenario["multi_bucket_incr"] = {
+            "spec": {
+                "bucket-1": {
+                    "width": 1,
+                    "update_spec": {
+                        "overRide": {
+                            "width": 2,
+                        }
+                    }
+                },
+                "bucket-2": {
+                    "width": 1,
+                    "update_spec": {
+                        "overRide": {
+                            "width": 2,
+                        }
+                    }
+                }
+            },
+        }
+        scenario["multi_bucket_decr"] = {
+            "spec": {
+                "bucket-1": {
+                    "width": 2,
+                    "update_spec": {
+                        "overRide": {
+                            "width": 1,
+                        }
+                    }
+                },
+                "bucket-2": {
+                    "width": 2,
+                    "update_spec": {
+                        "overRide": {
+                            "width": 1,
+                        }
+                    }
+                }
+            },
+        }
+        return scenario[target_scenario]
 
     def __get_serverless_bucket_obj(self, db_name, width, weight,
                                     num_vbs=None):
@@ -42,9 +116,10 @@ class TenantMgmtOnCloud(OnCloudBaseTest):
 
         return bucket_obj
 
-    def __create_database(self):
-        bucket = self.__get_serverless_bucket_obj(
-            self.db_name, self.bucket_width, self.bucket_weight)
+    def __create_database(self, bucket=None):
+        if not bucket:
+            bucket = self.__get_serverless_bucket_obj(
+                self.db_name, self.bucket_width, self.bucket_weight)
         task = self.bucket_util.async_create_database(self.cluster, bucket)
         self.task_manager.get_task_result(task)
         nebula = Nebula(task.srv, task.server)
@@ -93,6 +168,88 @@ class TenantMgmtOnCloud(OnCloudBaseTest):
 
         # TODO: Load docs to new buckets
 
-        self.log.info("Removing '%s' buckets")
+        self.log.info("Removing '%s' buckets" % new_buckets)
         for bucket in new_buckets:
             ServerlessUtils.delete_database(self.pod, self.tenant, bucket.name)
+
+    def test_create_database_negative(self):
+        """
+        1. Create serverless database using invalid name and validate
+        2. Create serverless db with name of one character
+        :return:
+        """
+        invalid_char_err = \
+            "{\"errorType\":\"ErrDatabaseNameInvalidChars\"," \
+            "\"message\":\"Not able to create serverless database. " \
+            "The name of the database contains one or more invalid " \
+            "characters ','. Database names can only contain " \
+            "alphanumeric characters, space, and hyphen.\"}"
+        name_too_short_err = \
+            "{\"errorType\":\"ErrDatabaseNameTooShort\"," \
+            "\"message\":\"Not able to create serverless database. " \
+            "The name provided must be at least two characters in length. " \
+            "Please try again.\"}"
+        bucket = self.__get_serverless_bucket_obj("123,", 1, 30)
+        task = self.bucket_util.async_create_database(self.cluster, bucket)
+        try:
+            self.task_manager.get_task_result(task)
+        except ExecutionException as exception:
+            if invalid_char_err not in str(exception):
+                self.fail("Exception mismatch. Got::%s" % exception)
+
+        bucket = self.__get_serverless_bucket_obj("1", 1, 30)
+        task = self.bucket_util.async_create_database(self.cluster, bucket)
+        try:
+            self.task_manager.get_task_result(task)
+        except ExecutionException as exception:
+            if name_too_short_err not in str(exception):
+                self.fail("Exception mismatch. Got::%s" % exception)
+
+    def test_update_bucket_width(self):
+        def verify_bucket_scaling():
+            self.sleep(120, "Waiting for bucket to complete scaling")
+            for t_key in scenario_dict["spec"].keys():
+                t_bucket = name_key_map[t_key]
+                srv = ServerlessUtils.get_database_nebula_endpoint(
+                    self.cluster.pod, self.cluster.tenant, t_bucket.name)
+                self.bucket_util.update_bucket_nebula_servers(
+                    self.cluster, Nebula(srv, t_bucket.servers[0]),
+                    t_bucket)
+                if "update_spec" in scenario_dict["spec"][t_key]:
+                    self.assertTrue(
+                        len(self.cluster.bucketDNNodes[t_bucket]) == (
+                                scenario_dict["spec"][t_key]["update_spec"]
+                                ["overRide"]["width"]
+                                * CbServer.Serverless.KV_SubCluster_Size),
+                        "Bucket scaling failed")
+                else:
+                    self.assertTrue(
+                        len(self.cluster.bucketDNNodes[t_bucket]) == (
+                            scenario_dict["spec"][t_key]["width"]
+                            * CbServer.Serverless.KV_SubCluster_Size),
+                        "Bucket scaling failed")
+
+        index = 0
+        name_key_map = dict()
+        token = self.input.capella.get("token")
+        target_scenario = self.input.param("target_scenario")
+
+        capella_api = CapellaAPI(self.pod.url_public, None, None, token)
+        scenario_dict = self.__get_width_scenarios(target_scenario)
+        for key in scenario_dict["spec"].keys():
+            bucket = self.__get_serverless_bucket_obj(
+                key, scenario_dict["spec"][key]["width"],
+                self.bucket_weight)
+            self.__create_database(bucket)
+            name_key_map[key] = self.cluster.buckets[index]
+            index += 1
+
+        for key in scenario_dict["spec"].keys():
+            if "update_spec" in scenario_dict["spec"][key]:
+                resp = capella_api.update_database(
+                    name_key_map[key].name,
+                    scenario_dict["spec"][key]["update_spec"])
+                self.assertTrue(resp.status_code == 200,
+                                "Update Api failed")
+
+        verify_bucket_scaling()
