90064aa4611cdce45b704f853a97a42845f2e76a
Raghav S K <raghav.sk@couchbase.com>
2023-04-06 14:43:03 +0530

    Adding ubuntu22, rhel9 and oel9 to lib/testconstants/py
    Change-Id: I230cc1b43090afdcc8f6f26ec459d95245f86e3d
    Reviewed-on: https://review.couchbase.org/c/TAF/+/189330
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- lib/testconstants.py
+++ lib/testconstants.py
@@ -195,8 +195,8 @@ MACOS_NAME = {"10.10": "Yosemite", "10.11": "El Capitan", "10.12": "Sierra",
               "10.13": "High Sierra", "10.14": "Mojave", "10.15": "Catalina"}
 
 SYSTEMD_SERVER = ["centos 8", "centos 7", "suse 12", "suse 15", "ubuntu 16.04",
-                  "ubuntu 18.04", "ubuntu 20.04", "debian 8", "debian 9",
-                  "debian 10", "debian 11", "rhel 8", "oel 7", "oel 8"]
+                  "ubuntu 18.04", "ubuntu 20.04", "ubuntu 22.04", "debian 8", "debian 9",
+                  "debian 10", "debian 11", "rhel 8", "rhel 9", "oel 7", "oel 8", "oel 9"]
 WIN_NUM_ERLANG_PROCESS = 4
 WIN_MEMBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Membase/Server/var/lib/membase/data/'
 WIN_COUCHBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Couchbase/Server/var/lib/couchbase/data/'
