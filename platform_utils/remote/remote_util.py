2043d36c9168cf53954da3db12378078159a1fac
Ashwin <ashwin.govindarajulu@couchbase.com>
2023-05-04 13:46:30 +0530

    CBQE-7877: Updating if up/down to use only eth0 interface
    Change-Id: Ib5a014f56970622af9c0cbf44eb1ff670ab39d5a
    Reviewed-on: https://review.couchbase.org/c/TAF/+/190501
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Umang Agrawal <umang.agrawal@couchbase.com>

--- platform_utils/remote/remote_util.py
+++ platform_utils/remote/remote_util.py
@@ -293,7 +293,7 @@ class RemoteMachineShellConnection:
         os_type = self.info.type.lower()
         if os_type == "unix" or os_type == Linux.NAME:
             if self.info.distribution_type.lower() == "ubuntu":
-                command = "ifdown -a && sleep {} && ifup -a eth0"
+                command = "ifdown eth0 && sleep {} && ifup eth0"
             else:
                 command = "service network stop && sleep {} && " \
                           "service network start"
