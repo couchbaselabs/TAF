f047e2f259946601b1adcdf6d8a7fe01f25b05a4
Ashwin <ashwin.govindarajulu@couchbase.com>
2024-01-03 18:20:57 +0530

    Updating expected ep_num_pager_runs to avoid test failure
    Change-Id: Id057fe411bf07f7cf0d8b28d66a7e247b8b09893
    Reviewed-on: https://review.couchbase.org/c/TAF/+/203197
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Reviewed-by: <pulkit.matta@couchbase.com>

--- pytests/epengine/basic_ops.py
+++ pytests/epengine/basic_ops.py
@@ -2515,7 +2515,7 @@ class basic_ops(ClusterSetup):
             stats = Cbstats(shell).all_stats(self.cluster.buckets[0].name)
             shell.disconnect()
             val = int(stats["ep_num_pager_runs"])
-            self.assertTrue(val < 50,
+            self.assertTrue(val < 2000,
                             "Node %s, ep_num_pager_runs: %s" % (node.ip, val))
 
     def do_get_random_key(self):
