a22a8c8d312e5b88517cdec57f90d8eafb7635ee
Ashwin <ashwin.govindarajulu@couchbase.com>
2024-02-21 15:11:36 +0530

    Removing flaky test from epeng/basic_ops
    Reason: This value is relative and hard to predict in realtime
    due to memory & CPU constraints
    
    Change-Id: I641d9a120dc481957cec01565fcd339c07792b15
    Reviewed-on: https://review.couchbase.org/c/TAF/+/205979
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/epengine/basic_ops.py
+++ pytests/epengine/basic_ops.py
@@ -2546,25 +2546,6 @@ class basic_ops(ClusterSetup):
         for _, n_info in node_info.items():
             n_info["shell"].disconnect()
 
-    def test_ephemeral_num_pager_runs(self):
-        load_gen = doc_generator(self.key, 0, 320000, doc_size=1024)
-        for i in range(20):
-            load_task = self.task.async_load_gen_docs(
-                self.cluster, self.cluster.buckets[0], load_gen,
-                DocLoading.Bucket.DocOps.UPDATE,
-                batch_size=20, process_concurrency=8, print_ops_rate=False,
-                skip_read_on_error=True, suppress_error_table=True,
-                sdk_client_pool=self.sdk_client_pool)
-            self.task_manager.get_task_result(load_task)
-
-        for node in self.cluster.nodes_in_cluster:
-            cbstat = Cbstats(node)
-            stats = cbstat.all_stats(self.cluster.buckets[0].name)
-            cbstat.shellConn.disconnect()
-            val = int(stats["ep_num_pager_runs"])
-            self.assertTrue(val < 2000,
-                            "Node %s, ep_num_pager_runs: %s" % (node.ip, val))
-
     def do_get_random_key(self):
         # MB-31548, get_Random key gets hung sometimes.
         mc = MemcachedClient(self.cluster.master.ip,
