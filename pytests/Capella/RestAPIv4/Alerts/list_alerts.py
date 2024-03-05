8f81199cb7450201eaa101babf484ee8388f3760
vipbhardwaj <vipul.bhardwaj@couchbase.com>
2024-03-05 12:05:09 +0530

    LIST endpoint for alerts API testfile
    Change-Id: Iab185daae6eb939babcbeaf4d6d299d3aa38ce9c
    Reviewed-on: https://review.couchbase.org/c/TAF/+/206692
    Reviewed-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Reviewed-by: Umang Agrawal <umang.agrawal@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Reviewed-on: https://review.couchbase.org/c/TAF/+/206958
    Tested-by: <pulkit.matta@couchbase.com>
    Reviewed-by: <pulkit.matta@couchbase.com>

---
+++ pytests/Capella/RestAPIv4/Alerts/list_alerts.py
@@ -0,0 +1,337 @@
+"""
+Created on February 26, 2024
+
+@author: Vipul Bhardwaj
+"""
+
+from pytests.Capella.RestAPIv4.Alerts.get_alerts import GetAlert
+
+class ListAlert(GetAlert):
+
+    def setUp(self, nomenclature="Alerts_List"):
+        GetAlert.setUp(self, nomenclature)
+
+        # Instantiate expected result for List endpoint
+        self.expected_res = {
+            "cursor": {
+                "hrefs": {
+                    "first": None,
+                    "last": None,
+                    "next": None,
+                    "previous": None
+                },
+                "pages": {
+                    "last": None,
+                    "next": None,
+                    "page": None,
+                    "perPage": None,
+                    "previous": None,
+                    "totalItems": None
+                }
+            },
+            "data": [
+                self.expected_res
+            ]
+        }
+
+    def tearDown(self):
+        super(ListAlert, self).tearDown()
+
+    def test_api_path(self):
+        testcases = [
+            {
+                "description": "Send call with valid path params"
+            }, {
+                "description": "Replace api version in URI",
+                "url": "/v3/organizations/{}/projects/{}/alertIntegrations",
+                "expected_status_code": 404,
+                "expected_error": {
+                    "errorType": "RouteNotFound",
+                    "message": "Not found"
+                }
+            }, {
+                "description": "Replace the last path param name in URI",
+                "url": "/v4/organizations/{}/projects/{}/alertIntegration",
+                "expected_status_code": 404,
+                "expected_error": "404 page not found"
+            }, {
+                "description": "Add an invalid segment to the URI",
+                "url": "/v4/organizations/{}/projects/{}/alertIntegrations"
+                       "/alert",
+                "expected_status_code": 400,
+                "expected_error": {
+                    "code": 1000,
+                    "hint": "Check if you have provided a valid URL and all "
+                            "the required params are present in the request "
+                            "body.",
+                    "httpStatusCode": 400,
+                    "message": "The server cannot or will not process the "
+                               "request due to something that is perceived to "
+                               "be a client error."
+                }
+            }, {
+                "description": "Send call with non-hex organizationID",
+                "invalid_organizationID": self.replace_last_character(
+                    self.organisation_id, non_hex=True),
+                "expected_status_code": 400,
+                "expected_error": {
+                    "code": 1000,
+                    "hint": "Check if you have provided a valid URL and all "
+                            "the required params are present in the request "
+                            "body.",
+                    "httpStatusCode": 400,
+                    "message": "The server cannot or will not process the "
+                               "request due to something that is perceived to "
+                               "be a client error."
+                }
+            }, {
+                "description": "Send call with non-hex projectID",
+                "invalid_projectID": self.replace_last_character(
+                    self.project_id, non_hex=True),
+                "expected_status_code": 400,
+                "expected_error": {
+                    "code": 1000,
+                    "hint": "Check if you have provided a valid URL and all "
+                            "the required params are present in the request "
+                            "body.",
+                    "httpStatusCode": 400,
+                    "message": "The server cannot or will not process the "
+                               "request due to something that is perceived to "
+                               "be a client error."
+                }
+            }
+        ]
+        failures = list()
+        for testcase in testcases:
+            self.log.info("Executing test: {}".format(testcase["description"]))
+            org = self.organisation_id
+            proj = self.project_id
+
+            if "url" in testcase:
+                self.capellaAPI.cluster_ops_apis.alerts_endpoint = \
+                    testcase["url"]
+            if "invalid_organizationID" in testcase:
+                org = testcase["invalid_organizationID"]
+            elif "invalid_projectID" in testcase:
+                proj = testcase["invalid_projectID"]
+
+            result = self.capellaAPI.cluster_ops_apis.list_alerts(org, proj)
+            if result.status_code == 429:
+                self.handle_rate_limit(int(result.headers["Retry-After"]))
+                result = self.capellaAPI.cluster_ops_apis.list_alerts(
+                    org, proj)
+
+            self.capellaAPI.cluster_ops_apis.alerts_endpoint = \
+                "/v4/organizations/{}/projects/{}/alertIntegrations"
+
+            self.validate_testcase(result, [200], testcase, failures, True,
+                                   self.expected_res, self.alert_id)
+
+        if failures:
+            for fail in failures:
+                self.log.warning(fail)
+            self.fail("{} tests FAILED out of {} TOTAL tests"
+                      .format(len(failures), len(testcases)))
+
+    def test_authorization(self):
+        self.api_keys.update(
+            self.create_api_keys_for_all_combinations_of_roles(
+                [self.project_id]))
+
+        resp = self.capellaAPI.org_ops_apis.create_project(
+            self.organisation_id, "Auth_Project")
+        if resp.status_code == 201:
+            other_project_id = resp.json()["id"]
+        else:
+            self.fail("Error while creating project")
+
+        testcases = []
+        for role in self.api_keys:
+            testcase = {
+                "description": "Calling API with {} role".format(role),
+                "token": self.api_keys[role]["token"],
+            }
+            if not any(element in [
+                "organizationOwner", "organizationMember", "projectOwner",
+                "projectManager", "projectViewer"] for
+                       element in self.api_keys[role]["roles"]):
+                testcase["expected_error"] = {
+                    "code": 1002,
+                    "hint": "Your access to the requested resource is denied. "
+                            "Please make sure you have the necessary "
+                            "permissions to access the resource.",
+                    "httpStatusCode": 403,
+                    "message": "Access Denied."
+                }
+                testcase["expected_status_code"] = 403
+            testcases.append(testcase)
+        self.auth_test_extension(testcases, other_project_id)
+
+        failures = list()
+        for testcase in testcases:
+            self.log.info("Executing test: {}".format(testcase["description"]))
+            header = dict()
+            self.auth_test_setup(testcase, failures, header,
+                                 self.project_id, other_project_id)
+            result = self.capellaAPI.cluster_ops_apis.list_alerts(
+                self.organisation_id, self.project_id, header)
+            if result.status_code == 429:
+                self.handle_rate_limit(int(result.headers["Retry-After"]))
+                result = self.capellaAPI.cluster_ops_apis.list_alerts(
+                    self.organisation_id, self.project_id, header)
+
+            self.validate_testcase(result, [200], testcase, failures, True,
+                                   self.expected_res, self.alert_id)
+
+        self.update_auth_with_api_token(self.org_owner_key["token"])
+        resp = self.capellaAPI.org_ops_apis.delete_project(
+            self.organisation_id, other_project_id)
+        if resp.status_code != 204:
+            self.log.error("Error while deleting project {}"
+                           .format(other_project_id))
+
+        if failures:
+            for fail in failures:
+                self.log.warning(fail)
+            self.fail("{} tests FAILED out of {} TOTAL tests"
+                      .format(len(failures), len(testcases)))
+
+    def test_query_parameters(self):
+        self.log.debug("Correct Params - OrgID: {}, ProjID: {} "
+                       .format(self.organisation_id, self.project_id))
+        testcases = 0
+        failures = list()
+        for combination in self.create_path_combinations(
+                self.organisation_id, self.project_id):
+            testcases += 1
+            testcase = {
+                "description": "OrganizationID: {}, ProjectID: {}"
+                .format(str(combination[0]), str(combination[1])),
+                "organizationID": combination[0],
+                "projectID": combination[1]
+            }
+            if not (combination[0] == self.organisation_id and
+                    combination[1] == self.project_id):
+                if combination[0] == "":
+                    testcase["expected_status_code"] = 404
+                    testcase["expected_error"] = "404 page not found"
+                elif combination[1] == "" or any(variable in [
+                    int, bool, float, list, tuple, set, type(None)] for
+                         variable in [type(combination[0]),
+                                      type(combination[1])]):
+                    testcase["expected_status_code"] = 400
+                    testcase["expected_error"] = {
+                        "code": 1000,
+                        "hint": "Check if you have provided a valid URL and "
+                                "all the required params are present in the "
+                                "request body.",
+                        "httpStatusCode": 400,
+                        "message": "The server cannot or will not process the "
+                                   "request due to something that is "
+                                   "perceived to be a client error."
+                    }
+                elif combination[0] != self.organisation_id:
+                    testcase["expected_status_code"] = 403
+                    testcase["expected_error"] = {
+                        "code": 1002,
+                        "hint": "Your access to the requested resource is "
+                                "denied. Please make sure you have the "
+                                "necessary permissions to access the "
+                                "resource.",
+                        "httpStatusCode": 403,
+                        "message": "Access Denied."
+                    }
+                else:
+                    testcase["expected_status_code"] = 404
+                    testcase["expected_error"] = {
+                        "code": 2000,
+                        "hint": "Check if the project ID is valid.",
+                        "httpStatusCode": 404,
+                        "message": "The server cannot find a project by its "
+                                   "ID."
+                    }
+            self.log.info("Executing test: {}".format(testcase["description"]))
+            if "param" in testcase:
+                kwarg = {testcase["param"]: testcase["paramValue"]}
+            else:
+                kwarg = dict()
+
+            result = self.capellaAPI.cluster_ops_apis.list_alerts(
+                testcase["organizationID"], testcase["projectID"], **kwarg)
+            if result.status_code == 429:
+                self.handle_rate_limit(int(result.headers["Retry-After"]))
+                result = self.capellaAPI.cluster_ops_apis.list_alerts(
+                    testcase["organizationID"], testcase["projectID"], **kwarg)
+
+            self.validate_testcase(result, [200], testcase, failures, True,
+                                   self.expected_res, self.alert_id)
+
+        if failures:
+            for fail in failures:
+                self.log.warning(fail)
+            self.fail("{} tests FAILED out of {} TOTAL tests"
+                      .format(len(failures), testcases))
+
+    def test_multiple_requests_using_API_keys_with_same_role_which_has_access(
+            self):
+        api_func_list = [[self.capellaAPI.cluster_ops_apis.list_alerts,
+                          (self.organisation_id, self.project_id)]]
+
+        for i in range(self.input.param("num_api_keys", 1)):
+            resp = self.capellaAPI.org_ops_apis.create_api_key(
+                self.organisation_id,
+                self.generate_random_string(prefix=self.prefix),
+                ["organizationOwner"], self.generate_random_string(50))
+            if resp.status_code == 429:
+                self.handle_rate_limit(int(resp.headers["Retry-After"]))
+                resp = self.capellaAPI.org_ops_apis.create_api_key(
+                    self.organisation_id,
+                    self.generate_random_string(prefix=self.prefix),
+                    ["organizationOwner"], self.generate_random_string(50))
+            if resp.status_code == 201:
+                self.api_keys["organizationOwner_{}".format(i)] = resp.json()
+            else:
+                self.fail("Error while creating API key for "
+                          "organizationOwner_{}".format(i))
+
+        results = self.throttle_test(api_func_list, self.api_keys)
+        for result in results:
+            # Removing failure for tests which are intentionally ran
+            # for :
+            #   # unauthorized roles, ie, which give a 403 response.
+            if "403" in results[result]["4xx_errors"]:
+                del results[result]["4xx_errors"]["403"]
+
+            if len(results[result]["4xx_errors"]) > 0 or len(
+                    results[result]["5xx_errors"]) > 0:
+                self.fail("Some API calls failed")
+
+    def test_multiple_requests_using_API_keys_with_diff_role(self):
+        api_func_list = [[self.capellaAPI.cluster_ops_apis.list_alerts,
+                          (self.organisation_id, self.project_id)]]
+
+        org_roles = self.input.param("org_roles", "organizationOwner")
+        proj_roles = self.input.param("proj_roles", "projectDataReader")
+        org_roles = org_roles.split(":")
+        proj_roles = proj_roles.split(":")
+
+        api_key_dict = self.create_api_keys_for_all_combinations_of_roles(
+            [self.project_id], proj_roles, org_roles)
+        for i, api_key in enumerate(api_key_dict):
+            if api_key in self.api_keys:
+                self.api_keys["{}_{}".format(api_key_dict[api_key], i)] = \
+                    api_key_dict[api_key]
+            else:
+                self.api_keys[api_key] = api_key_dict[api_key]
+
+        results = self.throttle_test(api_func_list, self.api_keys)
+        for result in results:
+            # Removing failure for tests which are intentionally ran
+            # for :
+            #   # unauthorized roles, ie, which give a 403 response.
+            if "403" in results[result]["4xx_errors"]:
+                del results[result]["4xx_errors"]["403"]
+
+            if len(results[result]["4xx_errors"]) > 0 or len(
+                    results[result]["5xx_errors"]) > 0:
+                self.fail("Some API calls failed")
