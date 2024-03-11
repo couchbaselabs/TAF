0dde42acb24049197a360f85aa326a1a9fed4dc1
Ashwin <ashwin.govindarajulu@couchbase.com>
2024-03-12 05:08:37 +0530

    Removing unused imports based on 'mode' param
    Change-Id: I9dd1f1d96cefef7f87634dab8718cddf445eeff6
    Reviewed-on: https://review.couchbase.org/c/TAF/+/207042
    Reviewed-by: Bharath G P <bharath.gp@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- lib/BucketLib/BucketOperations.py
+++ lib/BucketLib/BucketOperations.py
@@ -3,15 +3,8 @@ Created on Oct 24, 2017
 
 @author: riteshagarwal
 """
-import mode
-
-if mode.java:
-    from BucketOperations_JavaSDK import BucketHelper as bucketlib
-elif mode.cli:
-    from BucketOperations_CLI import BucketHelper as bucketlib
-else:
-    from BucketOperations_Rest import BucketHelper as bucketlib
+from BucketLib.BucketOperations_Rest import BucketHelper as BucketLib
 
 
-class BucketHelper(bucketlib):
+class BucketHelper(BucketLib):
     pass
--- lib/gsiLib/gsiHelper.py
+++ lib/gsiLib/gsiHelper.py
@@ -1,13 +1,4 @@
-import mode
-
-if mode.java:
-    # from GsiHelper_JavaSDK import GsiHelper as GsiLib
-    pass
-elif mode.cli:
-    # from GsiHelper_CLI import GsiHelper as GsiLib
-    pass
-else:
-    from GsiHelper_Rest import GsiHelper as GsiLib
+from gsiLib.GsiHelper_Rest import GsiHelper as GsiLib
 
 
 class GsiHelper(GsiLib):
