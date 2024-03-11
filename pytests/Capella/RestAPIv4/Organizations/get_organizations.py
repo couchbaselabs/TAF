0a1b0392108cd7d703802d5184289bc616d70bb4
vipbhardwaj <vipul.bhardwaj@couchbase.com>
2024-03-11 17:03:36 +0530

    ORG duplicate code removal
    Change-Id: I8f06af9f47493905ba5cbe43e1052c40167978ea
    Reviewed-on: https://review.couchbase.org/c/TAF/+/207001
    Reviewed-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Tested-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Well-Formed: Restriction Checker
    Tested-by: Build Bot <build@couchbase.com>

--- pytests/Capella/RestAPIv4/Organizations/get_organizations.py
+++ pytests/Capella/RestAPIv4/Organizations/get_organizations.py
@@ -233,53 +233,9 @@ class GetOrganization(APIBase):
             self):
         api_func_list = [[self.capellaAPI.org_ops_apis.fetch_organization_info,
                           (self.organisation_id,)]]
-
-        for i in range(self.input.param("num_api_keys", 1)):
-            resp = self.capellaAPI.org_ops_apis.create_api_key(
-                self.organisation_id, self.generate_random_string(),
-                ["organizationOwner"], self.generate_random_string(50))
-            if resp.status_code == 201:
-                self.api_keys["organizationOwner_{}".format(i)] = resp.json()
-            else:
-                self.fail("Error while creating API key for "
-                          "organizationOwner_{}".format(i))
-
-        results = self.throttle_test(api_func_list, self.api_keys)
-        for result in results:
-            # Removing failure for tests which are intentionally ran for
-            # unauthorized roles, ie, which give a 403 response.
-            if "403" in results[result]["4xx_errors"]:
-                del results[result]["4xx_errors"]["403"]
-
-            if len(results[result]["4xx_errors"]) > 0 or len(
-                    results[result]["5xx_errors"]) > 0:
-                self.fail("Some API calls failed")
+        self.throttle_test(api_func_list)
 
     def test_multiple_requests_using_API_keys_with_diff_role(self):
         api_func_list = [[self.capellaAPI.org_ops_apis.fetch_organization_info,
                           (self.organisation_id,)]]
-
-        org_roles = self.input.param("org_roles", "organizationOwner")
-        proj_roles = self.input.param("proj_roles", "projectDataReader")
-        org_roles = org_roles.split(":")
-        proj_roles = proj_roles.split(":")
-
-        api_key_dict = self.create_api_keys_for_all_combinations_of_roles(
-            [self.project_id], proj_roles, org_roles)
-        for i, api_key in enumerate(api_key_dict):
-            if api_key in self.api_keys:
-                self.api_keys["{}_{}".format(api_key_dict[api_key], i)] = \
-                    api_key_dict[api_key]
-            else:
-                self.api_keys[api_key] = api_key_dict[api_key]
-
-        results = self.throttle_test(api_func_list, self.api_keys)
-        for result in results:
-            # Removing failure for tests which are intentionally ran for
-            # unauthorized roles, ie, which give a 403 response.
-            if "403" in results[result]["4xx_errors"]:
-                del results[result]["4xx_errors"]["403"]
-
-            if len(results[result]["4xx_errors"]) > 0 or len(
-                    results[result]["5xx_errors"]) > 0:
-                self.fail("Some API calls failed")
+        self.throttle_test(api_func_list, True, self.project_id)
