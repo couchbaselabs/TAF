93f86d29f7a390e8e6249c0fea2d6ca4574b4e44
vipbhardwaj <vipul.bhardwaj@couchbase.com>
2024-03-14 14:16:44 +0530

    SCOPE duplicate code removal
    Change-Id: I9987659ebe26367f7e81f8e157422b0e7bab98b5
    Reviewed-on: https://review.couchbase.org/c/TAF/+/207216
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Umang Agrawal <umang.agrawal@couchbase.com>
    Tested-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Reviewed-on: https://review.couchbase.org/c/TAF/+/208361
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- pytests/Capella/RestAPIv4/Scopes/create_scopes.py
+++ pytests/Capella/RestAPIv4/Scopes/create_scopes.py
@@ -4,13 +4,13 @@ Created on December 4, 2023
 @author: Vipul Bhardwaj
 """
 
-from pytests.Capella.RestAPIv4.Buckets.get_buckets import GetBucket
+from pytests.Capella.RestAPIv4.Scopes.get_scopes import GetScope
 
 
-class CreateScope(GetBucket):
+class CreateScope(GetScope):
 
     def setUp(self, nomenclature="Scopes_Create"):
-        GetBucket.setUp(self, nomenclature)
+        GetScope.setUp(self, nomenclature)
 
         # Initialize scope params.
         self.scopes = list()
@@ -139,13 +139,12 @@ class CreateScope(GetBucket):
                 self.handle_rate_limit(int(result.headers["Retry-After"]))
                 result = self.capellaAPI.cluster_ops_apis.create_scope(
                     org, proj, clus, buck, scope_name)
-            if result.status_code == 201 and "expected_error" not in testcase:
-                self.scopes.append(scope_name)
-            else:
-                self.validate_testcase(result, 201, testcase, failures)
 
             self.capellaAPI.cluster_ops_apis.scope_endpoint = "/v4/" \
                 "organizations/{}/projects/{}/clusters/{}/buckets/{}/scopes"
+
+            if self.validate_testcase(result, [201], testcase, failures):
+                self.scopes.append(scope_name)
 
         if failures:
             for fail in failures:
@@ -201,10 +200,9 @@ class CreateScope(GetBucket):
                 result = self.capellaAPI.cluster_ops_apis.create_scope(
                     self.organisation_id, self.project_id, self.cluster_id,
                     self.bucket_id, scope_name, header)
-            if result.status_code == 201 and "expected_error" not in testcase:
+
+            if self.validate_testcase(result, [201], testcase, failures):
                 self.scopes.append(scope_name)
-            else:
-                self.validate_testcase(result, 201, testcase, failures)
 
             if len(self.scopes) == 1000:
                 self.log.warning("Reached 1000 Scopes, flushing all.")
@@ -341,10 +339,9 @@ class CreateScope(GetBucket):
                     testcase["organizationID"], testcase["projectID"],
                     testcase["clusterID"], testcase["bucketID"],
                     testcase["scopeName"], **kwarg)
-            if result.status_code == 201 and "expected_error" not in testcase:
+
+            if self.validate_testcase(result, [201], testcase, failures):
                 self.scopes.append(scope_name)
-            else:
-                self.validate_testcase(result, 201, testcase, failures)
 
             if len(self.scopes) == 1000:
                 self.log.warning("Reached 1000 Scopes, flushing all of them.")
@@ -393,15 +390,9 @@ class CreateScope(GetBucket):
                 res = self.capellaAPI.cluster_ops_apis.create_scope(
                     self.organisation_id, self.project_id, self.cluster_id,
                     self.bucket_id, testcase["name"])
-            if res.status_code == 201:
-                if "expected_error" in testcase:
-                    self.log.error(testcase["description"])
-                    self.log.warning("Result: {}".format(res.json()))
-                    failures.append(testcase["description"])
-                else:
-                    self.scopes.append(testcase["name"])
-            else:
-                self.validate_testcase(res, 201, testcase, failures)
+
+            if self.validate_testcase(res, [201], testcase, failures):
+                self.scopes.append(testcase["name"])
 
         if failures:
             for fail in failures:
@@ -419,50 +410,7 @@ class CreateScope(GetBucket):
         api_func_list = [[self.capellaAPI.cluster_ops_apis.create_scope,
                           (self.organisation_id, self.project_id,
                            self.cluster_id, self.bucket_id, "")]]
-
-        for i in range(self.input.param("num_api_keys", 1)):
-            resp = self.capellaAPI.org_ops_apis.create_api_key(
-                self.organisation_id,
-                self.generate_random_string(prefix=self.prefix),
-                ["organizationOwner"], self.generate_random_string(50))
-            if resp.status_code == 429:
-                self.handle_rate_limit(int(resp.headers["Retry-After"]))
-                resp = self.capellaAPI.org_ops_apis.create_api_key(
-                    self.organisation_id,
-                    self.generate_random_string(prefix=self.prefix),
-                    ["organizationOwner"], self.generate_random_string(50))
-            if resp.status_code == 201:
-                self.api_keys["organizationOwner_{}".format(i)] = resp.json()
-            else:
-                self.fail("Error while creating API key for "
-                          "organizationOwner_{}".format(i))
-
-        if self.input.param("rate_limit", False):
-            results = self.make_parallel_api_calls(
-                310, api_func_list, self.api_keys)
-            for result in results:
-                if ((not results[result]["rate_limit_hit"])
-                        or results[result][
-                            "total_api_calls_made_to_hit_rate_limit"] > 300):
-                    self.fail("Rate limit was hit after {0} API calls. This is"
-                              " definitely an issue.".format(results[result][
-                                "total_api_calls_made_to_hit_rate_limit"]))
-
-        results = self.make_parallel_api_calls(
-            99, api_func_list, self.api_keys)
-        for result in results:
-            # Removing failure for tests which are intentionally ran
-            # for :
-            #   # empty name dummies, ie, which give a 422 response.
-            if "422" in results[result]["4xx_errors"]:
-                del results[result]["4xx_errors"]["422"]
-            #   # unauthorized roles, ie, which give a 403 response.
-            if "403" in results[result]["4xx_errors"]:
-                del results[result]["4xx_errors"]["403"]
-
-            if len(results[result]["4xx_errors"]) > 0 or len(
-                    results[result]["5xx_errors"]) > 0:
-                self.fail("Some API calls failed")
+        self.throttle_test(api_func_list)
 
     def test_multiple_requests_using_API_keys_with_diff_role(self):
         """
@@ -473,44 +421,4 @@ class CreateScope(GetBucket):
         api_func_list = [[self.capellaAPI.cluster_ops_apis.create_scope,
                           (self.organisation_id, self.project_id,
                            self.cluster_id, self.bucket_id, "")]]
-
-        org_roles = self.input.param("org_roles", "organizationOwner")
-        proj_roles = self.input.param("proj_roles", "projectDataReader")
-        org_roles = org_roles.split(":")
-        proj_roles = proj_roles.split(":")
-
-        api_key_dict = self.create_api_keys_for_all_combinations_of_roles(
-            [self.project_id], proj_roles, org_roles)
-        for i, api_key in enumerate(api_key_dict):
-            if api_key in self.api_keys:
-                self.api_keys["{}_{}".format(api_key_dict[api_key], i)] = \
-                    api_key_dict[api_key]
-            else:
-                self.api_keys[api_key] = api_key_dict[api_key]
-
-        if self.input.param("rate_limit", False):
-            results = self.make_parallel_api_calls(
-                310, api_func_list, self.api_keys)
-            for result in results:
-                if ((not results[result]["rate_limit_hit"])
-                        or results[result][
-                            "total_api_calls_made_to_hit_rate_limit"] > 300):
-                    self.fail("Rate limit was hit after {0} API calls. This is"
-                              " definitely an issue.".format(results[result][
-                                "total_api_calls_made_to_hit_rate_limit"]))
-
-        results = self.make_parallel_api_calls(
-            99, api_func_list, self.api_keys)
-        for result in results:
-            # Removing failure for tests which are intentionally ran
-            # for :
-            #   # empty name dummies, ie, which give a 422 response.
-            if "422" in results[result]["4xx_errors"]:
-                del results[result]["4xx_errors"]["422"]
-            #   # unauthorized roles, ie, which give a 403 response.
-            if "403" in results[result]["4xx_errors"]:
-                del results[result]["4xx_errors"]["403"]
-
-            if len(results[result]["4xx_errors"]) > 0 or len(
-                    results[result]["5xx_errors"]) > 0:
-                self.fail("Some API calls failed")
+        self.throttle_test(api_func_list, True, self.project_id)
