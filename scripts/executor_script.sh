906c55e85b3e1c3e171fe845301d780fa525e895
Ashwin <ashwin.govindarajulu@couchbase.com>
2024-04-16 18:07:13 +0530

    Adding support to execute from new executors
    Using this, jobs migrated to new executors will
    be able to run regular jython jobs but by using the
    python installation present in master_py3_dev branch
    
    Change-Id: Ieca19a3fe2d2d881c0fc82a1e50b4a3091bb0783
    Reviewed-on: https://review.couchbase.org/c/TAF/+/208675
    Reviewed-by: Bharath G P <bharath.gp@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- scripts/executor_script.sh
+++ scripts/executor_script.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+run_populate_ini_script() {
+  set -x
+  python3 scripts/populateIni.py $skip_mem_info \
+  -s ${servers} $internal_servers_param \
+  -d ${addPoolServerId} \
+  -a ${addPoolServers} \
+  -i $WORKSPACE/testexec_reformat.$$.ini \
+  -p ${os} \
+  -o $WORKSPACE/testexec.$$.ini \
+  -k '{'${UPDATE_INI_VALUES}'}'
+  set +x
+}
+
 ulimit -a
 pip_path=/opt/jython/bin/pip
 jython_path=/opt/jython/bin/jython
@@ -29,21 +42,6 @@ newState=available
 
 majorRelease=`echo ${version_number} | awk '{print substr($0,1,1)}'`
 echo the major release is $majorRelease
-echo ${servers}
-
-${py_executable} -m easy_install httplib2
-if [ "$component" = "analytics" ]; then
-	${py_executable} -m pip install boto3==1.17.112 pyspark
-fi
-
-# Adding this to install libraries
-$pip_path install requests futures
-
-# Clone testrunner.git - to utilize latest populateIni changes
-mkdir tr_for_install
-cd tr_for_install
-git clone https://github.com/couchbase/testrunner.git
-cd ..
 
 UPDATE_INI_VALUES=""
 if [ ! "${username}" = "" ]; then
@@ -57,16 +55,6 @@ if [ ! "${password}" = "" ]; then
   fi
 fi
 
-cp ${iniFile} tr_for_install/testrunner/nodes.ini
-iniFile=nodes.ini
-
-cd tr_for_install/testrunner
-echo "#### Testrunner last commit ####"
-git log -1
-git submodule init
-git submodule update --init --force --remote
-echo "################################"
-
 # Fix for the ini format issue where both : and = used
 sed 's/=/:/' ${iniFile} > $WORKSPACE/testexec_reformat.$$.ini
 cat $WORKSPACE/testexec_reformat.$$.ini
@@ -75,32 +63,20 @@ if [ ! "${internal_servers}" = "" ]; then
 	internal_servers_param="--internal_servers $internal_servers"
 fi
 
+skip_mem_info=""
 if [ "$server_type" = "CAPELLA_LOCAL" ]; then
-	git submodule init
-    git submodule update --init --force --remote
-    echo python3  scripts/populateIni.py -m -s ${servers} $internal_servers_param -d ${addPoolServerId} -a ${addPoolServers} -i $WORKSPACE/testexec_reformat.$$.ini -p ${os} -o $WORKSPACE/testexec.$$.ini -k '{'${UPDATE_INI_VALUES}'}'
-    python3 scripts/populateIni.py -m -s ${servers} $internal_servers_param -d ${addPoolServerId} -a ${addPoolServers} -i $WORKSPACE/testexec_reformat.$$.ini -p ${os} -o $WORKSPACE/testexec.$$.ini -k '{'${UPDATE_INI_VALUES}'}'
-
-else
-    echo python3 scripts/populateIni.py -s ${servers} $internal_servers_param -d ${addPoolServerId} -a ${addPoolServers} -i $WORKSPACE/testexec_reformat.$$.ini -p ${os} -o $WORKSPACE/testexec.$$.ini -k '{'${UPDATE_INI_VALUES}'}'
-    python3 scripts/populateIni.py -s ${servers} $internal_servers_param -d ${addPoolServerId} -a ${addPoolServers} -i $WORKSPACE/testexec_reformat.$$.ini -p ${os} -o $WORKSPACE/testexec.$$.ini -k '{'${UPDATE_INI_VALUES}'}'
-fi
-
-cd ../..
-
-if [ "$os" = "windows" ]; then
-   echo "Have Windows,"
-   parallel=true   # serial worked even worse but may come back to is
- else
-   parallel=true
-fi
-
-if [ "$server_type" = "CAPELLA_LOCAL" ]; then
+  skip_mem_info=" -m "
 	installParameters="install_tasks=uninstall-install,h=true"
 else
     if [ "$server_type" = "ELIXIR_ONPREM" ]; then
         installParameters="cluster_profile=serverless"
     fi
+fi
+
+parallel=true
+if [ "$os" = "windows" ]; then
+   # serial worked even worse but may come back to is
+   parallel=true
 fi
 
 if [ "$installParameters" = "None" ]; then
@@ -124,7 +100,6 @@ if [ "$component" = "transaction" ]; then
     fi
 fi
 
-
 # Updating upgrade_version for upgrade jobs
 if [ "$parameters" = "" ]; then
     parameters="upgrade_version=${version_number}"
@@ -137,14 +112,61 @@ if [ "$component" = "analytics" ]; then
 	parameters="${parameters},aws_access_key=${aws_access_key},aws_secret_key=${aws_secret_key}"
 fi
 
-if [ "$server_type" != "CAPELLA_LOCAL" ]; then
-  if [ "$os" = "windows" ] || [ $(${py_executable} -c "print($small_ver < $support_ver)") = True ]; then
-      cd tr_for_install/testrunner
+status=0
+if [ "${slave}" == "deb12_executor" ]; then
+  pyenv local 3.10.14
+
+  py_executable=python
+  alias python3=python
+
+  # Switch to py3 branch that has all dependent modules for execution
+  git checkout master_py3_dev
+  git submodule init
+  git submodule update --init --force --remote
+  run_populate_ini_script
+
+  if [ "$server_type" != "CAPELLA_LOCAL" ]; then
+    cd platform_utils/ssh_util
+    export PYTHONPATH="../../couchbase_utils:../../py_constants"
+    python -m install_util.install -i $WORKSPACE/testexec.$$.ini -v ${version_number} --skip_local_download
+    status=$?
+    # Come back to TAF root dir
+    cd ../..
+  fi
+
+  # Come back to the requested branch for test execution
+  git checkout ${branch}
+else
+  # Regular jython_slave executor
+  ${py_executable} -m easy_install httplib2
+  if [ "$component" = "analytics" ]; then
+	  ${py_executable} -m pip install boto3==1.17.112 pyspark
+  fi
+
+  # Adding this to install libraries
+  $pip_path install requests futures
+
+  # Clone testrunner.git - to utilize latest populateIni changes
+  mkdir tr_for_install
+  cd tr_for_install
+  git clone https://github.com/couchbase/testrunner.git
+  cd testrunner
+
+  cp ../../${iniFile} nodes.ini
+  iniFile=nodes.ini
+
+  echo "#### Testrunner last commit ####"
+  git log -1
+  git submodule init
+  git submodule update --init --force --remote
+  echo "################################"
+  run_populate_ini_script
+  if [ "$server_type" != "CAPELLA_LOCAL" ]; then
+    if [ "$os" = "windows" ] || [ $(${py_executable} -c "print($small_ver < $support_ver)") = True ]; then
       python3 scripts/new_install.py -i $WORKSPACE/testexec.$$.ini -p timeout=2000,skip_local_download=False,version=${version_number},product=cb,parallel=${parallel},init_nodes=${initNodes},debug_logs=True,url=${url}${extraInstall}
       status=$?
-      cd ../../
-  else
-      #To handle nonroot user
+    else
+      # To handle nonroot user
       echo sed 's/nonroot/root/g' $WORKSPACE/testexec.$$.ini > $WORKSPACE/testexec_root.$$.ini
       sed 's/nonroot/root/g' $WORKSPACE/testexec.$$.ini > $WORKSPACE/testexec_root.$$.ini
 
@@ -171,21 +193,20 @@ if [ "$server_type" != "CAPELLA_LOCAL" ]; then
       echo "Server install cmd:"
 
       if [ "$component" = "os_certify" ]; then
-      	echo ${py_executable} scripts/new_install.py -i $WORKSPACE/testexec.$$.ini -p timeout=7200,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}
-        ${py_executable} scripts/new_install.py -i $WORKSPACE/testexec.$$.ini -p timeout=7200,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}
-        status=$?
+        new_install_params="timeout=7200,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}"
       else
-        echo ${py_executable} scripts/new_install.py -i $WORKSPACE/testexec.$$.ini -p force_reinstall=False,timeout=2000,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}
-        ${py_executable} scripts/new_install.py -i $WORKSPACE/testexec.$$.ini -p force_reinstall=False,timeout=2000,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}
-        status=$?
+        new_install_params="force_reinstall=False,timeout=2000,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}"
       fi
-      cd ../..
+      set -x
+      ${py_executable} scripts/new_install.py -i $WORKSPACE/testexec.$$.ini -p $new_install_params
+      status=$?
+      set +x
+    fi
   fi
-else
-  status=0
+  cd ../..
+  # Remove testrunner dir to reclaim disk space
+  rm -rf tr_for_install
 fi
-rm -rf tr_for_install
-
 
 # To pass client-versions to use from cmd_line
 sdk_client_params="-P transaction_version=$transaction_version -P client_version=$java_client_version"
@@ -275,7 +296,6 @@ if [ $status -eq 0 ]; then
   	echo "Gradle had non zero exit. Failing the job"
     exit 1
   fi
-
 else
   echo Desc: $desc
   newState=failedInstall
