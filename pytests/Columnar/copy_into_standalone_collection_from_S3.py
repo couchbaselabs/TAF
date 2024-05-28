6a16dc0ce0387e116df2542ce97753d79ac92a04
Abhay Aggrawal <abhay.aggrawal@couchbase.com>
2024-05-28 13:15:56 +0530

    adding drop link test for copy from s3
    Change-Id: I3d23611c115da294eb38c84e94ed1b62a933a34f
    Reviewed-on: https://review.couchbase.org/c/TAF/+/210536
    Reviewed-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Tested-by: Abhay Aggrawal <abhay.aggrawal@couchbase.com>

--- pytests/Columnar/copy_into_standalone_collection_from_S3.py
+++ pytests/Columnar/copy_into_standalone_collection_from_S3.py
@@ -3,6 +3,7 @@ Created on 25-OCTOBER-2023
 
 @author: umang.agrawal
 """
+import time
 
 from Columnar.columnar_base import ColumnarBaseTest
 from Queue import Queue
@@ -431,6 +432,75 @@ class CopyIntoStandaloneCollectionFromS3(ColumnarBaseTest):
                 self.fail("Error while copying data from S3 into {0} "
                           "standalone collection".format(standalone_coll.full_name))
 
+
+    def test_create_copyinto_query_drop_link(self):
+        self.columnar_spec["database"]["no_of_databases"] = self.input.param(
+            "no_of_DBs", 1)
+        self.columnar_spec["dataverse"]["no_of_dataverses"] = self.input.param(
+            "no_of_scopes", 1)
+
+        self.columnar_spec["external_link"][
+            "no_of_external_links"] = self.input.param(
+            "no_of_links", 1)
+        self.columnar_spec["external_link"]["properties"] = [{
+            "type": "s3",
+            "region": self.aws_region,
+            "accessKeyId": self.aws_access_key,
+            "secretAccessKey": self.aws_secret_key,
+            "serviceEndpoint": None
+        }]
+        self.columnar_spec["standalone_dataset"][
+            "num_of_standalone_coll"] = self.input.param(
+            "num_of_standalone_coll", 1)
+        self.columnar_spec["standalone_dataset"]["primary_key"] = [{"id": "string", "product_name": "string"}]
+
+        file_format = self.input.param("file_format", "json")
+        dataset_properties = self.columnar_spec["standalone_dataset"][
+            "standalone_collection_properties"][0]
+        dataset_properties["external_container_name"] = self.aws_bucket_name
+        dataset_properties["file_format"] = file_format
+        dataset_properties["include"] = "*.{0}".format(file_format)
+        dataset_properties["region"] = self.aws_region
+        result, msg = self.cbas_util.create_cbas_infra_from_spec(
+            self.instance, self.columnar_spec, self.bucket_util, False)
+        if not result:
+            self.fail(msg)
+        datasets = self.cbas_util.get_all_dataset_objs("standalone")
+        jobs = Queue()
+        results = []
+        for standalone_coll in datasets:
+            jobs.put((self.cbas_util.copy_from_external_resource_into_standalone_collection,
+                      {
+                          "cluster": self.instance, "collection_name": standalone_coll.name,
+                          "aws_bucket_name": standalone_coll.dataset_properties["external_container_name"],
+                          "external_link_name": standalone_coll.link_name,
+                          "dataverse_name": standalone_coll.dataverse_name,
+                          "database_name": standalone_coll.database_name,
+                          "files_to_include": standalone_coll.dataset_properties["include"],
+                          "file_format": standalone_coll.dataset_properties["file_format"],
+                          "type_parsing_info": standalone_coll.dataset_properties["object_construction_def"],
+                          "path_on_aws_bucket": standalone_coll.dataset_properties["path_on_external_container"],
+                          "header": standalone_coll.dataset_properties["header"],
+                          "null_string": standalone_coll.dataset_properties["null_string"],
+                          "files_to_exclude": standalone_coll.dataset_properties["exclude"],
+                          "parse_json_string": standalone_coll.dataset_properties["parse_json_string"],
+                          "convert_decimal_to_double": standalone_coll.dataset_properties["convert_decimal_to_double"],
+                          "timezone": standalone_coll.dataset_properties["timezone"]
+                      }))
+
+        self.cbas_util.run_jobs_in_parallel(
+            jobs, results, self.sdk_clients_per_user, async_run=True)
+
+        self.log.info("Sleeping 15 seconds, waiting all copy statement to execute")
+        time.sleep(5)
+        external_links = self.cbas_util.get_all_link_objs("external")
+        self.log.info("Disconnecting external links")
+        for link in external_links:
+            if not self.cbas_util.drop_link(self.instance, link.full_name):
+                self.fail("Fail to delete link while copy from s3")
+        jobs.join()
+        if not all(results):
+            self.fail("Copy to statement failed after link disconnect")
     def test_create_copyinto_query_incorrect_file_format_clause_drop_standalone_collection(self):
         # Update columnar spec based on conf file params
         self.columnar_spec["database"]["no_of_databases"] = self.input.param(
