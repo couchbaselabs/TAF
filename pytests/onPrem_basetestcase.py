couchbaselabs/TAF

commit 3dc689869a6427425b6d5c72f41f9000f1887bbb

CBQE-8190: Avoid building SDK client env for each connection
Merge from 'trinity' branch

Change-Id: I78e88098125611d8c8aec652be3bc660a251b0f4
Reviewed-on: https://review.couchbase.org/c/TAF/+/211259
Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>
Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

pytests/onPrem_basetestcase.py
@@ -245,6 +245,14 @@ class OnPremBaseTest(CouchbaseBaseTest):
             self.security_util = SecurityUtils(self.log)
             self.security_util.set_internal_creds_rotation_interval(self.cluster, self.int_pwd_rotn)
 
+            for _, cluster in self.cb_clusters.items():
+                if self.use_https:
+                    SDKClient.enable_tls_in_env(cluster)
+                # Building this object here once based on 'use_https',
+                # this will be possibly built once across the run.
+                # This will minimize the mem intensity of the run drastically
+                SDKClient.singleton_env_build(cluster)
+
             if self.skip_setup_cleanup:
                 for server in self.servers:
                     self.set_ports_for_server(server, "ssl")

couchbase_utils/cluster_utils/cluster_ready_functions.py
@@ -85,7 +85,7 @@ class CBCluster:
         self.sdk_client_pool = None
         # Note: Referenced only for sdk_client3.py SDKClient
         self.sdk_cluster_env = SDKClient.create_cluster_env()
-        self.sdk_env_built = self.sdk_cluster_env.build()
+        self.sdk_env_built = None
 
         # Capella specific params
         self.pod = None

lib/sdk_client3.py
@@ -266,6 +266,17 @@ class SDKClient(object):
     """
 
     @staticmethod
+    def enable_tls_in_env(cluster):
+        cluster.sdk_cluster_env = cluster.sdk_cluster_env.securityConfig(
+            SecurityConfig.enableTls(True)
+            .trustManagerFactory(InsecureTrustManagerFactory.INSTANCE))
+
+    @staticmethod
+    def singleton_env_build(cluster):
+        if cluster.sdk_env_built is None:
+            cluster.sdk_env_built = cluster.sdk_cluster_env.build()
+
+    @staticmethod
     def create_cluster_env(kv_timeout=60, num_kv_connections=25,
                            connection_timeout=20):
         return ClusterEnvironment.builder() \
@@ -370,7 +381,7 @@ class SDKClient(object):
         # compression settings and explicitly setting to 'False' as well
         build_env = False
         t_cluster_env = cluster.sdk_env_built
-        if CbServer.use_https or self.transaction_conf or self.compression:
+        if self.transaction_conf or self.compression:
             t_cluster_env = cluster.sdk_cluster_env
             build_env = True
 
@@ -384,11 +395,6 @@ class SDKClient(object):
                 compression_config = compression_config.minRatio(
                     self.compression["minRatio"])
             t_cluster_env = t_cluster_env.compressionConfig(compression_config)
-
-        if CbServer.use_https:
-            t_cluster_env = t_cluster_env.securityConfig(
-                SecurityConfig.enableTls(True)
-                .trustManagerFactory(InsecureTrustManagerFactory.INSTANCE))
 
         if self.transaction_conf:
             trans_conf = TransactionsConfig().cleanupConfig(

commit bf3ac055041a98a428360394d7e4c1e5c59cea11

Fixing SDKClient import error
This was introduced by commit: 3dc689869a6427425b6d5c72f41f9000f1887bbb

Change-Id: I1b6768a2289b658c0ab69be3622647bc8fa5dd3c
Reviewed-on: https://review.couchbase.org/c/TAF/+/211306
Reviewed-by: <pulkit.matta@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

pytests/onPrem_basetestcase.py
@@ -24,7 +24,7 @@ from couchbase_utils.security_utils.security_utils import SecurityUtils
 from couchbase_utils.security_utils.x509_multiple_CA_util import x509main
 from membase.api.rest_client import RestConnection
 from remote.remote_util import RemoteMachineShellConnection
-from sdk_client3 import SDKClientPool
+from sdk_client3 import SDKClientPool, SDKClient
 from security_config import trust_all_certs
 from docker_utils.DockerSDK import DockerClient
 from awsLib.s3_data_helper import perform_S3_operation