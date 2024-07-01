6a2a8434a4bfb2bdee12f1c7acf351eab29c52a7
Umang Agrawal <umang.agrawal@couchbase.com>
2024-07-01 19:43:01 +0530

    Updating jenkins script for job::test_suite_executor_cloud-TAF
    Change-Id: I675b238117ef8dcc272808ede2858486331a2ae6
    Reviewed-on: https://review.couchbase.org/c/TAF/+/212108
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Umang Agrawal <umang.agrawal@couchbase.com>

--- scripts/executor_script_cloud.sh
+++ scripts/executor_script_cloud.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-support_ver="6.5"
-small_ver=${version_number:0:3}
-py_executable=python3
-jython_path=/opt/jython/bin/jython
-
-echo "Set ALLOW_HTP to False so test could run."
-sed -i 's/ALLOW_HTP.*/ALLOW_HTP = False/' lib/testconstants.py
-
 git submodule init
 git submodule update --init --force --remote
 
@@ -26,8 +18,21 @@ echo the major release is $majorRelease
 
 echo ${servers}
 
-${py_executable} -m pip install easy_install
-${py_executable} -m easy_install httplib2 ground hypothesis_geometry argparse psycopg2 dnspython boto3
+# Setup GoLang in local dir
+go_version=1.22.4
+wget https://golang.org/dl/go${go_version}.linux-amd64.tar.gz --quiet
+tar -xzf go${go_version}.linux-amd64.tar.gz
+rm -f go${go_version}.linux-amd64.tar.gz
+export GOPATH=`pwd`/go
+export PATH="${GOPATH}/bin:${PATH}"
+export GO111MODULE=on
+# Set desired python env
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv local 3.10.14
+
+python -m pip install -r requirements.txt
 
 UPDATE_INI_VALUES=""
 if [ ! "${username}" = "" ]; then
@@ -44,8 +49,8 @@ fi
 # Below "if" block added by UMANG to run goldfish tests
 if [ "$server_type" = "SERVERLESS_COLUMNAR" ]; then
   cluster_info="{\"pod\": \"$capella_api_url\", \"tenant_id\": \"$tenant_id\", \"capella_user\": \"$capella_user\", \"capella_pwd\": \"$capella_password\", \"region\": \"$capella_region\"}"
-  #echo ${py_executable} signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region
-  #cluster_info=`${py_executable} signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region`
+  #echo python signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region
+  #cluster_info=`python signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region`
   echo $cluster_info
   servers="None"
 fi
@@ -56,8 +61,8 @@ if [ "$server_type" = "PROVISIONED_ONCLOUD" ]; then
   date
   cluster_info="{\"pod\": \"$capella_api_url\", \"tenant_id\": \"$tenant_id\", \"capella_user\": \"$capella_user\", \"capella_pwd\": \"$capella_password\", \"project_id\": \"$project_id\", \"region\": \"$capella_region\"}"
   #commented below 2 lines by Shaazin
-  #echo ${py_executable} signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region
-  #cluster_info=`${py_executable} signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region`
+  #echo python signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region
+  #cluster_info=`python signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region`
   if [ -n "$capella_dataplane_id" ]; then
     cluster_info=`echo $cluster_info | sed s/}/,\ \"dataplane_id\":\ \"$capella_dataplane_id\"}/`
   fi
@@ -77,8 +82,8 @@ fi
 # For Capella, signup new user to be use for this test execution
 if [ "$server_type" = "SERVERLESS_ONCLOUD" ]; then
 #if [ "$server_type" = "SERVERLESS_ONCLOUD" -o "$server_type" = "PROVISIONED_ONCLOUD" ]; then - commented by Shaazin
-  echo ${py_executable} signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region
-  cluster_info=`${py_executable} signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region`
+  echo python signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region
+  cluster_info=`python signup_user.py -e ${capella_email_prefix} -a $capella_api_url -x $capella_signup_token -r $capella_region`
   if [ -n "$capella_dataplane_id" ]; then
     cluster_info=`echo $cluster_info | sed s/}/,\ \"dataplane_id\":\ \"$capella_dataplane_id\"}/`
   fi
@@ -101,28 +106,19 @@ else
 	skipped_install_for_capella=
 fi
 
-# Run populate ini from testrunner instead of running frm TAF due to changes in TAF to move to python3
-mkdir tr_for_install
-cd tr_for_install
-git clone https://github.com/couchbase/testrunner.git
-cd testrunner
-git submodule init
-git submodule update --init --force --remote
+touch $WORKSPACE/testexec.$$.ini
+docker run --rm \
+  -v $WORKSPACE/testexec.$$.ini:/testrunner/testexec.$$.ini  \
+  testrunner:install python3 scripts/populateIni.py $skip_mem_info \
+  -s ${servers} $internal_servers_param \
+  -d ${addPoolServerId} \
+  -a ${addPoolServers} \
+  -i ${iniFile} \
+  -p ${os} \
+  -o testexec.$$.ini \
+  --keyValue "${cluster_info}"
 
-echo ${py_executable}  scripts/populateIni.py -s ${servers} -d ${addPoolServerId} -a ${addPoolServers} -i ${iniFile} -p ${os} -o /tmp/testexec.$$.ini --keyValue "$cluster_info"
-${py_executable}  scripts/populateIni.py -s ${servers} -d ${addPoolServerId} -a ${addPoolServers} -i ${iniFile} -p ${os} -o /tmp/testexec.$$.ini --keyValue "$cluster_info"
-
-# Get back to TAF directory
-cd ../../
-rm -rf tr_for_install
-
-if [ "$os" = "windows" ]; then
-   echo "Have Windows,"
-   parallel=true   # serial worked even worse but may come back to is
- else
-   parallel=true
-fi
-
+parallel=true
 if [ "$server_type" = "CAPELLA_LOCAL" ]; then
 	installParameters="install_tasks=uninstall-install,h=true"
 else
@@ -140,98 +136,50 @@ echo extra install is $extraInstall
 
 timedatectl
 if [ ${skip_install} == false ]; then
-  if [ "$os" = "windows" ] && ( [ $(${py_executable} -c "print($small_ver < $support_ver)") = True ] || [[ $small_ver != "0.0" ]] ); then
-      #echo "Using install.py"
-      #first_with_new_installer="4.7"
-      #dot_release=${version_number:0:3}
-      #echo the dot release is $dot_release
-      #if [ $(echo "$first_with_new_installer <= $dot_release" | bc) -eq 0 ]; then
-          #echo pre 4.7 release
-      ${py_executable} scripts/install.py -i /tmp/testexec.$$.ini -p version=${version_number},product=cb,parallel=${parallel},init_nodes=${initNodes},url=${url}${extraInstall}
-      #else
-          #echo 4.7 or greater
-          # interim nsis install
-          #python ./scripts/install.py -i /tmp/testexec.$$.ini -p version=4.7.0-903,product=cb,parallel=true,vbuckets=1024,nsis=True,url=http://172.23.120.24/builds/latestbuilds/couchbase-server/spock/win_installers/nsis/couchbase-server-enterprise_4.7.0-903-windows_amd64.exe
-      #fi
-  else
-      #To handle nonroot user
-      echo sed 's/nonroot/root/g' /tmp/testexec.$$.ini > /tmp/testexec_root.$$.ini
-      sed 's/nonroot/root/g' /tmp/testexec.$$.ini > /tmp/testexec_root.$$.ini
-      echo ${py_executable} scripts/ssh.py -i /tmp/testexec_root.$$.ini "iptables -F"
-      ${py_executable} scripts/ssh.py -i /tmp/testexec_root.$$.ini "iptables -F"
-      #python scripts/install.py -i /tmp/testexec.$$.ini -p version=${version_number},product=cb,parallel=${parallel},init_nodes=${initNodes}${extraInstall}
-      #NOTE: Below 2 lines are for finding the IPs for uninstall during downstream cleanup job.
-
-      if [ "${INSTALL_TIMEOUT}" = "" ]; then
-         INSTALL_TIMEOUT="1200"
-      fi
-      # 6.5.x has install issue. Reverting to older style
-      if [ "${SKIP_LOCAL_DOWNLOAD}" = "" ]; then
-         SKIP_LOCAL_DOWNLOAD="False"
-      fi
-
-      if [ ! "$majorRelease" = "7" ]; then
-         SKIP_LOCAL_DOWNLOAD="True"
-      fi
-
-      cp /tmp/testexec.$$.ini $WORKSPACE/
-      initial_version=$(echo $parameters | grep -P 'initial_version=[0-9]+.[0-9].[0-9]+-[0-9]+' -o | cut -d '=' -f 2)
-      #initial_version= $(echo ${parameters} |sed 's/.*,initial_version=\([0-9].[0-9].[0-9]-[0-9]\{4\}\),.*/ \1/')
-      echo "Initial version: $initial_version"
-      if [ -n "$initial_version" ]; then
-          echo ${py_executable} scripts/new_install.py -i /tmp/testexec.$$.ini -p timeout=${INSTALL_TIMEOUT},skip_local_download=${SKIP_LOCAL_DOWNLOAD},get-cbcollect-info=True,version=${initial_version},product=cb,debug_logs=True,ntp=True,url=${url}${extraInstall}
-          ${py_executable} scripts/new_install.py -i /tmp/testexec.$$.ini -p timeout=${INSTALL_TIMEOUT},skip_local_download=${SKIP_LOCAL_DOWNLOAD},get-cbcollect-info=True,version=${initial_version},product=cb,debug_logs=True,ntp=True,url=${url}${extraInstall}
-      else
-          echo ${py_executable} scripts/new_install.py -i /tmp/testexec.$$.ini -p timeout=${INSTALL_TIMEOUT},skip_local_download=${SKIP_LOCAL_DOWNLOAD},get-cbcollect-info=True,version=${version_number},product=cb,debug_logs=True,ntp=True,url=${url}${extraInstall}
-          ${py_executable} scripts/new_install.py -i /tmp/testexec.$$.ini -p timeout=${INSTALL_TIMEOUT},skip_local_download=${SKIP_LOCAL_DOWNLOAD},get-cbcollect-info=True,version=${version_number},product=cb,debug_logs=True,ntp=True,url=${url}${extraInstall}
-      fi
-      #echo "Sleeping now..."
-      #sleep 90000
+  if [ "$os" = "windows" ] ; then
+    docker run --rm \
+        -v $WORKSPACE/testexec.$$.ini:/testrunner/testexec.$$.ini \
+        testrunner:install python3 scripts/new_install.py \
+        -i testexec.$$.ini \
+        -p timeout=2000,skip_local_download=False,version=${version_number},product=cb,parallel=${parallel},init_nodes=${initNodes},debug_logs=True,url=${url}${extraInstall}
       status=$?
-      if [ ! $status = 0 ]; then
-          echo exiting
-          echo Desc: $desc
-          newState=failedInstall
-          echo newState=failedInstall>propfile
-          if [ ${rerun_job} == true ]; then
-              echo "${py_executable} scripts/rerun_jobs.py ${version_number} --executor_jenkins_job --install_failure"
-              ${py_executable} scripts/rerun_jobs.py ${version_number} --executor_jenkins_job --install_failure || true
-          fi
-          exit 1
-      else
-          echo success
+  else
+      # To handle nonroot user
+      echo sed 's/nonroot/root/g' $WORKSPACE/testexec.$$.ini > $WORKSPACE/testexec_root.$$.ini
+      sed 's/nonroot/root/g' $WORKSPACE/testexec.$$.ini > $WORKSPACE/testexec_root.$$.ini
+
+      if [ "$os" != "mariner2" ]; then
+      	python scripts/ssh.py -i $WORKSPACE/testexec.$$.ini "iptables -F"
       fi
 
+      # Doing installation from TESTRUNNER!!!
+      skip_local_download_val=False
+      if [[ "$os" = windows* ]]; then
+        skip_local_download_val=True
+      fi
+      if [ "$os" = "debian11nonroot" ]; then
+      	skip_local_download_val=True
+      fi
+
+      if [ "$component" = "os_certify" ]; then
+        new_install_params="timeout=7200,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}"
+      else
+        new_install_params="force_reinstall=False,timeout=2000,skip_local_download=$skip_local_download_val,get-cbcollect-info=True,version=${version_number},product=cb,ntp=True,debug_logs=True,url=${url},cb_non_package_installer_url=${cb_non_package_installer_url}${extraInstall}"
+      fi
+
+      # Install requirements for this venv
+      set -x
+      docker run --rm \
+        -v $WORKSPACE/testexec.$$.ini:/testrunner/testexec.$$.ini \
+        testrunner:install python3 scripts/new_install.py \
+        -i testexec.$$.ini \
+        -p $new_install_params
+      status=$?
+      set +x
   fi
 fi
 
 if [ "$?" -eq 0 ]; then
-  desc2=`echo $descriptor | awk '{split($0,r,"-");print r[1],r[2]}'`
-  #export test_params="get-cbcollect-info=True"
-  if [ ${skip_install} == false ]; then
-	  ${py_executable} scripts/ssh.py -i /tmp/testexec.$$.ini "iptables -F"
-  fi
-
-  if [ $majorRelease = "3" ]; then
-     echo have a 3.x release
-     git checkout 0b2bb7a53e350f90737112457877ef2c05ca482a
-  elif [[ "$testrunner_tag" != "master" ]]; then
-     git checkout $testrunner_tag
-  fi
-  echo "Need to set ALLOW_HTP back to True to do git pull branch"
-  sed -i 's/ALLOW_HTP.*/ALLOW_HTP = True/' lib/testconstants.py
-  #git checkout ${branch}
-  #git pull origin ${branch}
-
-  ###### Added on 22/March/2018 to fix auto merge failures. Please revert this if this does not work.
-  #git fetch
-  #git reset --hard origin/${branch}
-  ######
-
-  ## cherrypick the gerrit request if it was defined
-  if [ "$cherrypick" != "None" ]; then
-     sh -c "$cherrypick"
-  fi
 
   if [ "$server_type" = "CAPELLA_LOCAL" ] && [ ${skip_install} == false ]; then
 
@@ -242,7 +190,7 @@ if [ "$?" -eq 0 ]; then
     export ANSIBLE_CONFIG=$PWD/productivitynautomation/ansible_setup/.ansible.cfg
     export ANSIBLE_HOST_KEY_CHECKING=false
     mkdir -p /root/cloud
-    ${py_executable} $PWD/productivitynautomation/ansible_setup/create_hosts.py /tmp/testexec.$$.ini $PWD/productivitynautomation/ansible_setup/hosts_template $PWD/ans_hosts
+    python $PWD/productivitynautomation/ansible_setup/create_hosts.py $WORKSPACE/testexec.$$.ini $PWD/productivitynautomation/ansible_setup/hosts_template $PWD/ans_hosts
 #    IFS=', ' read -r -a server_array <<< "$servers"
 #    for index in "${!server_array[@]}";
 #    do
@@ -251,12 +199,12 @@ if [ "$?" -eq 0 ]; then
 #    done
 
 #    \cp -rf $PWD/productivitynautomation/ansible_setup/hosts /root/cloud/hosts
-    #${py_executable} scripts/create_cloud_ansible.py -i /tmp/testexec.$$.ini -o /root/cloud/hosts
+    #python scripts/create_cloud_ansible.py -i $WORKSPACE/testexec.$$.ini -o /root/cloud/hosts
     #pushd productivitynautomation/ansible_setup/
 
-    ${py_executable} -m venv $PWD/ansible
+    python -m venv $PWD/ansible
     source $PWD/ansible/bin/activate
-    ${py_executable} -m pip install ansible paramiko
+    python -m pip install ansible paramiko
     yum install sshpass -y
 
     ansible-playbook -v $PWD/productivitynautomation/ansible_setup/install-firewalld.yml -i $PWD/ans_hosts
@@ -279,8 +227,8 @@ if [ "$?" -eq 0 ]; then
     deactivate
 
     if [ "$component" != "backup_recovery" ]; then
-    	echo "sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' /tmp/testexec.$$.ini"
-    	sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' /tmp/testexec.$$.ini
+    	echo "sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' $WORKSPACE/testexec.$$.ini"
+    	sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' $WORKSPACE/testexec.$$.ini
     fi
 
     #popd
@@ -290,21 +238,11 @@ if [ "$?" -eq 0 ]; then
   fi
 
   if [ ${skip_install} == true ]; then
-    	echo "sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' /tmp/testexec.$$.ini"
-    	sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' /tmp/testexec.$$.ini    	
+    	echo "sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' $WORKSPACE/testexec.$$.ini"
+    	sed -i 's/admin_bucket_username:Administrator/admin_bucket_username:user1/g;s/rest_username:Administrator/rest_username:user1/g' $WORKSPACE/testexec.$$.ini
 
-    	echo "sed -i 's/admin_bucket_password:password/admin_bucket_password:Passw0rd\$/g;s/rest_password:password/rest_password:Passw0rd\$/g' /tmp/testexec.$$.ini"
-    	sed -i 's/admin_bucket_password:password/admin_bucket_password:Passw0rd\$/g;s/rest_password:password/rest_password:Passw0rd\$/g' /tmp/testexec.$$.ini
-  fi
-
-  ## Setup for java sdk client, e2e-app
-  git submodule init
-  git submodule update --init --force --remote
-  if [ -f /etc/redhat-release ]; then
-    yum install -y maven
-  fi
-  if [ -f /etc/lsb-release ]; then
-    apt install maven
+    	echo "sed -i 's/admin_bucket_password:password/admin_bucket_password:Passw0rd\$/g;s/rest_password:password/rest_password:Passw0rd\$/g' $WORKSPACE/testexec.$$.ini"
+    	sed -i 's/admin_bucket_password:password/admin_bucket_password:Passw0rd\$/g;s/rest_password:password/rest_password:Passw0rd\$/g' $WORKSPACE/testexec.$$.ini
   fi
 
   ###### Added on 4/April/2018 to fix issues related to disk full on slaves.
@@ -316,7 +254,7 @@ if [ "$?" -eq 0 ]; then
 
   ##Added on August 2nd 2017 to kill all python processes older than 10days, comment if it causes any failures
   ## Updated on 11/21/19 by Mihir to kill all python processes older than 3 days instead of 10 days.
-  killall --older-than 72h ${py_executable}
+  killall --older-than 72h python
 
   if [ -z "${rerun_params_manual}" ] && [ -z "${rerun_params}" ]; then
   	rerun_param=
@@ -326,14 +264,20 @@ if [ "$?" -eq 0 ]; then
   	rerun_param=${rerun_params_manual}
   fi
 
-  sed -i 's/pod\:https\:\/\//pod:/g' /tmp/testexec.$$.ini
-  sed -i 's/pod\:api/pod:cloudapi/g' /tmp/testexec.$$.ini
+  sed -i 's/pod\:https\:\/\//pod:/g' $WORKSPACE/testexec.$$.ini
+  sed -i 's/pod\:api/pod:cloudapi/g' $WORKSPACE/testexec.$$.ini
 
-  cat /tmp/testexec.$$.ini
+  cat $WORKSPACE/testexec.$$.ini
   git clone https://github.com/couchbaselabs/guides.git
-  echo guides/gradlew --refresh-dependencies testrunner -P jython="$jython_path" $sdk_client_params -P args="-i /tmp/testexec.$$.ini -c ${confFile} -p ${parameters} -m rest ${rerun_param}"
-  guides/gradlew --refresh-dependencies testrunner -P jython="$jython_path" $sdk_client_params -P args="-i /tmp/testexec.$$.ini -c ${confFile} -p ${parameters} -m rest ${rerun_param}"
-
+  #echo guides/gradlew --refresh-dependencies testrunner -P jython="$jython_path" $sdk_client_params -P args="-i $WORKSPACE/testexec.$$.ini -c ${confFile} -p ${parameters} -m rest ${rerun_param}"
+  #guides/gradlew --refresh-dependencies testrunner -P jython="$jython_path" $sdk_client_params -P args="-i $WORKSPACE/testexec.$$.ini -c ${confFile} -p ${parameters} -m rest ${rerun_param}"
+  # Find free port on this machine to use for this run
+  sirius_port=49152 ; INCR=1 ; while [ -n "$(ss -tan4H "sport = $sirius_port")" ]; do sirius_port=$((sirius_port+INCR)) ; done
+  echo "Will use $sirius_port for starting sirius"
+  export PATH=/usr/local/go/bin:$PATH
+  set -x
+  python testrunner.py -c $confFile -i $WORKSPACE/testexec.$$.ini -p $parameters --launch_sirius_docker --sirius_url http://localhost:$sirius_port
+  set +x
 
   echo workspace is $WORKSPACE
   fails=`cat $WORKSPACE/logs/*/*.xml | grep 'testsuite errors' | awk '{split($3,s1,"=");print s1[2]}' | sed s/\"//g | awk '{s+=$1} END {print s}'`
@@ -349,4 +293,3 @@ else
   newState=failedInstall
   echo newState=failedInstall>propfile
 fi
-
