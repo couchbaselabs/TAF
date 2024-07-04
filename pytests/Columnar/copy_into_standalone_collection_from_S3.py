e471ff2191cac92c42ac2f7774e9321b785526d3
Abhay Aggrawal <abhay.aggrawal@couchbase.com>
2024-07-04 16:21:18 +0530

    copy from s3 fix
    Change-Id: I9215b12478a567631f0f6d628a9183ddfee1f104
    Reviewed-on: https://review.couchbase.org/c/TAF/+/212272
    Reviewed-by: Umang Agrawal <umang.agrawal@couchbase.com>
    Tested-by: Umang Agrawal <umang.agrawal@couchbase.com>

--- pytests/Columnar/copy_into_standalone_collection_from_S3.py
+++ pytests/Columnar/copy_into_standalone_collection_from_S3.py
@@ -117,11 +117,11 @@ class CopyIntoStandaloneCollectionFromS3(ColumnarBaseTest):
                       {
                           "cluster": self.instance, "collection_name": standalone_coll.name,
                           "aws_bucket_name": standalone_coll.dataset_properties["external_container_name"],
-                          "external_link_name":  standalone_coll.link_name,
+                          "external_link_name": standalone_coll.link_name,
                           "dataverse_name": standalone_coll.dataverse_name,
                           "database_name": standalone_coll.database_name,
                           "files_to_include": standalone_coll.dataset_properties["include"],
-                          "file_format":  standalone_coll.dataset_properties["file_format"],
+                          "file_format": standalone_coll.dataset_properties["file_format"],
                           "type_parsing_info": standalone_coll.dataset_properties["object_construction_def"],
                           "path_on_aws_bucket": standalone_coll.dataset_properties["path_on_external_container"],
                           "header": standalone_coll.dataset_properties["header"],
@@ -133,7 +133,7 @@ class CopyIntoStandaloneCollectionFromS3(ColumnarBaseTest):
                       }))
 
         self.cbas_util.run_jobs_in_parallel(
-                jobs, results, self.sdk_clients_per_user, async_run=False)
+            jobs, results, self.sdk_clients_per_user, async_run=False)
 
         if not all(results):
             self.fail("Copy into command failure")
@@ -311,7 +311,7 @@ class CopyIntoStandaloneCollectionFromS3(ColumnarBaseTest):
         self.columnar_spec["standalone_dataset"][
             "num_of_standalone_coll"] = self.input.param(
             "num_of_standalone_coll", 1)
-        self.columnar_spec["standalone_dataset"]["primary_key"] = [{"name": "string", "email": "string"}]
+        self.columnar_spec["standalone_dataset"]["primary_key"] = [{"id": "string", "product_name": "string"}]
 
         file_format = self.input.param("file_format", "json")
         dataset_properties = self.columnar_spec["standalone_dataset"][
@@ -387,7 +387,7 @@ class CopyIntoStandaloneCollectionFromS3(ColumnarBaseTest):
         self.columnar_spec["standalone_dataset"][
             "num_of_standalone_coll"] = self.input.param(
             "num_of_standalone_coll", 1)
-        self.columnar_spec["standalone_dataset"]["primary_key"] = [{"name": "string", "email": "string"}]
+        self.columnar_spec["standalone_dataset"]["primary_key"] = [{"id": "string", "product_name": "string"}]
         dataset_properties = self.columnar_spec["standalone_dataset"][
             "standalone_collection_properties"][0]
         dataset_properties["external_container_name"] = self.s3_source_bucket
@@ -422,7 +422,6 @@ class CopyIntoStandaloneCollectionFromS3(ColumnarBaseTest):
                     )):
                 self.fail("Error while copying data from S3 into {0} "
                           "standalone collection".format(standalone_coll.full_name))
-
 
     def test_create_copyinto_query_drop_link(self):
         self.columnar_spec["database"]["no_of_databases"] = self.input.param(
@@ -492,6 +491,7 @@ class CopyIntoStandaloneCollectionFromS3(ColumnarBaseTest):
         jobs.join()
         if not all(results):
             self.fail("Copy to statement failed after link disconnect")
+
     def test_create_copyinto_query_incorrect_file_format_clause_drop_standalone_collection(self):
         # Update columnar spec based on conf file params
         self.columnar_spec["database"]["no_of_databases"] = self.input.param(
