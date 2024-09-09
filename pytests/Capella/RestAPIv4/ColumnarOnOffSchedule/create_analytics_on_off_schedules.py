4330b7705fa0b0f2157e82cc26f2ff42e44d232c
vipbhardwaj <vipul.bhardwaj@couchbase.com>
2024-09-09 17:55:34 +0530

    Auth func changes - Columnar OnOff Sch
    Change-Id: Ie84a3be23b86522e898fb34cc631254449960bb7
    Reviewed-on: https://review.couchbase.org/c/TAF/+/215810
    Reviewed-by: Abhay Aggrawal <abhay.aggrawal@couchbase.com>
    Tested-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: VIPUL BHARDWAJ <vipul.bhardwaj@couchbase.com>

--- pytests/Capella/RestAPIv4/ColumnarOnOffSchedule/create_analytics_on_off_schedules.py
+++ pytests/Capella/RestAPIv4/ColumnarOnOffSchedule/create_analytics_on_off_schedules.py
@@ -169,10 +169,8 @@ class PostOnOffSchedule(GetAnalyticsClusters):
                 result = self.columnarAPI.create_on_off_schedule(
                     organization, project, analyticsCluster,
                     self.expected_res["timezone"], self.expected_res["days"])
-
             self.columnarAPI.schedule_on_off_endpoint = \
                 "/v4/organizations/{}/projects/{}/analyticsClusters/{}/onOffSchedule"
-
             if self.validate_testcase(result, [204], testcase, failures):
                 self.log.info("Schedule created successfully.")
                 time.sleep(2)
@@ -193,45 +191,14 @@ class PostOnOffSchedule(GetAnalyticsClusters):
                       .format(len(failures), len(testcases)))
 
     def test_authorization(self):
-        self.api_keys.update(
-            self.create_api_keys_for_all_combinations_of_roles(
-                [self.project_id]))
-
-        resp = self.capellaAPI.org_ops_apis.create_project(
-            self.organisation_id, "Auth_Project")
-        if resp.status_code == 201:
-            other_project_id = resp.json()["id"]
-        else:
-            self.fail("Error while creating project")
-
-        testcases = []
-        for role in self.api_keys:
-            testcase = {
-                "description": "Calling API with {} role".format(role),
-                "token": self.api_keys[role]["token"],
-            }
-            if not any(element in [
-                 "organizationOwner", "projectOwner",
-                 "projectManager"
-            ] for element in self.api_keys[role]["roles"]):
-                testcase["expected_error"] = {
-                    "code": 1002,
-                    "hint": "Your access to the requested resource is denied. "
-                            "Please make sure you have the necessary "
-                            "permissions to access the resource.",
-                    "httpStatusCode": 403,
-                    "message": "Access Denied."
-                }
-                testcase["expected_status_code"] = 403
-            testcases.append(testcase)
-        self.auth_test_extension(testcases, other_project_id)
-
         failures = list()
-        for testcase in testcases:
+        for testcase in self.v4_RBAC_injection_init([
+            "organizationOwner", "projectOwner", "projectManager"
+        ]):
             self.log.info("Executing test: {}".format(testcase["description"]))
             header = dict()
             self.auth_test_setup(testcase, failures, header,
-                                 self.project_id, other_project_id)
+                                 self.project_id, self.other_project_id)
             result = self.columnarAPI.create_on_off_schedule(
                 self.organisation_id, self.project_id,
                 self.analyticsCluster_id, self.expected_res["timezone"],
@@ -242,7 +209,6 @@ class PostOnOffSchedule(GetAnalyticsClusters):
                     self.organisation_id, self.project_id,
                     self.analyticsCluster_id, self.expected_res["timezone"],
                     self.expected_res["days"], header)
-
             if self.validate_testcase(result, [204], testcase, failures):
                 self.log.info("Schedule created successfully.")
                 time.sleep(2)
@@ -257,18 +223,10 @@ class PostOnOffSchedule(GetAnalyticsClusters):
                     self.log.info("Schedule deleted successfully.")
                     time.sleep(2)
 
-        self.update_auth_with_api_token(self.curr_owner_key)
-        resp = self.capellaAPI.org_ops_apis.delete_project(
-            self.organisation_id, other_project_id)
-        if resp.status_code != 204:
-            self.log.error("Error while deleting project {}"
-                           .format(other_project_id))
-
         if failures:
             for fail in failures:
                 self.log.warning(fail)
-            self.fail("{} tests FAILED out of {} TOTAL tests"
-                      .format(len(failures), len(testcases)))
+            self.fail("{} tests FAILED.".format(len(failures)))
 
     def test_query_parameters(self):
         self.log.debug(
@@ -367,7 +325,6 @@ class PostOnOffSchedule(GetAnalyticsClusters):
                     testcase["analyticsClusterID"],
                     self.expected_res["timezone"], self.expected_res["days"],
                     **kwarg)
-
             if self.validate_testcase(result, [204], testcase, failures):
                 self.log.info("Schedule created successfully.")
                 time.sleep(2)
--- pytests/Capella/RestAPIv4/ColumnarOnOffSchedule/get_analytics_on_off_schedules.py
+++ pytests/Capella/RestAPIv4/ColumnarOnOffSchedule/get_analytics_on_off_schedules.py
@@ -191,10 +191,8 @@ class GetOnOffSchedule(GetAnalyticsClusters):
                 self.handle_rate_limit(int(result.headers["Retry-After"]))
                 result = self.columnarAPI.fetch_on_off_schedule_info(
                     organization, project, analyticsCluster)
-
             self.columnarAPI.schedule_on_off_endpoint = \
                 "/v4/organizations/{}/projects/{}/analyticsClusters/{}/onOffSchedule"
-
             self.validate_testcase(result, [200], testcase, failures)
 
         if failures:
@@ -204,45 +202,14 @@ class GetOnOffSchedule(GetAnalyticsClusters):
                       .format(len(failures), len(testcases)))
 
     def test_authorization(self):
-        self.api_keys.update(
-            self.create_api_keys_for_all_combinations_of_roles(
-                [self.project_id]))
-
-        resp = self.capellaAPI.org_ops_apis.create_project(
-            self.organisation_id, "Auth_Project")
-        if resp.status_code == 201:
-            other_project_id = resp.json()["id"]
-        else:
-            self.fail("Error while creating project")
-
-        testcases = []
-        for role in self.api_keys:
-            testcase = {
-                "description": "Calling API with {} role".format(role),
-                "token": self.api_keys[role]["token"],
-            }
-            if not any(element in [
-                 "organizationOwner", "projectOwner",
-                 "projectManager"
-            ] for element in self.api_keys[role]["roles"]):
-                testcase["expected_error"] = {
-                    "code": 1002,
-                    "hint": "Your access to the requested resource is denied. "
-                            "Please make sure you have the necessary "
-                            "permissions to access the resource.",
-                    "httpStatusCode": 403,
-                    "message": "Access Denied."
-                }
-                testcase["expected_status_code"] = 403
-            testcases.append(testcase)
-        self.auth_test_extension(testcases, other_project_id)
-
         failures = list()
-        for testcase in testcases:
+        for testcase in self.v4_RBAC_injection_init([
+            "organizationOwner", "projectOwner", "projectManager"
+        ]):
             self.log.info("Executing test: {}".format(testcase["description"]))
             header = dict()
             self.auth_test_setup(testcase, failures, header,
-                                 self.project_id, other_project_id)
+                                 self.project_id, self.other_project_id)
             result = self.columnarAPI.fetch_on_off_schedule_info(
                 self.organisation_id, self.project_id,
                 self.analyticsCluster_id, header)
@@ -251,21 +218,12 @@ class GetOnOffSchedule(GetAnalyticsClusters):
                 result = self.columnarAPI.fetch_on_off_schedule_info(
                     self.organisation_id, self.project_id,
                     self.analyticsCluster_id, header)
-
             self.validate_testcase(result, [200], testcase, failures)
-
-        self.update_auth_with_api_token(self.curr_owner_key)
-        resp = self.capellaAPI.org_ops_apis.delete_project(
-            self.organisation_id, other_project_id)
-        if resp.status_code != 204:
-            self.log.error("Error while deleting project {}"
-                           .format(other_project_id))
 
         if failures:
             for fail in failures:
                 self.log.warning(fail)
-            self.fail("{} tests FAILED out of {} TOTAL tests"
-                      .format(len(failures), len(testcases)))
+            self.fail("{} tests FAILED.".format(len(failures)))
 
     def test_query_parameters(self):
         self.log.debug(
@@ -360,7 +318,6 @@ class GetOnOffSchedule(GetAnalyticsClusters):
                     testcase["organizationID"], testcase["projectID"],
                     testcase["analyticsClusterID"],
                     **kwarg)
-
             self.validate_testcase(result, [200], testcase, failures)
 
         if failures:
