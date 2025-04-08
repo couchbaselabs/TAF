3b152bc046415710be2160dffed6fdfcec6a6fa9
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2025-04-08 16:21:16 -0700

    Add support to disable reranking for bhive indexes.
    Change-Id: I1a318d69659a733d7052d3b9b4658e99a0911d91
    Reviewed-on: https://review.couchbase.org/c/TAF/+/226042
    Reviewed-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>
    Tested-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>

--- pytests/aGoodDoctor/Hospital.py
+++ pytests/aGoodDoctor/Hospital.py
@@ -324,6 +324,10 @@ class Murphy(BaseTestCase, OPD):
         if enableInMemoryCompression is False:
             self.sleep(0, "sleep before setting indexer params")
             self.gsi_rest.set_index_settings({"indexer.plasma.mainIndex.enableInMemoryCompression": False})
+        reranking = self.input.param("reranking", False)
+        if reranking is False:
+            self.sleep(0, "sleep before setting indexer params")
+            self.gsi_rest.set_index_settings({"indexer.scan.vector.rerank_factor": 0})
         # self.sleep(10, "sleep  after setting indexer params")
         if self.fts_nodes>0 and self.fts_nodes > len(self.cluster.fts_nodes):
             self.rest.set_service_mem_quota({CbServer.Settings.FTS_MEM_QUOTA:
