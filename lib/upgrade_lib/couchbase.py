50f7fa7eb86697bcb2a351678bd6945208f61be4
Raghav S K <raghav.sk@couchbase.com>
2025-06-24 03:35:05 +0530

    CBQE-8536 : Adding released builds to testconstants
    Change-Id: I8015416fd254a2e77947d8bf7c768d534f4f6b06
    Reviewed-on: https://review.couchbase.org/c/TAF/+/229720
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- lib/upgrade_lib/couchbase.py
+++ lib/upgrade_lib/couchbase.py
@@ -24,12 +24,15 @@ upgrade_chains = {
     "7.2.4": ["7.2.4-" + CB_RELEASE_BUILDS["7.2.4"]],
     "7.2.5": ["7.2.5-" + CB_RELEASE_BUILDS["7.2.5"]],
     "7.2.6": ["7.2.6-" + CB_RELEASE_BUILDS["7.2.6"]],
+    "7.2.7": ["7.2.7-" + CB_RELEASE_BUILDS["7.2.7"]],
 
     "7.6.0": ["7.6.0-" + CB_RELEASE_BUILDS["7.6.0"]],
     "7.6.1": ["7.6.1-" + CB_RELEASE_BUILDS["7.6.1"]],
     "7.6.2": ["7.6.2-" + CB_RELEASE_BUILDS["7.6.2"]],
     "7.6.3": ["7.6.3-" + CB_RELEASE_BUILDS["7.6.3"]],
     "7.6.4": ["7.6.4-" + CB_RELEASE_BUILDS["7.6.4"]],
+    "7.6.5": ["7.6.5-" + CB_RELEASE_BUILDS["7.6.5"]],
+    "7.6.6": ["7.6.6-" + CB_RELEASE_BUILDS["7.6.6"]],
 
     # Ref: https://docs.couchbase.com/server/current/install/upgrade.html
     "6.6.4_7.2.3": ["6.6.4-" + CB_RELEASE_BUILDS["6.6.4"],
