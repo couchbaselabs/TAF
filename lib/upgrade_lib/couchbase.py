620a80bc7aeb36c3c9e0a6570e7be226456ea88c
Raghav S K <raghav.sk@couchbase.com>
2025-09-12 08:51:42 +0530

    Adding latest 7.2.x releases to testconstants
    Change-Id: I0e4c49d161744a9b59820d086eba1c3f0ac92974
    Reviewed-on: https://review.couchbase.org/c/TAF/+/233729
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- lib/upgrade_lib/couchbase.py
+++ lib/upgrade_lib/couchbase.py
@@ -23,8 +23,10 @@ upgrade_chains = {
     "7.2.2": ["7.2.2-" + CB_RELEASE_BUILDS["7.2.2"]],
     "7.2.3": ["7.2.3-" + CB_RELEASE_BUILDS["7.2.3"]],
     "7.2.4": ["7.2.4-" + CB_RELEASE_BUILDS["7.2.4"]],
-    "7.2.5": ["7.2.4-" + CB_RELEASE_BUILDS["7.2.5"]],
-    "7.2.6": ["7.2.4-" + CB_RELEASE_BUILDS["7.2.6"]],
+    "7.2.5": ["7.2.5-" + CB_RELEASE_BUILDS["7.2.5"]],
+    "7.2.6": ["7.2.6-" + CB_RELEASE_BUILDS["7.2.6"]],
+    "7.2.7": ["7.2.7-" + CB_RELEASE_BUILDS["7.2.7"]],
+    "7.2.8": ["7.2.8-" + CB_RELEASE_BUILDS["7.2.8"]],
 
     "7.6.0": ["7.6.0-" + CB_RELEASE_BUILDS["7.6.0"]],
     "7.6.1": ["7.6.0-" + CB_RELEASE_BUILDS["7.6.1"]],
