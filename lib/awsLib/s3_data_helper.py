d5cbfae1ff36f8e4448cde1009ce9b5b97634be9
Ashwin <ashwin.govindarajulu@couchbase.com>
2026-02-04 15:20:52 +0530

    Updating s3_data_helper.py to handle None values in aws keys
    This is due to the prev. changes we introduced to read the values
    from env using os.getenv("XXX", None) code
    
    Change-Id: I7cfd493b2decfaa2e0ee32cad7f3dfcb978708ad
    Reviewed-on: https://review.couchbase.org/c/TAF/+/239700
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Reviewed-by: Himanshu Jain <himanshu.jain@couchbase.com>

--- lib/awsLib/s3_data_helper.py
+++ lib/awsLib/s3_data_helper.py
@@ -17,9 +17,11 @@ def perform_S3_operation(**kwargs):
     """
     aws_util_file_path = os.path.abspath(os.path.join(
         os.path.dirname(__file__), "S3.py"))
-    arguements = ["python3", aws_util_file_path, kwargs.get("aws_access_key"),
-                  kwargs.get("aws_secret_key"), kwargs.get("aws_session_token", ""),
-                  kwargs.get("endpoint_url", "")]
+    arguements = ["python3", aws_util_file_path,
+                  kwargs.get("aws_access_key") or "",
+                  kwargs.get("aws_secret_key") or "",
+                  kwargs.get("aws_session_token") or "",
+                  kwargs.get("endpoint_url") or ""]
 
     if kwargs.get("get_regions", False):
         arguements.append("--get_regions")
