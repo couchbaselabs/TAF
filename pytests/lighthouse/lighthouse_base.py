742a89921e4e5833426f9a95abc00b383f55b86b
pulkitM1 <pulkit.matta@couchbase.com>
2026-06-24 14:09:53 +0530

    AV-132790: moving base class to relevant directory
    Change-Id: I2a0c5281a651acc8aa8352d716e63268d8b4cbd0
    Reviewed-on: https://review.couchbase.org/c/TAF/+/247397
    Reviewed-by: Kushagra <kushagra.kinjawadekar@couchbase.com>
    Tested-by: <pulkit.matta@couchbase.com>
    (cherry picked from commit c80757a940a544cb4118d77bfb23de3edd923b21)
    Reviewed-on: https://review.couchbase.org/c/TAF/+/247457

--- pytests/lighthouse/lighthouse_base.py
+++ pytests/lighthouse/lighthouse_base.py
@@ -1,46 +1,42 @@
 # -*- coding: utf-8 -*-
 """
 LighthouseBase - Base class for multi-cluster Lighthouse tests.
-
 Inheritance: LighthouseBase -> CollectionBase -> ClusterSetup -> OnPremBaseTest
-
 Usage in ini file:
     [cluster1]
     1:_1
     2:_2
-
     [cluster3]
     3:_3
     4:_4
-
     [LHPortal]
     5:_5
-
     [servers]
     1:_1
     2:_2
     3:_3
     4:_4
     5:_5
-
 Usage in conf:
     test_something,nodes_init=2|2,services_init=kv:n1ql-kv:n1ql|kv:cbas-kv:cbas,num_of_clusters=2
 """
-
 from TestInput import TestInputSingleton
 from bucket_collections.collections_base import CollectionBase
 from membase.api.rest_client import RestConnection
-from couchbase_utils.cluster_utils.cluster_ready_functions import ClusterUtils, CBCluster
+from cluster_utils.cluster_ready_functions import ClusterUtils, CBCluster
 from global_vars import logger
-
+from unified_control_plane import UnifiedControlPlaneClient
+from lighthouse.lighthouse_portal import LighthousePortal
 
 class LighthouseBase(CollectionBase):
-
     def setUp(self):
         if not hasattr(self, "input"):
             self.input = TestInputSingleton.input
         self.log = logger.get("test")
         self.log.info("Test parameters: %s" % self.input.test_params)
+        # Remove LHPortal servers from input.servers so they are
+        # never treated as Couchbase Server cluster nodes.
+        self._exclude_portal_from_servers()
         self._topology = ClusterUtils.get_cluster_topology(self.input)
         self._is_multi_cluster = self._topology["num_clusters"] > 1
         self.log.info("Cluster topology: %s" % self._topology)
@@ -53,7 +49,52 @@ class LighthouseBase(CollectionBase):
         self.clusters.append(self.cluster)
         if self._is_multi_cluster:
             self._setup_additional_clusters()
+        # Extract UCP portal from [LHPortal] ini section
+        self._setup_ucp_portal()
         self.log_setup_status("LighthouseBase", "complete")
+
+    def _exclude_portal_from_servers(self):
+        """
+        Remove LHPortal nodes from input.servers and input.clusters
+        so they are never included in Couchbase cluster operations
+        (rebalance, tearDown reset, etc).
+        """
+        if not self.input.lh_portal:
+            return
+        portal_ips = {s.ip for s in self.input.lh_portal}
+        self.input.servers = [s for s in self.input.servers
+                              if s.ip not in portal_ips]
+        # Also remove from any cluster group that may contain them
+        for key in list(self.input.clusters.keys()):
+            self.input.clusters[key] = [
+                s for s in self.input.clusters[key]
+                if s.ip not in portal_ips]
+            # Remove empty cluster entries
+            if not self.input.clusters[key]:
+                del self.input.clusters[key]
+
+    def _setup_ucp_portal(self):
+        """
+        Extract the UCP portal server from input.lh_portal and
+        build a LighthousePortal config object.
+        The IP comes from [LHPortal] ini section.
+        Port and credentials can be overridden via test params:
+            - ucp_port (default 8080)
+            - ucp_username (default from [membase] rest_username)
+            - ucp_password (default from [membase] rest_password)
+        Exposes:
+            self.ucp_portal  - LighthousePortal config object
+            self.ucp_client  - UnifiedControlPlaneClient instance
+        """
+        if not self.input.lh_portal:
+            self.fail("No UCP portal server found. "
+                      "Ensure [LHPortal] section is defined in the ini file.")
+        server = self.input.lh_portal[0]
+        self.ucp_portal = LighthousePortal.from_server_and_params(
+            server, self.input)
+        self.log.info("UCP portal: %s" % self.ucp_portal)
+        # Create UCP client using the portal config
+        self.ucp_client = UnifiedControlPlaneClient(self.ucp_portal)
 
     def tearDown(self):
         if hasattr(self, "_original_servers"):
@@ -63,19 +104,15 @@ class LighthouseBase(CollectionBase):
             extra_clusters = [name for name in self.cb_clusters
                               if name != "C1"]
             for name in extra_clusters:
+                cluster = self.cb_clusters[name]
+                for server in cluster.servers:
+                    try:
+                        rest = RestConnection(server)
+                        rest.reset_node()
+                    except Exception as e:
+                        self.log.warning(
+                            "Failed to reset node %s: %s" % (server.ip, e))
                 del self.cb_clusters[name]
-
-        super(LighthouseBase, self).tearDown()
-        all_servers = getattr(self, "_original_servers",
-                              self.input.servers)
-        for server in all_servers:
-            try:
-                rest = RestConnection(server)
-                rest.reset_node()
-            except Exception as e:
-                self.log.warning(
-                    "Failed to reset node %s: %s" % (server.ip, e))
-
 
     def _restrict_input_to_primary_cluster(self):
         self._original_servers = list(self.input.servers)
@@ -95,23 +132,17 @@ class LighthouseBase(CollectionBase):
             cluster_nodes = cluster_info["nodes"]
             cluster_services = cluster_info["services"]
             cluster_name = cluster_name_format % str(i + 1)
-
             cluster = CBCluster(name=cluster_name,
                                 servers=cluster_nodes)
             self.cb_clusters[cluster_name] = cluster
-
             master_services = cluster_services[0] \
                 if cluster_services else None
             self.initialize_cluster(
                 cluster_name, cluster, services=master_services)
-
             self.enforce_tls_and_set_ports(cluster_nodes)
-
             cluster.nodes_in_cluster = list(cluster_nodes)
-
             self._rebalance_in_nodes(cluster, cluster_name,
                                      cluster_nodes, cluster_services)
-
             self.cluster_util.update_cluster_nodes_service_list(cluster)
             self.clusters.append(cluster)
             self.log.info("Cluster %s ready: %d node(s) [%s]"
@@ -126,4 +157,3 @@ class LighthouseBase(CollectionBase):
             self.task, cluster, services=services)
         if not result:
             self.fail("Initial rebalance failed for %s" % cluster_name)
-
