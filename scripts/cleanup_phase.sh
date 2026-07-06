cb09f3be95bd088afe4cb7a62aca6b9433f96277
Ashwin <ashwin.govindarajulu@couchbase.com>
2026-07-06 17:07:44 +0530

    Adding ulimit -u for cleanup script
    Seeing longer execution times for lsof command execution
    
    Change-Id: Ia3f736ad51931af7631ea514731bb47f6f90f742
    Reviewed-on: https://review.couchbase.org/c/TAF/+/248185
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Reviewed-on: https://review.couchbase.org/c/TAF/+/248187

--- scripts/cleanup_phase.sh
+++ scripts/cleanup_phase.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+set +x
+echo "Setting ulimit values for this session"
+# Set data seg size
+ulimit -d unlimited
+# Set file size
+ulimit -f unlimited
+# Set pending signals
+ulimit -i 127868
+# Set max locked memory
+ulimit -l 3075360
+# Set max memory size
+ulimit -m unlimited
+# Set open files
+ulimit -n 204200
+# Set POSIX message queues
+ulimit -q 819200
+# Set stack size
+ulimit -s 8192
+# Set cpu time
+ulimit -t unlimited
+# Set max user processes
+ulimit -u 127868
+# Set virtual memory
+ulimit -v unlimited
+# Set file locks
+ulimit -x unlimited
+
+echo "########## ulimit values ###########"
+ulimit -a
+echo "####################################"
+
 # To clean any available space from docker
 docker system prune -f
 
